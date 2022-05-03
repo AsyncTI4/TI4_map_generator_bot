c06313f1add4ce9d49bc2e553eff4c714dd64e90
TerTerro <terastrom@yahoo.com>
2022-05-03 19:39:23 +0300

    bug fix

--- src/main/java/ti4/commands/status/RevealStage1.java
+++ src/main/java/ti4/commands/status/RevealStage1.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import ti4.generator.Mapper;
 import ti4.helpers.Constants;
+import ti4.helpers.Helper;
 import ti4.map.*;
 import ti4.message.MessageHelper;
 
@@ -23,7 +24,8 @@ public class RevealStage1 extends StatusSubcommandData {
         java.util.Map.Entry<String, Integer> objective = activeMap.revealState1();
 
         StringBuilder sb = new StringBuilder();
-        sb.append("**Public Objective Stage 1:**").append("\n");
+        sb.append(Helper.getGamePing(event, activeMap));
+        sb.append(" **Public Objective Stage 1:**").append("\n");
 
         sb.append("(").append(objective.getValue()).append(") ").append("\n");
         sb.append(Mapper.getPublicObjective(objective.getKey())).append("\n");
