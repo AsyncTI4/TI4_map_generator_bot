29b99e14bf5afd8171b4cf2866c774e1d9c8ce40
TerTerro <terastrom@yahoo.com>
2022-05-07 10:52:53 +0300

    tech support

--- src/main/java/ti4/AutoCompleteProvider.java
+++ src/main/java/ti4/AutoCompleteProvider.java
@@ -13,7 +13,8 @@ import java.util.stream.Stream;
 public class AutoCompleteProvider {
 
     public static void autoCompleteListener(CommandAutoCompleteInteractionEvent event) {
-        if (event.getFocusedOption().getName().equals(Constants.COLOR)) {
+        String optionName = event.getFocusedOption().getName();
+        if (optionName.equals(Constants.COLOR)) {
             String enteredValue = event.getFocusedOption().getValue();
             List<Command.Choice> options = Mapper.getColors().stream()
                     .limit(25)
@@ -21,7 +22,7 @@ public class AutoCompleteProvider {
                     .map(color -> new net.dv8tion.jda.api.interactions.commands.Command.Choice(color, color))
                     .collect(Collectors.toList());
             event.replyChoices(options).queue();
-        }  else if (event.getFocusedOption().getName().equals(Constants.FACTION)) {
+        }  else if (optionName.equals(Constants.FACTION)) {
             String enteredValue = event.getFocusedOption().getValue();
             List<net.dv8tion.jda.api.interactions.commands.Command.Choice> options = Mapper.getFactions().stream()
                     .filter(token -> token.contains(enteredValue))
@@ -29,7 +30,7 @@ public class AutoCompleteProvider {
                     .map(token -> new net.dv8tion.jda.api.interactions.commands.Command.Choice(token, token))
                     .collect(Collectors.toList());
             event.replyChoices(options).queue();
-        } else if (event.getFocusedOption().getName().equals(Constants.TOKEN)) {
+        } else if (optionName.equals(Constants.TOKEN)) {
             String enteredValue = event.getFocusedOption().getValue();
             List<net.dv8tion.jda.api.interactions.commands.Command.Choice> options = Mapper.getTokens().stream()
                     .filter(token -> token.contains(enteredValue))
@@ -37,7 +38,7 @@ public class AutoCompleteProvider {
                     .map(token -> new net.dv8tion.jda.api.interactions.commands.Command.Choice(token, token))
                     .collect(Collectors.toList());
             event.replyChoices(options).queue();
-        } else if (event.getFocusedOption().getName().equals(Constants.GAME_STATUS)) {
+        } else if (optionName.equals(Constants.GAME_STATUS)) {
             String enteredValue = event.getFocusedOption().getValue();
             List<net.dv8tion.jda.api.interactions.commands.Command.Choice> options = Stream.of("open", "locked")
                     .filter(value -> value.contains(enteredValue))
@@ -45,7 +46,7 @@ public class AutoCompleteProvider {
                     .map(value -> new net.dv8tion.jda.api.interactions.commands.Command.Choice(value, value))
                     .collect(Collectors.toList());
             event.replyChoices(options).queue();
-        } else if (event.getFocusedOption().getName().equals(Constants.DISPLAY_TYPE)) {
+        } else if (optionName.equals(Constants.DISPLAY_TYPE)) {
             String enteredValue = event.getFocusedOption().getValue();
             List<net.dv8tion.jda.api.interactions.commands.Command.Choice> options = Stream.of("all", "map", "stats", "none")
                     .filter(value -> value.contains(enteredValue))
@@ -53,7 +54,7 @@ public class AutoCompleteProvider {
                     .map(value -> new net.dv8tion.jda.api.interactions.commands.Command.Choice(value, value))
                     .collect(Collectors.toList());
             event.replyChoices(options).queue();
-        } else if (event.getFocusedOption().getName().equals(Constants.TECH)) {
+        } else if (optionName.equals(Constants.TECH) || optionName.equals(Constants.TECH2) || optionName.equals(Constants.TECH3) || optionName.equals(Constants.TECH4)) {
             String enteredValue = event.getFocusedOption().getValue().toLowerCase();
             HashMap<String, String> techs = Mapper.getTechs();
             List<net.dv8tion.jda.api.interactions.commands.Command.Choice> options = techs.entrySet().stream()
