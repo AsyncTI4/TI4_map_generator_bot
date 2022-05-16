0f435399901428b485bb08e190e7091629040aa3
TerTerro <terastrom@yahoo.com>
2022-05-16 21:05:06 +0300

    improvements

--- src/main/java/ti4/commands/player/Turn.java
+++ src/main/java/ti4/commands/player/Turn.java
@@ -79,7 +79,7 @@ public class Turn extends PlayerSubcommandData {
             }
         }
         if (scPassed.values().stream().allMatch(value -> value)){
-            MessageHelper.replyToMessage(event, "All PASSED " +Helper.getGamePing(event, map));
+            MessageHelper.replyToMessage(event, "All players passed. Please score objectives or react with your faction symbol for no scoring. " +Helper.getGamePing(event, map));
             return;
         }
 
