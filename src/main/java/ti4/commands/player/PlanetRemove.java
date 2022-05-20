d82b9d3dc25d561cae6814d2d57de8184a83d131
TerTerro <terastrom@yahoo.com>
2022-05-20 23:23:40 +0300

    bug fixes

--- src/main/java/ti4/commands/player/PlanetRemove.java
+++ src/main/java/ti4/commands/player/PlanetRemove.java
@@ -1,8 +1,10 @@
 package ti4.commands.player;
 
+import ti4.generator.Mapper;
 import ti4.helpers.Constants;
 import ti4.map.Map;
 import ti4.map.Player;
+import ti4.map.UnitHolder;
 
 public class PlanetRemove extends PlanetAddRemove {
     public PlanetRemove() {
@@ -12,5 +14,14 @@ public class PlanetRemove extends PlanetAddRemove {
     @Override
     public void doAction(Player player, String planet, Map map) {
         player.removePlanet(planet);
+        UnitHolder unitHolder = map.getPlanetsInfo().get(planet);
+        String color = player.getColor();
+        if (unitHolder != null && color != null && !"white".equals(color)) {
+            String ccID = Mapper.getControlID(color);
+            String ccPath = Mapper.getCCPath(ccID);
+            if (ccPath != null) {
+                unitHolder.removeControl(ccID);
+            }
+        }
     }
 }
