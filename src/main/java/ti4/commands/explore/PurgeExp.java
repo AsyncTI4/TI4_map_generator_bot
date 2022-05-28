e65360353a5508b87fd0efb81e1f1ebb94a53748
Updownbanana <dylan.yoder@gmail.com>
2022-05-28 03:19:26 -0500

    attempting to fix mistakes in the repository

---
+++ src/main/java/ti4/commands/explore/PurgeExp.java
@@ -0,0 +1,34 @@
+package ti4.commands.explore;
+
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import ti4.generator.Mapper;
+import ti4.helpers.Constants;
+import ti4.map.Map;
+import ti4.message.MessageHelper;
+
+public class PurgeExp extends ExploreSubcommandData {
+
+	public PurgeExp() {
+		super(Constants.REMOVE, "Remove an Exploration card from the game.");
+		addOptions(idOption.setRequired(true));
+	}
+
+    @Override
+    public void execute(SlashCommandInteractionEvent event) {
+        Map activeMap = getActiveMap();
+        String ids = event.getOption(Constants.EXPLORE_CARD_ID).getAsString().replaceAll(" ", "");
+        String[] idList = ids.split(",");
+        StringBuilder sb = new StringBuilder();
+        for (String id : idList) {
+            String card = Mapper.getExplore(id);
+            if (card != null) {
+                activeMap.purgeExplore(id);
+                sb.append("Exploration card removed: ").append(displayExplore(id)).append(System.lineSeparator());
+            } else {
+                activeMap.purgeExplore(id);
+                sb.append("Removed id without matching card: ").append(id).append(System.lineSeparator());
+            }
+        }
+        MessageHelper.replyToMessage(event, sb.toString());
+    }
+}
