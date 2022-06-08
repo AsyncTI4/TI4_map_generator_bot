bb6e8cc57c2c7dd3e0130c9e867710d8cb7dc697
shavnir <shavnir@gmail.com>
2022-06-08 06:00:27 -0500

    Switch tabs to spaces

--- src/main/java/ti4/commands/status/ListVoteCount.java
+++ src/main/java/ti4/commands/status/ListVoteCount.java
@@ -15,55 +15,55 @@ import java.util.*;
 
 public class ListVoteCount extends StatusSubcommandData {
     public ListVoteCount() {
-	super(Constants.VOTE_COUNT, "List Vote count for agenda");
+        super(Constants.VOTE_COUNT, "List Vote count for agenda");
     }
 
     @Override
     public void execute(SlashCommandInteractionEvent event) {
-	Map map = getActiveMap();
-	turnOrder(event, map);
+        Map map = getActiveMap();
+        turnOrder(event, map);
     }
 
     public static void turnOrder(SlashCommandInteractionEvent event, Map map) {
-	StringBuilder msg = new StringBuilder();
-	int i = 1;
-	List<Player> orderList = map.getPlayers().values().stream().toList();
-	String speakerName = map.getSpeaker();
-	Optional<Player> optSpeaker = orderList.stream().filter(player -> player.getUserName().equals(speakerName))
-		.findFirst();
+        StringBuilder msg = new StringBuilder();
+        int i = 1;
+        List<Player> orderList = map.getPlayers().values().stream().toList();
+        String speakerName = map.getSpeaker();
+        Optional<Player> optSpeaker = orderList.stream().filter(player -> player.getUserName().equals(speakerName))
+                .findFirst();
 
-	if (optSpeaker.isPresent()) {
-	    int rotationDistance = orderList.size() - orderList.indexOf(optSpeaker.get()) - 1;
-	    Collections.rotate(orderList, rotationDistance);
-	}
+        if (optSpeaker.isPresent()) {
+            int rotationDistance = orderList.size() - orderList.indexOf(optSpeaker.get()) - 1;
+            Collections.rotate(orderList, rotationDistance);
+        }
 
-	for (Player player : orderList) {
-	    List<String> planets = new ArrayList<>(player.getPlanets());
-	    planets.removeAll(player.getExhaustedPlanets());
-	    String userName = player.getUserName();
-	    String color = player.getColor();
+        for (Player player : orderList) {
+            List<String> planets = new ArrayList<>(player.getPlanets());
+            planets.removeAll(player.getExhaustedPlanets());
+            String userName = player.getUserName();
+            String color = player.getColor();
 
-	    String text = "";
-	    text += Helper.getFactionIconFromDiscord(player.getFaction());
-	    text += " " + userName;
-	    if (color != null) {
-		text += " (" + color + ")";
-	    }
-	    HashMap<String, UnitHolder> planetsInfo = map.getPlanetsInfo();
-	    int influenceCount = planets.stream().map(planetsInfo::get).filter(Objects::nonNull)
-		    .map(planet -> (Planet) planet).mapToInt(Planet::getInfluence).sum();
-	    text += " vote Count: **" + influenceCount + "**";
-	    if (userName.equals(speakerName)) {
-		text += " <:Speakertoken:965363466821050389> ";
-	    }
-	    msg.append(i).append(". ").append(text).append("\n");
-	    i++;
-	}
-	MessageHelper.replyToMessage(event, msg.toString());
+            String text = "";
+            text += Helper.getFactionIconFromDiscord(player.getFaction());
+            text += " " + userName;
+            if (color != null) {
+                text += " (" + color + ")";
+            }
+            HashMap<String, UnitHolder> planetsInfo = map.getPlanetsInfo();
+            int influenceCount = planets.stream().map(planetsInfo::get).filter(Objects::nonNull)
+                    .map(planet -> (Planet) planet).mapToInt(Planet::getInfluence).sum();
+            text += " vote Count: **" + influenceCount + "**";
+            if (userName.equals(speakerName)) {
+                text += " <:Speakertoken:965363466821050389> ";
+            }
+            msg.append(i).append(". ").append(text).append("\n");
+            i++;
+        }
+        MessageHelper.replyToMessage(event, msg.toString());
     }
 
     @Override
     public void reply(SlashCommandInteractionEvent event) {
-	// We reply in execute command
+        // We reply in execute command
     }
 }
