b433ef5e6584f6c4fab977d87683520a5ab53299
TerTerro <terastrom@yahoo.com>
2022-06-14 22:34:52 +0300

    Community mode support

--- src/main/java/ti4/commands/agenda/LookAtBottomAgenda.java
+++ src/main/java/ti4/commands/agenda/LookAtBottomAgenda.java
@@ -1,9 +1,13 @@
 package ti4.commands.agenda;
 
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import ti4.generator.Mapper;
 import ti4.helpers.Constants;
+import ti4.helpers.Helper;
 import ti4.map.Map;
+import ti4.map.Player;
 import ti4.message.MessageHelper;
 
 public class LookAtBottomAgenda extends AgendaSubcommandData {
@@ -20,6 +24,22 @@ public class LookAtBottomAgenda extends AgendaSubcommandData {
         sb.append("Agenda Bottom:\n");
         sb.append(Mapper.getAgenda(activeMap.lookAtBottomAgenda()));
         sb.append("\n-----------\n");
-        MessageHelper.sentToMessageToUser(event, sb.toString());
+
+        Player player = activeMap.getPlayer(getUser().getId());
+        player = Helper.getGamePlayer(activeMap, player, event, null);
+        if (player == null){
+            MessageHelper.sentToMessageToUser(event, sb.toString());
+        } else {
+            User userById = event.getJDA().getUserById(player.getUserID());
+            if (userById != null) {
+                if (activeMap.isCommunityMode() && player.getChannelForCommunity() instanceof MessageChannel) {
+                    MessageHelper.sendMessageToChannel((MessageChannel) player.getChannelForCommunity(), sb.toString());
+                } else {
+                    MessageHelper.sentToMessageToUser(event, sb.toString(), userById);
+                }
+            } else {
+                MessageHelper.sentToMessageToUser(event, sb.toString());
+            }
+        }
     }
 }
