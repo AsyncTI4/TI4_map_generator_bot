adb2f39e39e6c9ebda043048b6e632a65a6fd1d2
TerTerro <terastrom@yahoo.com>
2022-06-14 22:34:52 +0300

    Community mode support

--- src/main/java/ti4/commands/agenda/DrawAgenda.java
+++ src/main/java/ti4/commands/agenda/DrawAgenda.java
@@ -1,12 +1,16 @@
 package ti4.commands.agenda;
 
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import ti4.generator.Mapper;
 import ti4.helpers.Constants;
+import ti4.helpers.Helper;
 import ti4.map.Map;
+import ti4.map.Player;
 import ti4.message.MessageHelper;
 
 public class DrawAgenda extends AgendaSubcommandData {
@@ -38,6 +42,21 @@ public class DrawAgenda extends AgendaSubcommandData {
             }
         }
         sb.append("-----------\n");
-        MessageHelper.sentToMessageToUser(event, sb.toString());
+        Player player = activeMap.getPlayer(getUser().getId());
+        player = Helper.getGamePlayer(activeMap, player, event, null);
+        if (player == null){
+            MessageHelper.sentToMessageToUser(event, sb.toString());
+        } else {
+            User userById = event.getJDA().getUserById(player.getUserID());
+            if (userById != null) {
+                if (activeMap.isCommunityMode() && player.getChannelForCommunity() instanceof MessageChannel) {
+                    MessageHelper.sendMessageToChannel((MessageChannel) player.getChannelForCommunity(), sb.toString());
+                } else {
+                    MessageHelper.sentToMessageToUser(event, sb.toString(), userById);
+                }
+            } else {
+                MessageHelper.sentToMessageToUser(event, sb.toString());
+            }
+        }
     }
 }
