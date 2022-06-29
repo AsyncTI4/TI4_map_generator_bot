d5d07123ec215a90fc00774c751a3bc88b396d86
TerTerro <terastrom@yahoo.com>
2022-06-29 21:55:43 +0300

    leaders

--- src/main/java/ti4/commands/leaders/ExhaustLeader.java
+++ src/main/java/ti4/commands/leaders/ExhaustLeader.java
@@ -1,6 +1,10 @@
 package ti4.commands.leaders;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import ti4.commands.player.Stats;
 import ti4.helpers.Constants;
 import ti4.map.Leader;
 import ti4.map.Map;
@@ -10,13 +14,22 @@ import ti4.message.MessageHelper;
 public class ExhaustLeader extends LeaderAction {
     public ExhaustLeader() {
         super(Constants.EXHAUST_LEADER, "Exhaust leader");
+        addOptions(new OptionData(OptionType.STRING, Constants.TG, "TG count to add to leader").setRequired(false));
     }
 
     @Override
     void action(SlashCommandInteractionEvent event, String leader, Map activeMap, Player player) {
         Leader playerLeader = player.getLeader(leader);
-        if (playerLeader != null){
+        if (playerLeader != null) {
+            if (playerLeader.isLocked()){
+                MessageHelper.sendMessageToChannel(event.getChannel(), "Leader is locked");
+                return;
+            }
             playerLeader.setExhausted(true);
+            OptionMapping optionTG = event.getOption(Constants.TG);
+            if (optionTG != null) {
+                Stats.setValue(event, player, optionTG, playerLeader::setTgCount, playerLeader::getTgCount);
+            }
         } else {
             MessageHelper.sendMessageToChannel(event.getChannel(), "Leader not found");
         }
