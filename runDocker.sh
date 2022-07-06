47d05b95d9b5e6319b18c153a700832142bc19a7
Adam Best <abest@dwolla.com>
2022-07-06 15:54:17 -0500

    Working through some things

--- runDocker.sh
+++ runDocker.sh
@@ -1,1 +1,2 @@
-docker run -v/g/Dev_stuff/TI_Bot:/opt/STORAGE tibot java -jar BotTest.jar 
+docker run -v $(pwd)/storage:/opt/STORAGE tibot java -jar tibot.jar "OTc4Mjk1ODgzMTUyMTA1NTUz.GyLGNo.H-cLoVqlfqHuGe4_r6mqWJ7lyFgI8SGky1Sjo4" "978295883152105553" "950413954327396362"
+# docker run -it -v $(pwd)/storage:/opt/STORAGE tibot sh
