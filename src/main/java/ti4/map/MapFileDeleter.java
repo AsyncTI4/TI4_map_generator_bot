9b19c1cf00e5859421cf90f4f11659c428f1eb05
TerTerro <terastrom@yahoo.com>
2022-07-24 21:42:33 +0300

    log

--- src/main/java/ti4/map/MapFileDeleter.java
+++ src/main/java/ti4/map/MapFileDeleter.java
@@ -17,7 +17,7 @@ public class MapFileDeleter {
 
         File mapImageDirectory = Storage.getMapImageDirectory();
 
-        System.out.println("FILE0: " + mapImageDirectory.getAbsolutePath());
+        System.out.println("FILE0: " + mapImageDirectory.exists());
         System.out.println("FILE1: " + new File(Storage.getStoragePath()).getAbsolutePath());
         System.out.println("----------------------");
         System.out.println("VAR1: " + new File("/ti4bot/ti4bot_saves" + Storage.MAPS).exists());
