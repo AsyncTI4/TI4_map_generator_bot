db457b9dfdbcb2d8ed3e6ddc5d68de15d82cee76
TerTerro <terastrom@yahoo.com>
2022-07-24 21:39:50 +0300

    log

--- src/main/java/ti4/map/MapFileDeleter.java
+++ src/main/java/ti4/map/MapFileDeleter.java
@@ -19,6 +19,18 @@ public class MapFileDeleter {
 
         System.out.println("FILE0: " + mapImageDirectory.getAbsolutePath());
         System.out.println("FILE1: " + new File(Storage.getStoragePath()).getAbsolutePath());
+        System.out.println("----------------------");
+        System.out.println("VAR1: " + new File("/ti4bot/ti4bot_saves" + Storage.MAPS).exists());
+        System.out.println("VAR2: " + new File("./ti4bot/ti4bot_saves" + Storage.MAPS).exists());
+        System.out.println("VAR3: " + new File("../ti4bot/ti4bot_saves" + Storage.MAPS).exists());
+        System.out.println("VAR4: " + new File("../../ti4bot/ti4bot_saves" + Storage.MAPS).exists());
+        System.out.println("VAR5: " + new File("../../ti4bot_saves" + Storage.MAPS).exists());
+        System.out.println("VAR6: " + new File("../ti4bot_saves" + Storage.MAPS).exists());
+        System.out.println("VAR7: " + new File("../../../ti4bot/ti4bot_saves" + Storage.MAPS).exists());
+        System.out.println("VAR8: " + new File("../../../ti4bot/ti4bot_saves" + Storage.MAPS).exists());
+        System.out.println("VAR9: " + new File("./../../../ti4bot/ti4bot_saves" + Storage.MAPS).exists());
+        System.out.println("VAR10: " + new File("./../../ti4bot/ti4bot_saves" + Storage.MAPS).exists());
+
 
         for (File file : mapImageDirectory.listFiles()) {
             String absolutePath = file.getAbsolutePath();
