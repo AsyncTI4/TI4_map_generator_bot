f425a03594076bf3c5ef80cc9ca3a9f500fc7b5c
Adam Best <abest@dwolla.com>
2022-08-18 09:09:19 -0500

    Add a simple junit test, remove unnecessary .sh file, reorder deploy commands to minimize downtime

---
+++ src/test/java/TestResourceHelper.java
@@ -0,0 +1,27 @@
+package ti4;
+
+import org.junit.jupiter.api.Test;
+import ti4.ResourceHelper;
+import ti4.generator.Mapper;
+import ti4.generator.PositionMapper;
+import ti4.helpers.AliasHandler;
+import ti4.helpers.Storage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestResourceHelper {
+
+    @Test
+    public void testHelper() {
+        PositionMapper.init();
+        Mapper.init();
+        AliasHandler.init();
+        Storage.init();
+        String tileID = "18";
+        String tileName = Mapper.getTileID(tileID);
+        String tilePath = ResourceHelper.getInstance().getTileFile(tileID);
+
+        assertEquals(tileID, tileID);
+    }
+
+}
