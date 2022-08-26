fcf55ab1ec13237c94c1faaac6af1b818ccd9b39
TerTerro <terastrom@yahoo.com>
2022-08-26 14:11:35 +0300

    Bot logging to channel

---
+++ src/main/java/ti4/message/BotLogger.java
@@ -0,0 +1,29 @@
+package ti4.message;
+
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import ti4.MapGenerator;
+
+import java.util.List;
+
+public class BotLogger {
+    private static TextChannel channel;
+
+    static {
+        List<TextChannel> textChannels = MapGenerator.jda.getTextChannels();
+        for (TextChannel textChannel : textChannels) {
+            if ("bot-log".equals(textChannel.getName())) {
+                channel = textChannel;
+            }
+
+        }
+    }
+
+    public static void log(SlashCommandInteractionEvent event, String msg) {
+        if (channel != null) {
+            String channelName = event.getChannel().getName();
+            String commandString = event.getCommandString();
+            channel.sendMessage(channelName + " [" + commandString +" ] "+ msg).queue();
+        }
+    }
+}
