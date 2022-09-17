1f6104395621548552f607f27cdf98c093492840
TerTerro <terastrom@yahoo.com>
2022-09-17 22:25:45 +0300

    emote info

--- src/main/java/ti4/commands/admin/Testing.java
+++ src/main/java/ti4/commands/admin/Testing.java
@@ -2,12 +2,16 @@ package ti4.commands.admin;
 
 import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.entities.Emoji;
+import net.dv8tion.jda.api.entities.Emote;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.components.ActionRow;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import ti4.buttons.ButtonListener;
 import ti4.helpers.Constants;
+import ti4.message.BotLogger;
+
+import java.util.List;
 
 public class Testing extends AdminSubcommandData {
 
@@ -18,36 +22,41 @@ public class Testing extends AdminSubcommandData {
     @Override
     public void execute(SlashCommandInteractionEvent event) {
 
-        Emoji emoji = Emoji.fromMarkdown("\uD83D\uDEAB");
-        Emoji sabotage = Emoji.fromMarkdown(":sabotage:");
-        Emoji noSabo = Emoji.fromMarkdown(":nosabo:");
-
-
-        if (System.getenv("TESTING").equals("true")){
-            sabotage = emoji;
-            noSabo = emoji;
+        List<Emote> emotes = event.getJDA().getEmotes();
+        for (Emote emote : emotes) {
+            BotLogger.log(emote.getName() + " " + emote.getIdLong());
         }
 
-        event.getHook().sendMessage("-").queue();
-
-        Button button1 = Button.danger("sabotage", "Sabotage").withEmoji(sabotage);
-        Button button2 = Button.primary("no_sabotage", "No Sabotage").withEmoji(noSabo);
-        Button button3 = Button.danger("test_button3", "Test Button3");
-
-
-
-
-        Button button4 = Button.danger("test_button4", "Sabotage");
-        Button button5 = Button.primary("test_button5", "No Sabotage").withEmoji(sabotage);
-
-        Message message = new MessageBuilder()
-                .append("Testing message with Buttons")
-                .setActionRows(ActionRow.of(button1, button2, button3),
-                        ActionRow.of(button4, button5)).build();
-//        String id = message.getId();
-
-
-//        message.addReaction();
-        event.getChannel().sendMessage(message).queue();
+//        Emoji emoji = Emoji.fromMarkdown("\uD83D\uDEAB");
+//        Emoji sabotage = Emoji.fromMarkdown(":sabotage:");
+//        Emoji noSabo = Emoji.fromMarkdown(":nosabo:");
+//
+//
+//        if (System.getenv("TESTING").equals("true")){
+//            sabotage = emoji;
+//            noSabo = emoji;
+//        }
+//
+//        event.getHook().sendMessage("-").queue();
+//
+//        Button button1 = Button.danger("sabotage", "Sabotage").withEmoji(sabotage);
+//        Button button2 = Button.primary("no_sabotage", "No Sabotage").withEmoji(noSabo);
+//        Button button3 = Button.danger("test_button3", "Test Button3");
+//
+//
+//
+//
+//        Button button4 = Button.danger("test_button4", "Sabotage");
+//        Button button5 = Button.primary("test_button5", "No Sabotage").withEmoji(sabotage);
+//
+//        Message message = new MessageBuilder()
+//                .append("Testing message with Buttons")
+//                .setActionRows(ActionRow.of(button1, button2, button3),
+//                        ActionRow.of(button4, button5)).build();
+////        String id = message.getId();
+//
+//
+////        message.addReaction();
+//        event.getChannel().sendMessage(message).queue();
     }
 }
