9d355d6bbca9bc8fa585fa2e08f1c940ebe2516c
TerTerro <terastrom@yahoo.com>
2022-09-17 22:24:38 +0300

    Sabotage enhacments

--- src/main/java/ti4/buttons/ButtonListener.java
+++ src/main/java/ti4/buttons/ButtonListener.java
@@ -1,19 +1,58 @@
 package ti4.buttons;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Emoji;
+import net.dv8tion.jda.api.entities.Emote;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Objects;
+import ti4.helpers.Helper;
+import ti4.map.Map;
+import ti4.map.MapManager;
+import ti4.map.Player;
 
 public class ButtonListener extends ListenerAdapter {
 
     @Override
     public void onButtonInteraction(@NotNull ButtonInteractionEvent event) {
-        if (Objects.equals(event.getButton().getId(), "test_button1")){
+        String buttonID = event.getButton().getId();
 
-           event.reply("button pressed" + event.getButton().getId()).queue();
-//            event.getChannel().sendMessage("aa").queue();
+        switch (buttonID) {
+            case "sabotage" -> addReactionForSabo(event, true);
+            case "no_sabotage" -> addReactionForSabo(event, false);
+            default -> event.reply("Button " + buttonID + " pressed.").queue();
+        }
+    }
+
+    private void addReactionForSabo(@NotNull ButtonInteractionEvent event, boolean sabotage) {
+        JDA jda = event.getJDA();
+        String id = event.getUser().getId();
+        Map activeMap = MapManager.getInstance().getUserActiveMap(id);
+        Player player = Helper.getGamePlayer(activeMap, null, event.getMember(), id);
+        if (player == null) {
+            event.reply("Your not a player of the game").queue();
+            return;
+        }
+        String playerFaction = player.getFaction();
+        Emote emoteToUse = null;
+        for (Emote emote : jda.getEmotes()) {
+            if (emote.getName().contains(playerFaction)) {
+                emoteToUse = emote;
+                break;
+            }
+        }
+        if (emoteToUse == null) {
+            event.reply("Could not find faction symbol for reaction").queue();
+            return;
+        }
+        event.getChannel().addReactionById(event.getInteraction().getMessage().getId(), emoteToUse).queue();
+        if (sabotage) {
+            String text = Helper.getGamePing(event.getGuild(), activeMap);
+            text += " " + Helper.getFactionIconFromDiscord(playerFaction) + " Sabotaging Action Card Play";
+            event.reply(text).queue();
+        } else {
+            String text = Helper.getFactionIconFromDiscord(playerFaction) + " No Sabotage";
+            event.reply(text).queue();
         }
     }
 }
