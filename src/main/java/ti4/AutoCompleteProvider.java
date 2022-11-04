7879e180f630fe7363e667498e53a15c26dbaec2
Adam Best <abest@dwolla.com>
2022-11-04 13:18:14 -0500

    update the autocomplete - reinforcements instead of construction

--- src/main/java/ti4/AutoCompleteProvider.java
+++ src/main/java/ti4/AutoCompleteProvider.java
@@ -73,7 +73,7 @@ public class AutoCompleteProvider {
             }
             case Constants.CC_USE -> {
                 String enteredValue = event.getFocusedOption().getValue();
-                List<String> values = Arrays.asList("tactics", "t", "construction", "c");
+                List<String> values = Arrays.asList("tactics", "t", "reinforcements", "r");
                 List<Command.Choice> options = values.stream()
                         .filter(token -> token.contains(enteredValue))
                         .limit(25)
