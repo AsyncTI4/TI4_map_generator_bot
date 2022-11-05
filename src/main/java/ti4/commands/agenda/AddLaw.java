ac5edf4d1d163501f39eb5add155a9cecc2ab8b3
TerTerro <terastrom@yahoo.com>
2022-11-05 15:17:35 +0200

    fixes

--- src/main/java/ti4/commands/agenda/AddLaw.java
+++ src/main/java/ti4/commands/agenda/AddLaw.java
@@ -6,14 +6,17 @@ import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import ti4.generator.Mapper;
 import ti4.helpers.Constants;
+import ti4.helpers.Helper;
 import ti4.map.Map;
+import ti4.map.Player;
 import ti4.message.MessageHelper;
 
 public class AddLaw extends AgendaSubcommandData {
     public AddLaw() {
         super(Constants.ADD_LAW, "Add Agenda as Law");
         addOptions(new OptionData(OptionType.INTEGER, Constants.AGENDA_ID, "Agenda ID that is sent between ()").setRequired(true));
-        addOptions(new OptionData(OptionType.STRING, Constants.ELECTED, "Elected player or PO or anything").setRequired(false));
+        addOptions(new OptionData(OptionType.STRING, Constants.ELECTED, "Elected PO or anything").setRequired(false));
+        addOptions(new OptionData(OptionType.STRING, Constants.FACTION_COLOR, "Elected faction").setRequired(false));
     }
 
     @Override
@@ -25,12 +28,16 @@ public class AddLaw extends AgendaSubcommandData {
             return;
         }
 
+        Player player = Helper.getPlayer(activeMap, null, event);
+        player = Helper.getGamePlayer(activeMap, player, event, null);
+
         OptionMapping optionElected = event.getOption(Constants.ELECTED);
         String optionText = null;
         if (optionElected != null) {
            optionText = optionElected.getAsString();
         }
-        boolean success = activeMap.addLaw(option.getAsInt(), optionText);
+
+        boolean success = activeMap.addLaw(option.getAsInt(), player != null ? player.getFaction() : optionText);
         if (success) {
             MessageHelper.sendMessageToChannel(event.getChannel(), "Law added");
         } else {
