d56f23083b7d724c47433006f2ee8906408013d6
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2022-12-28 14:07:46 -0500

    added relic emoji

--- src/main/java/ti4/commands/explore/PurgeRelic.java
+++ src/main/java/ti4/commands/explore/PurgeRelic.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import ti4.generator.Mapper;
 import ti4.helpers.Constants;
+import ti4.helpers.Helper;
 import ti4.map.Player;
 import ti4.message.MessageHelper;
 
@@ -29,7 +30,7 @@ public class PurgeRelic extends GenericRelicAction {
             player.removeRelic(relicId);
             player.removeExhaustedRelic(relicId);
             String relicName = Mapper.getRelic(relicId).split(";")[0];
-            MessageHelper.replyToMessage(event, "Purged relic: " + relicName);
+            MessageHelper.replyToMessage(event, "Purged " + Helper.getEmojiFromDiscord("Relic") + " relic: " + relicName);
         } else {
             MessageHelper.replyToMessage(event, "Invalid relic or player does not have specified relic");
         }
