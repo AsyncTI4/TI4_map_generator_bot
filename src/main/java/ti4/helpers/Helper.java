ae7987667bb303ff49f340bf2786f2952bdd26ad
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-01-08 03:53:27 -0500

    colourized player colours

--- src/main/java/ti4/helpers/Helper.java
+++ src/main/java/ti4/helpers/Helper.java
@@ -148,6 +148,21 @@ public class Helper {
         };
     }
 
+    public static String getColourAsMention(String colour) {
+        return switch (colour) {
+            case "gray" -> "<@&1061551360870453259>";
+            case "red" -> "<@&1061551006019764274>";
+            case "purple" -> "<@&1061551337344614462>";
+            case "pink" -> "<@&1061551925218267166>";
+            case "yellow" -> "<@&1061551266213408788>";
+            case "orange" -> "<@&1061551323616657468>";
+            case "green" -> "<@&1061551347561926716>";
+            case "blue" -> "<@&1061551166397366292>";
+            case "black" -> "<@&1061551792128806962>";
+            default -> "(" + colour + ")";
+        };
+    }
+
     public static String getSCName(int sc) {
         return switch (sc) {
             case 1 -> "leadership";
@@ -385,7 +400,7 @@ public class Helper {
         StringBuilder sb = new StringBuilder(Helper.getFactionIconFromDiscord(player.getFaction()));
         sb.append(" ").append(Helper.getPlayerPing(player));
         if (player.getColor() != null) {
-            sb.append(" _(").append(player.getColor()).append(")_");
+            sb.append(" _").append(getColourAsMention(player.getColor())).append("_");
         }
         return sb.toString();
     }
