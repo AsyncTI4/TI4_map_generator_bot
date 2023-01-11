5d5b21e804194f7c62c2f6637ec5564b10f6a4b7
TerTerro <terastrom@yahoo.com>
2023-01-11 20:48:57 +0200

    card info thread 1h

--- src/main/java/ti4/commands/cards/CardsInfo.java
+++ src/main/java/ti4/commands/cards/CardsInfo.java
@@ -218,6 +218,7 @@ public class CardsInfo extends CardsSubcommandData {
                     }
                     if (!threadFound) {
                         ThreadChannel new_thread = textChannel.createThreadChannel(threadName, true)
+                        .setAutoArchiveDuration(ThreadChannel.AutoArchiveDuration.TIME_1_HOUR)
                         .setInvitable(false)
                         .complete();
                         sendTextToChannel(new_thread, playerPing + "\n");
