cfbf4912cc73ef197f432eeac33aaa75cad3baf8
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-01-20 14:03:28 -0500

    initial commit of TTPG objects

---
+++ src/main/java/ti4/map_ttpg/TTPGConfig.java
@@ -0,0 +1,84 @@
+
+package ti4.map_ttpg;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "codex1",
+    "codex2",
+    "codex3",
+    "baseMagen"
+})
+
+public class TTPGConfig {
+
+    @JsonProperty("codex1")
+    private Boolean codex1;
+    @JsonProperty("codex2")
+    private Boolean codex2;
+    @JsonProperty("codex3")
+    private Boolean codex3;
+    @JsonProperty("baseMagen")
+    private Boolean baseMagen;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("codex1")
+    public Boolean getCodex1() {
+        return codex1;
+    }
+
+    @JsonProperty("codex1")
+    public void setCodex1(Boolean codex1) {
+        this.codex1 = codex1;
+    }
+
+    @JsonProperty("codex2")
+    public Boolean getCodex2() {
+        return codex2;
+    }
+
+    @JsonProperty("codex2")
+    public void setCodex2(Boolean codex2) {
+        this.codex2 = codex2;
+    }
+
+    @JsonProperty("codex3")
+    public Boolean getCodex3() {
+        return codex3;
+    }
+
+    @JsonProperty("codex3")
+    public void setCodex3(Boolean codex3) {
+        this.codex3 = codex3;
+    }
+
+    @JsonProperty("baseMagen")
+    public Boolean getBaseMagen() {
+        return baseMagen;
+    }
+
+    @JsonProperty("baseMagen")
+    public void setBaseMagen(Boolean baseMagen) {
+        this.baseMagen = baseMagen;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
---
+++ src/main/java/ti4/map_ttpg/TTPGHexSummaryMapping.java
@@ -0,0 +1,6 @@
+package ti4.map_ttpg;
+
+//MAP This: https://github.com/TI4-Online/TI4-TTPG/blob/550da64b160893d93618d66692025fe47fe771b4/src/lib/game-data/updator-hex-summary.js
+public class TTPGHexSummaryMapping {
+    
+}
