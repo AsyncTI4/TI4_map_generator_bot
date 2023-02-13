db8f5739884477f6ca75960d145fdde6b2a74d3a
Chase Hinesman <chasehinesman@gmail.com>
2023-02-12 21:13:03 -0600

    Fix FOW filter for split map view

--- src/main/java/ti4/commands/map/ShowGame.java
+++ src/main/java/ti4/commands/map/ShowGame.java
@@ -69,17 +69,10 @@ public class ShowGame implements Command {
             } else if (temp.equals(DisplayType.stats.getValue())) {
                 displayType = DisplayType.stats;
             } else if (temp.equals(DisplayType.split.getValue())) {
-                displayType = DisplayType.map;
-                File map_file = GenerateMap.getInstance().saveImage(map, displayType, null);
+                File map_file = GenerateMap.getInstance().saveImage(map, DisplayType.map, event);
                 MessageHelper.replyToMessage(event, map_file);
 
                 displayType = DisplayType.stats;
-                File stats_file = GenerateMap.getInstance().saveImage(map, displayType, null);
-                MessageHelper.replyToMessage(event, stats_file);
-
-                displayType = DisplayType.all;
-                GenerateMap.getInstance().saveImage(map, displayType, null);
-                return;
             }
         }
         File file = GenerateMap.getInstance().saveImage(map, displayType, event);
