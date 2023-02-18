c6156a560a122ca6ae0d1d5044de3b3701106cc1
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-02-17 19:57:23 -0500

    pbd channel count

--- src/main/java/ti4/commands/bothelper/ServerLimitStats.java
+++ src/main/java/ti4/commands/bothelper/ServerLimitStats.java
@@ -4,7 +4,9 @@ import java.text.NumberFormat;
 import java.util.List;
 
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.channel.Channel;
 import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import ti4.helpers.Constants;
 import ti4.message.MessageHelper;
@@ -17,17 +19,24 @@ public class ServerLimitStats extends BothelperSubcommandData {
 
     public void execute(SlashCommandInteractionEvent event) {
         Guild guild = event.getGuild();
-        List<ThreadChannel> threadChannels = guild.getThreadChannels();
-
+        
         int memberCount = guild.getMemberCount();
         int memberMax = guild.getMaxMembers();
         int boostCount = guild.getBoostCount();
         int roleCount = guild.getRoles().size(); //250
-        int channelCount = guild.getChannels().size(); //500
+        
+        //CHANNELS
+        List<GuildChannel> channels = guild.getChannels();
+        int channelCount = channels.size(); //500
+        long pbdChannelCount = channels.stream().filter(c -> c.getName().startsWith("pbd")).count();
+        
+        //THREADS
+        List<ThreadChannel> threadChannels = guild.getThreadChannels();
         int threadCount = threadChannels.size(); //1000
         long cardsInfoThreadCount = threadChannels.stream().filter(t -> t.getName().startsWith("Cards Info")).count();
         long botThreadCount = threadChannels.stream().filter(t -> t.getName().contains("-bot-")).count();
         long scThreadCount = threadChannels.stream().filter(t -> t.getName().contains("-round-")).count();
+
         int emojiCount = guild.getEmojis().size();
         int emojiMax = guild.getMaxEmojis();
 
@@ -40,6 +49,7 @@ public class ServerLimitStats extends BothelperSubcommandData {
         sb.append(emojiCount).append(" / " + emojiMax + getPercentage(emojiCount, emojiMax) + " - emojis").append("\n");
         sb.append(roleCount).append(" / 250" + getPercentage(roleCount, 250) + " - roles").append("\n");
         sb.append(channelCount).append(" / 500" + getPercentage(channelCount, 500) + " - channels").append("\n");
+        sb.append("     - ").append(pbdChannelCount).append("   " + getPercentage(pbdChannelCount, channelCount) + " 'pbd' channels").append("\n");
         sb.append(threadCount).append(" / 1000" + getPercentage(threadCount, 1000) + " - threads").append("\n");
         sb.append("     - ").append(cardsInfoThreadCount).append("   " + getPercentage(cardsInfoThreadCount, threadCount) + " 'Cards Info' threads (/ac info)").append("\n");
         sb.append("     - ").append(botThreadCount).append("   " + getPercentage(botThreadCount, threadCount) + "'-bot' threads").append("\n");
