8b4fb6c9e9374f3536b1d77b4f138741b703ec66
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-02-21 14:22:35 -0500

    replace existing logic with getLeaderRepresentation

--- src/main/java/ti4/commands/leaders/ExhaustLeader.java
+++ src/main/java/ti4/commands/leaders/ExhaustLeader.java
@@ -31,8 +31,7 @@ public class ExhaustLeader extends LeaderAction {
             MessageHelper.sendMessageToChannel(event.getChannel(), Helper.getFactionLeaderEmoji(player, playerLeader));
             StringBuilder message = new StringBuilder(Helper.getPlayerRepresentation(event, player))
                     .append(" exhausted ")
-                    .append(playerLeader.getId()).append(" ")
-                    .append(playerLeader.getName());
+                    .append(Helper.getLeaderRepresentation(player.getFaction(), playerLeader,true,true));
             OptionMapping optionTG = event.getOption(Constants.TG);
             if (optionTG != null) {
                 Stats.setValue(event, player, optionTG, playerLeader::setTgCount, playerLeader::getTgCount);
