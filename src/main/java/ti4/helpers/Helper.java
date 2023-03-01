a28bc39be224c2e6c8b7899babf7319cbd4b4698
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-02-28 23:55:34 -0500

    Auto-archive fix

--- src/main/java/ti4/helpers/Helper.java
+++ src/main/java/ti4/helpers/Helper.java
@@ -949,7 +949,7 @@ public class Helper {
 
     public static void checkThreadLimitAndArchive(Guild guild) {
         int threadCount = guild.getThreadChannels().size();
-        int closeCount = 25;
+        int closeCount = 20;
 
         if (threadCount >= 980) {
             BotLogger.log("`Helper.checkThreadLimitAndArchive:` Thread count is too high ( " + threadCount + " ) - auto-archiving  " + closeCount + " threads:");
