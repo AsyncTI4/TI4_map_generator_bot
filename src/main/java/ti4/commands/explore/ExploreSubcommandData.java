eb3342d8e92a50ecdcd6d285f74d29edec4f98b7
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-03-07 19:59:13 -0500

    /explore message improvement stub

--- src/main/java/ti4/commands/explore/ExploreSubcommandData.java
+++ src/main/java/ti4/commands/explore/ExploreSubcommandData.java
@@ -18,8 +18,10 @@ import ti4.message.MessageHelper;
 
 public abstract class ExploreSubcommandData extends SubcommandData {
 
+    private SlashCommandInteractionEvent event;
     private Map activeMap;
     private User user;
+    private boolean replyHasBeenEdited;
     protected final OptionData typeOption = new OptionData(OptionType.STRING, Constants.TRAIT, "Cultural, Industrial, Hazardous, or Frontier.").setAutoComplete(true);
     protected final OptionData idOption = new OptionData(OptionType.STRING, Constants.EXPLORE_CARD_ID, "Explore card id sent between (). Can include multiple comma-separated ids.");
 
@@ -38,10 +40,28 @@ public abstract class ExploreSubcommandData extends SubcommandData {
     public User getUser() {
         return user;
     }
+    
+    /**
+     * Edits the original message after submitting a slash command
+     * @param messageText new message
+     */
+    public void sendMessage(String messageText) {
+        if (this.replyHasBeenEdited) {
+            MessageHelper.sendMessageToChannel(this.event.getChannel(), messageText);
+        } else if (messageText.length() >= 2000) {
+            this.event.getHook().editOriginal("_ _").queue();
+            MessageHelper.sendMessageToChannel(this.event.getChannel(), messageText);
+        } else {
+            this.event.getHook().editOriginal(messageText).queue();
+            this.replyHasBeenEdited = true;
+        }
+    }
 
     abstract public void execute(SlashCommandInteractionEvent event);
 
     public void preExecute(SlashCommandInteractionEvent event) {
+        this.event = event;
+        replyHasBeenEdited = false;
         user = event.getUser();
         activeMap = MapManager.getInstance().getUserActiveMap(user.getId());
     }
