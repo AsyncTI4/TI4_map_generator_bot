56ae849a64698b9b9ca0506d9f3a909804d6eefd
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-03-08 15:57:15 -0500

    /status message improvement stub

--- src/main/java/ti4/commands/status/StatusSubcommandData.java
+++ src/main/java/ti4/commands/status/StatusSubcommandData.java
@@ -4,18 +4,16 @@ import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 import org.jetbrains.annotations.NotNull;
-import ti4.generator.GenerateMap;
 import ti4.map.Map;
 import ti4.map.MapManager;
-import ti4.map.MapSaveLoadManager;
 import ti4.message.MessageHelper;
-
-import java.io.File;
 
 public abstract class StatusSubcommandData extends SubcommandData {
 
+    private SlashCommandInteractionEvent event;
     private Map activeMap;
     private User user;
+    private boolean replyHasBeenEdited;
 
     public String getActionID() {
         return getName();
@@ -33,9 +31,27 @@ public abstract class StatusSubcommandData extends SubcommandData {
         return user;
     }
 
+    /**
+     * Edits the original message after submitting a slash command
+     * @param messageText new message
+     */
+    public void sendMessage(String messageText) {
+        if (this.replyHasBeenEdited) {
+            MessageHelper.sendMessageToChannel(this.event.getChannel(), messageText);
+        } else if (messageText.length() >= 2000) {
+            this.event.getHook().editOriginal("_ _").queue();
+            MessageHelper.sendMessageToChannel(this.event.getChannel(), messageText);
+        } else {
+            this.event.getHook().editOriginal(messageText).queue();
+            this.replyHasBeenEdited = true;
+        }
+    }
+
     abstract public void execute(SlashCommandInteractionEvent event);
 
     public void preExecute(SlashCommandInteractionEvent event) {
+        this.event = event;
+        replyHasBeenEdited = false;
         user = event.getUser();
         activeMap = MapManager.getInstance().getUserActiveMap(user.getId());
     }
