4825f6e2e07beb4f645b34e2bd0f7e21b0c6e9c6
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-03-09 11:50:32 -0500

    disable botlogger on splitAndSent

--- src/main/java/ti4/message/MessageHelper.java
+++ src/main/java/ti4/message/MessageHelper.java
@@ -119,7 +119,7 @@ public class MessageHelper {
 
     private static void splitAndSent(String messageText, MessageChannel channel, SlashCommandInteractionEvent event, Boolean pinMessages, String... reaction) {
         if (messageText == null || channel == null) {
-            BotLogger.log("`splitAndSent` - `messageText` or `channel` was null");
+            // BotLogger.log("`splitAndSent` - `messageText` or `channel` was null");
             return;
         }
 
@@ -174,7 +174,7 @@ public class MessageHelper {
 
     private static void  splitAndSent(String messageText, MessageChannel channel, SlashCommandInteractionEvent event, Guild guild, Button... buttons) {
         if (messageText == null || channel == null) {
-            BotLogger.log("`splitAndSent` - `messageText` or `channel` was null");
+            // BotLogger.log("`splitAndSent` - `messageText` or `channel` was null");
             return;
         }
         
