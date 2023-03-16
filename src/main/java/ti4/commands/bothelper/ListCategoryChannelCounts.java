ac7b0883a8a62d045826481986cb83a0d49e94fb
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-03-16 11:25:06 -0400

    bothelper category channel counts

---
+++ src/main/java/ti4/commands/bothelper/ListCategoryChannelCounts.java
@@ -0,0 +1,37 @@
+package ti4.commands.bothelper;
+
+import java.text.NumberFormat;
+import java.util.List;
+
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.channel.concrete.Category;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import ti4.helpers.Constants;
+
+public class ListCategoryChannelCounts extends BothelperSubcommandData {
+    public ListCategoryChannelCounts(){
+        super(Constants.CATEGORY_CHANNEL_COUNT, "List all categories and their channel counts.");
+    }
+    
+    public void execute(SlashCommandInteractionEvent event) {
+        Guild guild = event.getGuild();
+
+        StringBuilder sb = new StringBuilder("**__Categories:__**\n");
+
+        int maxChannels = 50;
+        List<Category> categories = guild.getCategories();
+        for (Category category : categories) {
+            int channelCount = category.getChannels().size();
+            sb.append("> **" + category.getName() + "**: " + channelCount + "/" + maxChannels + getPercentage(channelCount, maxChannels)).append("\n");
+        }
+        sendMessage(sb.toString());
+    }
+    
+    private String getPercentage(double numerator, double denominator) {
+        NumberFormat formatPercent = NumberFormat.getPercentInstance();
+        // formatPercent.setMinimumFractionDigits(1);
+        String formatted = formatPercent.format(denominator == 0 ? 0.0 : (numerator / denominator));
+        formatted = " *(" + formatted + ")* ";
+        return formatted;
+    }
+}
