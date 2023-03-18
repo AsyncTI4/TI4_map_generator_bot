35ba91f8bb05dbc1b51c2c029cd81781590a3217
Brendan Speer <brendan.speer@gmail.com>
2023-03-17 22:21:41 -0500

    * Retry for AdminCommand for GlobalSettings

--- src/main/java/ti4/helpers/Constants.java
+++ src/main/java/ti4/helpers/Constants.java
@@ -36,6 +36,10 @@ public class Constants {
     public static final String LEAVE = "leave";
     public static final String REMOVE = "remove";
 
+    public static final String SET_SETTING = "setting_name";
+    public static final String SETTING_NAME = "setting_name";
+    public static final String SETTING_VALUE = "setting_value";
+
     public static String ALPHA = "alpha";
     public static String BETA = "beta";
     public static String GAMMA = "gamma";
