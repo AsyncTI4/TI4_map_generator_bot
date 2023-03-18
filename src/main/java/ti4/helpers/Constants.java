aef8de4267f523fac730adade310e35dad381d73
Brendan Speer <brendan.speer@gmail.com>
2023-03-17 21:40:33 -0500

    Still Broken :-o :-o

--- src/main/java/ti4/helpers/Constants.java
+++ src/main/java/ti4/helpers/Constants.java
@@ -36,9 +36,8 @@ public class Constants {
     public static final String LEAVE = "leave";
     public static final String REMOVE = "remove";
 
-    public static final String SETTING_NAME = "setting_name";
-    public static final String SETTING_VALUE = "setting_value";
-    
+    //public static final String SETTING_NAME = "setting_name";
+    //public static final String SETTING_VALUE = "setting_value";
     public static String ALPHA = "alpha";
     public static String BETA = "beta";
     public static String GAMMA = "gamma";
