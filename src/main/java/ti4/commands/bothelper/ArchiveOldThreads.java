7174dacb6a3f976e8f3e9104bab536fcc193d7a0
Brendan Speer <brendan.speer@gmail.com>
2023-03-26 13:53:37 -0500

    * WOrking on the bot.... do de do

--- src/main/java/ti4/commands/bothelper/ArchiveOldThreads.java
+++ src/main/java/ti4/commands/bothelper/ArchiveOldThreads.java
@@ -8,6 +8,7 @@ import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEve
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import ti4.helpers.Constants;
+import ti4.helpers.LoggerHandler;
 
 public class ArchiveOldThreads extends BothelperSubcommandData {
     public ArchiveOldThreads(){
@@ -24,38 +25,25 @@ public class ArchiveOldThreads extends BothelperSubcommandData {
         sendMessage("Archiving " + threadCount + " threads");
         sendMessage(ListOldChannels.getOldThreadsMessage(event.getGuild(), threadCount));
 
-        List<ThreadChannel> threadChannelsBlank = event.getGuild().getThreadChannels();
-
-        threadChannelsBlank = threadChannelsBlank.stream()
-                .filter(c -> c.getLatestMessageIdLong() == 0)
-                .toList();
-        for (ThreadChannel threadChannel : threadChannelsBlank) {
-            threadChannel.getManager().setArchived(true).queue();
-        }
-
-        sendMessage("Blank Channels:" + threadChannelsBlank.size());
         archiveOldThreads(event.getGuild(), threadCount);
     }
 
     public static void archiveOldThreads(Guild guild, Integer threadCount) {
         List<ThreadChannel> threadChannels = guild.getThreadChannels();
-        List<ThreadChannel> threadChannelsBlank = guild.getThreadChannels();
 
-        threadChannelsBlank = threadChannelsBlank.stream()
-            .filter(c -> c.getLatestMessageIdLong() == 0)
-            .toList();
-        for (ThreadChannel threadChannel : threadChannelsBlank) {
-            threadChannel.getManager().setArchived(true).queue();
-        }
-        
         threadChannels = threadChannels.stream()
             .filter(c -> c.getLatestMessageIdLong() != 0)
             .sorted((object1, object2) -> object1.getLatestMessageId().compareTo(object2.getLatestMessageId()))
             .limit(threadCount)
             .toList();
-    
+
         for (ThreadChannel threadChannel : threadChannels) {
-            threadChannel.getManager().setArchived(true).queue();
+            threadChannel.getManager().setArchived(true)
+                .onErrorMap((e) -> {
+                        LoggerHandler.logError("Error map error:");
+                        return null;
+                        })
+                .queue();
         }
     }
 }
