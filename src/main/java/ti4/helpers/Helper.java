e01e3aa658e584730b7e8867fd7a7f2193796070
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-03-27 11:10:23 -0400

    fix for auto-unlock hero when hero already unlocked or purged

--- src/main/java/ti4/helpers/Helper.java
+++ src/main/java/ti4/helpers/Helper.java
@@ -1246,40 +1246,46 @@ public class Helper {
     }
 
     public static void checkIfHeroUnlocked(SlashCommandInteractionEvent event, Map activeMap, Player player) {
-        int scoredSOCount = player.getSecretsScored().size();
-        int scoredPOCount = 0;
-        HashMap<String, List<String>> playerScoredPublics = activeMap.getScoredPublicObjectives();
-        for (Entry<String, List<String>> scoredPublic : playerScoredPublics.entrySet()) {
-            if (Mapper.getPublicObjectivesState1().keySet().contains(scoredPublic.getKey()) || Mapper.getPublicObjectivesState2().keySet().contains(scoredPublic.getKey())) {
-                if (scoredPublic.getValue().contains(player.getUserID())) {
-                    scoredPOCount++;
+        Leader playerLeader = player.getLeader(Constants.HERO);
+        if (playerLeader != null && playerLeader.isLocked()) {
+            int scoredSOCount = player.getSecretsScored().size();
+            int scoredPOCount = 0;
+            HashMap<String, List<String>> playerScoredPublics = activeMap.getScoredPublicObjectives();
+            for (Entry<String, List<String>> scoredPublic : playerScoredPublics.entrySet()) {
+                if (Mapper.getPublicObjectivesState1().keySet().contains(scoredPublic.getKey()) || Mapper.getPublicObjectivesState2().keySet().contains(scoredPublic.getKey())) {
+                    if (scoredPublic.getValue().contains(player.getUserID())) {
+                        scoredPOCount++;
+                    }
                 }
+            
             }
-        
-        }
-        int scoredObjectiveCount = scoredPOCount + scoredSOCount;
-        if (scoredObjectiveCount >= 3) {
-            UnlockLeader ul = new UnlockLeader();
-            ul.unlockLeader(event, "hero", activeMap, player);
+            int scoredObjectiveCount = scoredPOCount + scoredSOCount;
+            if (scoredObjectiveCount >= 3) {
+                UnlockLeader ul = new UnlockLeader();
+                ul.unlockLeader(event, "hero", activeMap, player);
+            }
         }
     }
 
     public static void checkIfHeroUnlocked(ButtonInteractionEvent event, Map activeMap, Player player) {
-        int scoredSOCount = player.getSecretsScored().size();
-        int scoredPOCount = 0;
-        HashMap<String, List<String>> playerScoredPublics = activeMap.getScoredPublicObjectives();
-        for (Entry<String, List<String>> scoredPublic : playerScoredPublics.entrySet()) {
-            if (Mapper.getPublicObjectivesState1().keySet().contains(scoredPublic.getKey()) || Mapper.getPublicObjectivesState2().keySet().contains(scoredPublic.getKey())) {
-                if (scoredPublic.getValue().contains(player.getUserID())) {
-                    scoredPOCount++;
+        Leader playerLeader = player.getLeader(Constants.HERO);
+        if (playerLeader != null && playerLeader.isLocked()) {
+            int scoredSOCount = player.getSecretsScored().size();
+            int scoredPOCount = 0;
+            HashMap<String, List<String>> playerScoredPublics = activeMap.getScoredPublicObjectives();
+            for (Entry<String, List<String>> scoredPublic : playerScoredPublics.entrySet()) {
+                if (Mapper.getPublicObjectivesState1().keySet().contains(scoredPublic.getKey()) || Mapper.getPublicObjectivesState2().keySet().contains(scoredPublic.getKey())) {
+                    if (scoredPublic.getValue().contains(player.getUserID())) {
+                        scoredPOCount++;
+                    }
                 }
+            
             }
-        
-        }
-        int scoredObjectiveCount = scoredPOCount + scoredSOCount;
-        if (scoredObjectiveCount >= 3) {
-            UnlockLeader ul = new UnlockLeader();
-            ul.unlockLeader(event, "hero", activeMap, player);
+            int scoredObjectiveCount = scoredPOCount + scoredSOCount;
+            if (scoredObjectiveCount >= 3) {
+                UnlockLeader ul = new UnlockLeader();
+                ul.unlockLeader(event, "hero", activeMap, player);
+            }
         }
     }
 }
