8a6b9cdf56816ebacce24e5ac76932669cff66a3
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-04-03 10:46:46 -0400

    Agenda Representation (#214)
    Fixes #184 
    * pull agendaRepresentation into Helper method
    
    * add agendaRepresentation to other /agenda commands

--- src/main/java/ti4/commands/agenda/RevealAgenda.java
+++ src/main/java/ti4/commands/agenda/RevealAgenda.java
@@ -6,12 +6,10 @@ import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
 
-import ti4.generator.Mapper;
 import ti4.helpers.Constants;
 import ti4.helpers.Emojis;
 import ti4.helpers.Helper;
 import ti4.map.Map;
-import ti4.message.BotLogger;
 import ti4.message.MessageHelper;
 
 import java.util.LinkedHashMap;
@@ -32,52 +30,11 @@ public class RevealAgenda extends AgendaSubcommandData {
             revealFromBottom = revealFromBottomOption.getAsBoolean();
         }
 
-        StringBuilder sb = new StringBuilder();
         String id = activeMap.revealAgenda(revealFromBottom);
         LinkedHashMap<String, Integer> discardAgendas = activeMap.getDiscardAgendas();
         Integer uniqueID = discardAgendas.get(id);
-        String[] agendaDetails = Mapper.getAgenda(id).split(";");
-        String agendaName = agendaDetails[0];
-        String agendaType = agendaDetails[1];
-        String agendaTarget = agendaDetails[2];
-        String arg1 = agendaDetails[3];
-        String arg2 = agendaDetails[4];
-        String agendaSource = agendaDetails[5];
-
-        if (agendaName == null || agendaType == null || agendaTarget == null || arg1 == null || arg2 == null || agendaSource == null) {
-            BotLogger.log("Agenda improperly formatted: " + id);
-            sb.append("Agenda ----------\n").append(Mapper.getAgenda(id)).append("\n------------------");
-        } else {
-            sb.append("**__");
-            if (uniqueID != null) {
-                sb.append("(").append(uniqueID).append(") - ");
-            }
-            sb.append(agendaName).append("__** ");
-            switch (agendaSource) {
-                case "absol" -> sb.append(Emojis.Absol);
-                case "PoK" -> sb.append(Emojis.AgendaWhite);
-                default -> sb.append(Emojis.AsyncTI4Logo);
-            }
-            sb.append("\n");
-
-            sb.append("> **").append(agendaType).append(":** *").append(agendaTarget).append("*\n");
-            if (arg1.length() > 0) {
-                arg1 = arg1.replace("For:", "**For:**");
-                sb.append("> ").append(arg1).append("\n");
-            }
-            if (arg2.length() > 0) {
-                arg2 = arg2.replace("Against:", "**Against:**");
-                sb.append("> ").append(arg2).append("\n");
-            }
-        }
-
-        switch (id) {
-            case ("mutiny") -> sb.append("Use this command to add the objective: `/status po_add_custom public_name:Mutiny public_vp_worth:1`").append("\n");
-            case ("seed_empire") -> sb.append("Use this command to add the objective: `/status po_add_custom public_name:Seed of an Empire public_vp_worth:1`").append("\n");
-            case ("censure") -> sb.append("Use this command to add the objective: `/status po_add_custom public_name:Political Censure public_vp_worth:1`").append("\n");
-        }
-
-        MessageHelper.sendMessageToChannel(event, sb.toString());
+        
+        MessageHelper.sendMessageToChannel(event, Helper.getAgendaRepresentation(id, uniqueID));
         String text = Helper.getGamePing(event, activeMap) + " Please indicate whether you will **Play a When** or **Play an After** or not by pressing the buttons below:";
 
         Button playWhen = Button.danger("play_when", "Play When");
