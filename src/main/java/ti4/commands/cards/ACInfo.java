7792cf5c546895b2e6f36a320f3f592dd7fd13d3
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-04-04 22:43:00 -0400

    add /ac info2 as temporary command until /ac info can be replaced not working yet, done work for the night

---
+++ src/main/java/ti4/commands/cards/ACInfo.java
@@ -0,0 +1,77 @@
+package ti4.commands.cards;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.utils.messages.MessageCreateData;
+import ti4.generator.Mapper;
+import ti4.helpers.Constants;
+import ti4.helpers.Emojis;
+import ti4.helpers.Helper;
+import ti4.map.Map;
+import ti4.map.Player;
+import ti4.message.MessageHelper;
+
+public class ACInfo extends CardsSubcommandData {
+    public ACInfo() {
+        super(Constants.INFO + 2, "Send Action Cards to your Cards Info thread");
+    }
+
+    @Override
+    public void execute(SlashCommandInteractionEvent event) {
+        Map activeMap = getActiveMap();
+        Player player = activeMap.getPlayer(getUser().getId());
+        player = Helper.getGamePlayer(activeMap, player, event, null);
+        if (player == null) {
+            sendMessage("Player could not be found");
+            return;
+        }
+        MessageHelper.sendMessageToPlayerCardsInfoThread(player, activeMap, Helper.getPlayerRepresentation(event, player));
+        sendActionCardInfo(activeMap, player);
+        sendMessage("AC Info Sent");
+    }
+
+    public static void sendActionCardInfo(Map activeMap, Player player) {
+        //CARDS INFO
+        MessageHelper.sendMessageToPlayerCardsInfoThread(player, activeMap, getActionCardInfo(activeMap, player));
+
+        //BUTTONS
+        String secretScoreMsg = "_ _\nClick a button below to play an Action Card";
+        List<Button> acButtons = getPlayActionCardButtons(activeMap, player);
+        List<MessageCreateData> messageList = MessageHelper.getMessageObject(secretScoreMsg, acButtons);
+        ThreadChannel cardsInfoThreadChannel = Helper.getPlayerCardsInfoThread(activeMap, player);
+        for (MessageCreateData message : messageList) {
+            cardsInfoThreadChannel.sendMessage(message).queue();
+        }
+    } 
+
+    private static String getActionCardInfo(Map activeMap, Player player) {
+        StringBuilder sb = new StringBuilder();
+        int index = 1;
+        sb.append("\n");
+
+        return sb.toString();
+    }
+
+    private static List<Button> getPlayActionCardButtons(Map activeMap, Player player) {
+        List<Button> acButtons = new ArrayList<>();
+        LinkedHashMap<String, Integer> actionCards = player.getActionCards();
+        if (actionCards != null) {
+            for (java.util.Map.Entry<String, Integer> ac : actionCards.entrySet()) {
+                Integer value = ac.getValue();
+                String key = ac.getKey();
+                String ac_name = Mapper.getActionCardName(key);
+                if (ac_name != null) {
+                    acButtons.add(Button.danger(Constants.AC_PLAY_FROM_HAND + value, "(" + value + ") " + ac_name).withEmoji(Emoji.fromFormatted(Emojis.ActionCard)));
+                }
+            }
+        }
+        return acButtons;
+    }
+}
