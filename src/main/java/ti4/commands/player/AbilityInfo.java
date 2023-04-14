89e7802f441d02a4b4b4149ed804873b780cc93d
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-04-14 09:56:43 -0400

    DS Abilities (#249)
    * add faction symbol to ability info
    
    * started adding DS abilities and PoK faction
    
    * finish adding PoK factions
    
    * autocomplete improvements
    
    * fix for tile d24/d34
    
    * sorted DS abilities by faction
    
    * add DS abilities to faction setup
    
    * spelling

--- src/main/java/ti4/commands/player/AbilityInfo.java
+++ src/main/java/ti4/commands/player/AbilityInfo.java
@@ -53,18 +53,19 @@ public class AbilityInfo extends PlayerSubcommandData {
         HashMap<String, String> abilityInfo = Mapper.getFactionAbilities();
         String abilityRawText = abilityInfo.get(abilityID);
         StringTokenizer tokenizer = new StringTokenizer(abilityRawText, "|");
-        int expectedTokenCount = 4;
+        int expectedTokenCount = 5;
         if (tokenizer.countTokens() != expectedTokenCount) {
             BotLogger.log("Ability info raw text is incorrectly formatted (needs " + (expectedTokenCount - 1) + " | to split properly):\n> " + abilityRawText);
             return abilityRawText;
         }
         String abilityName = tokenizer.nextToken();
+        String abilitySourceFaction = tokenizer.nextToken();
         String abilityRawModifier = tokenizer.nextToken();
         String abilityWindow = tokenizer.nextToken();
         String abilityText = tokenizer.nextToken();
 
         StringBuilder sb = new StringBuilder();
-        sb.append("__**").append(abilityName).append("**__");
+        sb.append(Helper.getFactionIconFromDiscord(abilitySourceFaction)).append("__**").append(abilityName).append("**__");
         if (!abilityRawModifier.isBlank()) sb.append(": ").append(abilityRawModifier);
         if (!abilityWindow.isBlank() || !abilityText.isBlank()) sb.append("\n> *").append(abilityWindow).append("*:\n> ").append(abilityText);
 
