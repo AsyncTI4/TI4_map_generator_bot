eed5ae991b6fb9a205255891a89e2a6438830f83
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-04-20 16:26:34 -0400

    hide FoW from list_my_games

--- src/main/java/ti4/commands/help/ListMyGames.java
+++ src/main/java/ti4/commands/help/ListMyGames.java
@@ -43,7 +43,7 @@ public class ListMyGames extends HelpSubcommandData {
         int index = 1;
         StringBuilder sb = new StringBuilder("**__").append(user.getName()).append("'s Games__**\n");
         for (Map map : maps) {
-            sb.append(Helper.rightpad("`" + index + ".`", 6)).append(getPlayerMapListRepresentation(map, userID, event)).append("\n");
+            sb.append(Helper.rightpad("`" + index + ".`", 6)).append(getPlayerMapListRepresentation(map, userID, event));
             index++;
         }
         MessageHelper.sendMessageToChannel(event.getMessageChannel(), sb.toString());
@@ -51,12 +51,13 @@ public class ListMyGames extends HelpSubcommandData {
 
     private String getPlayerMapListRepresentation(Map map, String userID, GenericInteractionCreateEvent event) {
         Player player = map.getPlayer(userID);
-        if (player == null) return "fail";
+        if (player == null || map.isFoWMode()) return "";
         StringBuilder sb = new StringBuilder();
         sb.append("**").append(map.getName()).append("**:  ");
         sb.append(Helper.getFactionIconFromDiscord(player.getFaction())).append(Helper.getColourAsMention(event.getGuild(), player.getColor())).append(" ").append(map.getActionChannel() == null ? "" : map.getActionChannel().getAsMention());
         if (map.getActivePlayer() == null ? false : map.getActivePlayer().equals(userID)) sb.append(" - **__IT IS YOUR TURN__**");
         if (map.isHasEnded()) sb.append(" - GAME HAS ENDED");
+        sb.append("\n");
         return sb.toString();
     }
 
