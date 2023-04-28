b7fc5b5dc3305e76f33080edf1d9cd8fdabe8780
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-04-28 15:41:03 -0400

    fix /sc_unpick, will bring it back around to strat phase

--- src/main/java/ti4/commands/player/SCUnpick.java
+++ src/main/java/ti4/commands/player/SCUnpick.java
@@ -54,92 +54,92 @@ public class SCUnpick extends PlayerSubcommandData {
             return;
         }
 
-        return; //force end this for now, let players manage next to pick until look at this again
+        // return; //force end this for now, let players manage next to pick until look at this again
 
-        // String msg = "";
-        // String msgExtra = "";
-        // boolean allPicked = true;
-        // Player privatePlayer = null;
+        String msg = "";
+        String msgExtra = "";
+        boolean allPicked = true;
+        Player privatePlayer = null;
         
-        // boolean nextCorrectPing = false;
-        // Queue<Player> players = new ArrayDeque<>(activePlayers);
-        // while (players.iterator().hasNext()) {
-        //     Player player_ = players.poll();
-        //     if (player_ == null || !player_.isRealPlayer()) {
-        //         continue;
-        //     }
-        //     int player_SCCount = player_.getSCs().size();
-        //     if (nextCorrectPing && player_SCCount < maxSCsPerPlayer && player_.getFaction() != null) {
-        //         msgExtra += Helper.getPlayerRepresentation(event, player_, true) + " To Pick SC";
-        //         privatePlayer = player_;
-        //         allPicked = false;
-        //         break;
-        //     }
-        //     if (player_ == player) {
-        //         nextCorrectPing = true;
-        //     }
-        //     if (player_SCCount < maxSCsPerPlayer && player_.getFaction() != null) {
-        //         players.add(player_);
-        //     }
-        // }
+        boolean nextCorrectPing = false;
+        Queue<Player> players = new ArrayDeque<>(activePlayers);
+        while (players.iterator().hasNext()) {
+            Player player_ = players.poll();
+            if (player_ == null || !player_.isRealPlayer()) {
+                continue;
+            }
+            int player_SCCount = player_.getSCs().size();
+            if (nextCorrectPing && player_SCCount < maxSCsPerPlayer && player_.getFaction() != null) {
+                msgExtra += Helper.getPlayerRepresentation(event, player_, true) + " To Pick SC";
+                privatePlayer = player_;
+                allPicked = false;
+                break;
+            }
+            if (player_ == player) {
+                nextCorrectPing = true;
+            }
+            if (player_SCCount < maxSCsPerPlayer && player_.getFaction() != null) {
+                players.add(player_);
+            }
+        }
 
-        // //INFORM ALL PLAYER HAVE PICKED
-        // if (allPicked) {
-        //     msgExtra += Helper.getGamePing(event, activeMap) + "\nAll players picked SC";
+        //INFORM ALL PLAYER HAVE PICKED
+        if (allPicked) {
+            msgExtra += Helper.getGamePing(event, activeMap) + "\nAll players picked SC";
 
-        //     LinkedHashMap<Integer, Integer> scTradeGoods = activeMap.getScTradeGoods();
-        //     Set<Integer> scPickedList = new HashSet<>();
-        //     for (Player player_ : activePlayers) {
-        //         scPickedList.addAll(player_.getSCs());
-        //     }
+            LinkedHashMap<Integer, Integer> scTradeGoods = activeMap.getScTradeGoods();
+            Set<Integer> scPickedList = new HashSet<>();
+            for (Player player_ : activePlayers) {
+                scPickedList.addAll(player_.getSCs());
+            }
             
-        //     //ADD A TG TO UNPICKED SC
-        //     for (Integer scNumber : scTradeGoods.keySet()) {
-        //         if (!scPickedList.contains(scNumber) && scNumber != 0) {
-        //             Integer tgCount = scTradeGoods.get(scNumber);
-        //             tgCount = tgCount == null ? 1 : tgCount + 1;
-        //             activeMap.setScTradeGood(scNumber, tgCount);
-        //         }
-        //     }
+            //ADD A TG TO UNPICKED SC
+            for (Integer scNumber : scTradeGoods.keySet()) {
+                if (!scPickedList.contains(scNumber) && scNumber != 0) {
+                    Integer tgCount = scTradeGoods.get(scNumber);
+                    tgCount = tgCount == null ? 1 : tgCount + 1;
+                    activeMap.setScTradeGood(scNumber, tgCount);
+                }
+            }
 
-        //     Player nextPlayer = null;
-        //     int lowestSC = 100;
-        //     for (Player player_ : activePlayers) {
-        //         int playersLowestSC = player_.getLowestSC();
-        //         String scNumberIfNaaluInPlay = GenerateMap.getSCNumberIfNaaluInPlay(player_, activeMap, Integer.toString(playersLowestSC));
-        //         if (scNumberIfNaaluInPlay.startsWith("0/")) {
-        //             nextPlayer = player_; //no further processing, this player has the 0 token
-        //             break;
-        //         }
-        //         if (playersLowestSC < lowestSC) {
-        //             lowestSC = playersLowestSC;
-        //             nextPlayer = player_;
-        //         }
-        //     }
+            Player nextPlayer = null;
+            int lowestSC = 100;
+            for (Player player_ : activePlayers) {
+                int playersLowestSC = player_.getLowestSC();
+                String scNumberIfNaaluInPlay = GenerateMap.getSCNumberIfNaaluInPlay(player_, activeMap, Integer.toString(playersLowestSC));
+                if (scNumberIfNaaluInPlay.startsWith("0/")) {
+                    nextPlayer = player_; //no further processing, this player has the 0 token
+                    break;
+                }
+                if (playersLowestSC < lowestSC) {
+                    lowestSC = playersLowestSC;
+                    nextPlayer = player_;
+                }
+            }
 
-        //     //INFORM FIRST PLAYER IS UP FOR ACTION
-        //     if (nextPlayer != null) {
-        //         msgExtra += " " + Helper.getPlayerRepresentation(event, nextPlayer) + " is up for an action";
-        //         privatePlayer = nextPlayer;
-        //         activeMap.updateActivePlayer(nextPlayer);
-        //     }
-        // }
+            //INFORM FIRST PLAYER IS UP FOR ACTION
+            if (nextPlayer != null) {
+                msgExtra += " " + Helper.getPlayerRepresentation(event, nextPlayer) + " is up for an action";
+                privatePlayer = nextPlayer;
+                activeMap.updateActivePlayer(nextPlayer);
+            }
+        }
 
-        // //SEND EXTRA MESSAGE
-        // if (isFowPrivateGame ) {
-        //     if (allPicked) {
-        //         msgExtra = Helper.getPlayerRepresentation(event, privatePlayer, true) + " UP NEXT";
-        //     }
-        //     String fail = "User for next faction not found. Report to ADMIN";
-        //     String success = "The next player has been notified";
-        //     MessageHelper.sendPrivateMessageToPlayer(privatePlayer, activeMap, event, msgExtra, fail, success);
-        // } else {
-        //     if (allPicked) {
-        //         ListTurnOrder.turnOrder(event, activeMap);
-        //     }
-        //     if (!msgExtra.isEmpty()) {
-        //         sendMessage(msgExtra);
-        //     }
-        // }
+        //SEND EXTRA MESSAGE
+        if (isFowPrivateGame ) {
+            if (allPicked) {
+                msgExtra = Helper.getPlayerRepresentation(event, privatePlayer, true) + " UP NEXT";
+            }
+            String fail = "User for next faction not found. Report to ADMIN";
+            String success = "The next player has been notified";
+            MessageHelper.sendPrivateMessageToPlayer(privatePlayer, activeMap, event, msgExtra, fail, success);
+        } else {
+            if (allPicked) {
+                ListTurnOrder.turnOrder(event, activeMap);
+            }
+            if (!msgExtra.isEmpty()) {
+                sendMessage(msgExtra);
+            }
+        }
     }
 }
