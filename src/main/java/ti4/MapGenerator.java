cddcdf1b8ef62939264e1a131f39457b7f76406a
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-05-01 15:02:34 -0400

    setup, not functioning

--- src/main/java/ti4/MapGenerator.java
+++ src/main/java/ti4/MapGenerator.java
@@ -131,6 +131,7 @@ public class MapGenerator {
         // commandManager.addCommand(new AddControl()); //disabled due to confusing players - /player planet_add should be used
         // commandManager.addCommand(new RemoveControl()); //disabled due to confusing players - /player planet_remove should be used
         commandManager.addCommand(new MoveUnits());
+        commandManager.addCommand(new MoveUnits_Beta());
         commandManager.addCommand(new RemoveToken());
         commandManager.addCommand(new AddToken());
         commandManager.addCommand(new AddUnitDamage());
