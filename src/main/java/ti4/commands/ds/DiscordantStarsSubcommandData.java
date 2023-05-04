0cf719863fafa10b3712b0fa51680dc3b2b36fe5
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-05-04 16:51:50 -0400

    add DS command

---
+++ src/main/java/ti4/commands/ds/DiscordantStarsSubcommandData.java
@@ -0,0 +1,53 @@
+package ti4.commands.ds;
+
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+import org.jetbrains.annotations.NotNull;
+import ti4.commands.sc.SCCommand;
+import ti4.map.Map;
+import ti4.map.MapManager;
+import ti4.message.MessageHelper;
+
+public abstract class DiscordantStarsSubcommandData extends SubcommandData {
+
+    private SlashCommandInteractionEvent event;
+    private Map activeMap;
+    private User user;
+
+    public String getActionID() {
+        return getName();
+    }
+
+   public DiscordantStarsSubcommandData(@NotNull String name, @NotNull String description) {
+        super(name, description);
+    }
+
+    public Map getActiveMap() {
+        return activeMap;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    /**
+     * Send a message to the event's channel, handles large text
+     * @param messageText new message
+     */
+    public void sendMessage(String messageText) {
+        MessageHelper.sendMessageToChannel(event.getMessageChannel(), messageText);
+    }
+
+    abstract public void execute(SlashCommandInteractionEvent event);
+
+    public void preExecute(SlashCommandInteractionEvent event) {
+        this.event = event;
+        user = event.getUser();
+        activeMap = MapManager.getInstance().getUserActiveMap(user.getId());
+    }
+
+    public void reply(SlashCommandInteractionEvent event) {
+        //do nothing
+    }
+}
