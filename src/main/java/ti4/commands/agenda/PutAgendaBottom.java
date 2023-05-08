7c3a873354bc1012ad17ad48bffdd3ca88723a86
Fingon00 <britainfan234@gmail.com>
2023-05-08 16:48:00 -0400

    Small Fixes to putTop/putBottom and Status Phase stuff

--- src/main/java/ti4/commands/agenda/PutAgendaBottom.java
+++ src/main/java/ti4/commands/agenda/PutAgendaBottom.java
@@ -1,5 +1,9 @@
 package ti4.commands.agenda;
 
+import java.util.List;
+
+import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.interaction.GenericInteractionCreateEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
@@ -18,8 +22,19 @@ public class PutAgendaBottom extends AgendaSubcommandData {
 
     public void putBottom(GenericInteractionCreateEvent event, int agendaID, Map activeMap) {
         boolean success = activeMap.putAgendaBottom(agendaID);
-        if (success) {
+        if (success && !activeMap.isFoWMode()) {
             MessageHelper.sendMessageToChannel(activeMap.getActionsChannel(), "Agenda put on bottom");
+            List<ThreadChannel> threadChannels = activeMap.getActionsChannel().getThreadChannels();
+            if (threadChannels == null) return;
+            String threadName = activeMap.getName()+"-round-"+activeMap.getRound()+"-politics";
+            // SEARCH FOR EXISTING OPEN THREAD
+            for (ThreadChannel threadChannel_ : threadChannels) {
+                if (threadChannel_.getName().equals(threadName)) {
+                    MessageHelper.sendMessageToChannel((MessageChannel)threadChannel_, "Agenda put on bottom");
+                }
+            }
+
+
         } else {
             MessageHelper.sendMessageToChannel(activeMap.getActionsChannel(), "No Agenda ID found");
         }
