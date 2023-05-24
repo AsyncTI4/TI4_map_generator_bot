d07d0dbb76f3af3fc5ea92335a1e23253ee7c837
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-05-24 19:31:09 -0400

    fix leader remove

--- src/main/java/ti4/commands/franken/LeaderRemove.java
+++ src/main/java/ti4/commands/franken/LeaderRemove.java
@@ -20,7 +20,7 @@ public class LeaderRemove extends LeaderAddRemove {
                 sb.append("> ").append(leaderID).append(" (player did not have this leader)");
             } else {
                 Leader leader = new Leader(leaderID);
-                sb.append("> ").append(Helper.getLeaderFullRepresentation(null, leader));
+                sb.append("> ").append(Helper.getLeaderFullRepresentation(leader));
             }
             sb.append("\n");
             player.removeLeader(leaderID);
