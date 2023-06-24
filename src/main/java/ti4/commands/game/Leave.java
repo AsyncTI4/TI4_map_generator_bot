35c4c9d621468b4655315a0c77981f85ed09ad8a
Fingon00 <britainfan234@gmail.com>
2023-06-24 14:44:01 -0400

    Game eliminate

--- src/main/java/ti4/commands/game/Leave.java
+++ src/main/java/ti4/commands/game/Leave.java
@@ -3,6 +3,7 @@ package ti4.commands.game;
 import net.dv8tion.jda.api.entities.User;
 import ti4.helpers.Constants;
 import ti4.map.Map;
+import ti4.message.MessageHelper;
 
 public class Leave extends JoinLeave {
 
@@ -12,11 +13,18 @@ public class Leave extends JoinLeave {
 
     @Override
     protected String getResponseMessage(Map activeMap, User user) {
+         if(activeMap.getPlayer(user.getId()) != null && activeMap.getPlayer(user.getId()).isRealPlayer()){
+            return "Did not leave game: " + activeMap.getName() + ". Try a different method or set status to dummy. ";
+        }
         return "Left map: " + activeMap.getName() + " successful";
     }
 
     @Override
     protected void action(Map activeMap, User user) {
+        if(activeMap.getPlayer(user.getId()).isRealPlayer()){
+            MessageHelper.sendMessageToChannel(activeMap.getMainGameChannel(), "You are a real player, and thus should not do /game leave. You should do /game eliminate, or /game replace, depending on what you are looking for.");
+            return;
+        }
         activeMap.removePlayer(user.getId());
     }
 }
