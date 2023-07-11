416021c5281f51a13bcb9bf166cad16274732f29
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-07-11 15:07:52 -0400

    Revert "load issue fix"
    This reverts commit dcd92e12417bd016e2484e36120ba2fb881e978d.

--- src/main/java/ti4/map/Map.java
+++ src/main/java/ti4/map/Map.java
@@ -68,8 +68,6 @@ public class Map {
     private int playerCountForMap = 6;
 
     @ExportableField
-    private int ringCount = 3;
-    @ExportableField
     private int activationCount = 0;
     @ExportableField
     private int vp = 10;
@@ -708,16 +706,9 @@ public class Map {
 
     public int getRingCount() {
         HashMap<String, Tile> tileMap = new HashMap<>(getTileMap());
-        Optional<String> max = tileMap.keySet().stream().filter(Helper::isInteger).max(Comparator.comparingInt(Integer::parseInt));
-        if (max.isPresent()) {
-            String highestPosition = max.get();
-            ringCount = Integer.parseInt(StringUtils.left(highestPosition, highestPosition.length() - 2));
-        }
+        String highestPosition = tileMap.keySet().stream().filter(pos -> Helper.isInteger(pos)).max(Comparator.comparingInt(Integer::parseInt)).get();
+        int ringCount = Integer.parseInt(StringUtils.left(highestPosition, highestPosition.length() - 2));
         return ringCount;
-    }
-
-    public void setRingCount(int ringCount) {
-        this.ringCount = ringCount;
     }
 
     public int getActivationCount() {
