71260454b1a62f1f94018032b1aea83cfa9ece51
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-07-11 23:54:45 -0400

    half baked

--- src/main/java/ti4/commands/planet/PlanetCommand.java
+++ src/main/java/ti4/commands/planet/PlanetCommand.java
@@ -1,15 +1,25 @@
 package ti4.commands.planet;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
+
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import ti4.commands.Command;
+import ti4.helpers.Constants;
+import ti4.map.Map;
+import ti4.map.MapManager;
+import ti4.map.MapSaveLoadManager;
 
 public class PlanetCommand implements Command {
 
+    private final Collection<PlanetSubcommandData> subcommandData = getSubcommands();
+
     @Override
     public String getActionID() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getActionID'");
+        return Constants.PLANET;
     }
 
     @Override
@@ -20,14 +30,47 @@ public class PlanetCommand implements Command {
 
     @Override
     public void execute(SlashCommandInteractionEvent event) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+        String subcommandName = event.getInteraction().getSubcommandName();
+        PlanetSubcommandData executedCommand = null;
+        for (PlanetSubcommandData subcommand : subcommandData) {
+            if (Objects.equals(subcommand.getName(), subcommandName)) {
+                subcommand.preExecute(event);
+                subcommand.execute(event);
+                executedCommand = subcommand;
+                break;
+            }
+        }
+        if (executedCommand == null) {
+            reply(event);
+        } else {
+            executedCommand.reply(event);
+        }
+    }
+
+    public static void reply(SlashCommandInteractionEvent event) {
+        String userID = event.getUser().getId();
+        Map activeMap = MapManager.getInstance().getUserActiveMap(userID);
+        MapSaveLoadManager.saveMap(activeMap, event);
+    }
+
+
+    protected String getActionDescription() {
+        return "Planets";
+    }
+
+    private Collection<PlanetSubcommandData> getSubcommands() {
+        Collection<PlanetSubcommandData> subcommands = new HashSet<>();
+        // subcommands.add(new LeaderInfo());
+
+
+        return subcommands;
     }
 
     @Override
     public void registerCommands(CommandListUpdateAction commands) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'registerCommands'");
+        commands.addCommands(
+                Commands.slash(getActionID(), getActionDescription())
+                        .addSubcommands(getSubcommands()));
     }
-    
+ 
 }
