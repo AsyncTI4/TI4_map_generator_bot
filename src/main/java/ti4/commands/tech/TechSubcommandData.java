a051d50b195ff805447ccfd17f83550543117fc8
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-07-11 23:50:07 -0400

    extentions

--- src/main/java/ti4/commands/tech/TechSubcommandData.java
+++ src/main/java/ti4/commands/tech/TechSubcommandData.java
@@ -1,5 +1,12 @@
 package ti4.commands.tech;
 
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+
 public class TechSubcommandData extends SubcommandData {
+
+    public TechSubcommandData(String name, String description) {
+        super(name, description);
+        //TODO Auto-generated constructor stub
+    }
     
 }
