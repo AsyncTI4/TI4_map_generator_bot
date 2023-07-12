178bc4fbb0293a5f58889da97bb1dcb9c7485d66
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-07-11 23:50:07 -0400

    extentions

--- src/main/java/ti4/commands/planet/PlanetCommand.java
+++ src/main/java/ti4/commands/planet/PlanetCommand.java
@@ -1,7 +1,33 @@
 package ti4.commands.planet;
 
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import ti4.commands.Command;
 
 public class PlanetCommand implements Command {
+
+    @Override
+    public String getActionID() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getActionID'");
+    }
+
+    @Override
+    public boolean accept(SlashCommandInteractionEvent event) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'accept'");
+    }
+
+    @Override
+    public void execute(SlashCommandInteractionEvent event) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
+
+    @Override
+    public void registerCommands(CommandListUpdateAction commands) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'registerCommands'");
+    }
     
 }
--- src/main/java/ti4/commands/planet/PlanetSubcommandData.java
+++ src/main/java/ti4/commands/planet/PlanetSubcommandData.java
@@ -3,5 +3,10 @@ package ti4.commands.planet;
 import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 
 public class PlanetSubcommandData extends SubcommandData {
+
+    public PlanetSubcommandData(String name, String description) {
+        super(name, description);
+        //TODO Auto-generated constructor stub
+    }
     
 }
