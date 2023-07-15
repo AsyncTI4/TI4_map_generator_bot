5268683743c329e8b82a425a51999086b8619645
Chase Hinesman <chasehinesman@gmail.com>
2023-07-15 00:26:01 -0500

    Show ACs, SOs, and PNs in player card info threads

--- src/main/java/ti4/commands/cardsso/ShowAllSO.java
+++ src/main/java/ti4/commands/cardsso/ShowAllSO.java
@@ -27,29 +27,26 @@ public class ShowAllSO extends SOCardsSubcommandData {
             sendMessage("Player could not be found");
             return;
         }
-         Player player_ = Helper.getPlayer(activeMap, null, event);
+        
+        Player player_ = Helper.getPlayer(activeMap, null, event);
         if (player_ == null) {
             sendMessage("Player not found");
             return;
         }
         showAll(player, player_, activeMap);
     }
-    public void showAll(Player player, Player player_, Map activeMap ){
+
+    public void showAll(Player player, Player player_, Map activeMap ) {
         StringBuilder sb = new StringBuilder();
         sb.append("Game: ").append(activeMap.getName()).append("\n");
         sb.append("Player: ").append(player.getUserName()).append("\n");
         sb.append("Showed Secret Objectives:").append("\n");
         List<String> secrets = new ArrayList<>(player.getSecrets().keySet());
-        LinkedHashMap<String, Integer> secretsScored = player.getSecretsScored();
         Collections.shuffle(secrets);
         for (String id : secrets) {
             sb.append(SOInfo.getSecretObjectiveRepresentation(id)).append("\n");
-            if (!secretsScored.containsKey(id)) {
-                player.setSecret(id);
-            }
         }
-        MessageHelper.sendPrivateMessageToPlayer(player, activeMap, "All SOs shown to player");
-        MessageHelper.sendPrivateMessageToPlayer(player_, activeMap, sb.toString());
+        MessageHelper.sendMessageToPlayerCardsInfoThread(player_, activeMap, sb.toString());
+        MessageHelper.sendMessageToPlayerCardsInfoThread(player, activeMap, "All SOs shown to player");
     }
-
 }
--- src/main/java/ti4/commands/cardsso/ShowSO.java
+++ src/main/java/ti4/commands/cardsso/ShowSO.java
@@ -60,13 +60,9 @@ public class ShowSO extends SOCardsSubcommandData {
             sendMessage("Player not found");
             return;
         }
-        User user = MapGenerator.jda.getUserById(player_.getUserID());
-        if (user == null) {
-            sendMessage("User for faction not found. Report to ADMIN");
-            return;
-        }
-
-        sendMessage("SO shown to player - check DMs");
-        MessageHelper.sendMessageToUser(sb.toString(), user);
+        
+        sendMessage("SO shown to player");
+        SOInfo.sendSecretObjectiveInfo(activeMap, player);
+        MessageHelper.sendMessageToPlayerCardsInfoThread(player_, activeMap, sb.toString());
     }
 }
