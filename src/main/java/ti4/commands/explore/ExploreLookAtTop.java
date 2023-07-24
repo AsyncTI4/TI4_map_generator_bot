0276766d9c3cc6a7e3e62160cc534c80ac7b9ec2
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-07-24 10:17:32 -0400

    removing legacy commented code and removed migration code

--- src/main/java/ti4/commands/explore/ExploreLookAtTop.java
+++ src/main/java/ti4/commands/explore/ExploreLookAtTop.java
@@ -32,49 +32,12 @@ public class ExploreLookAtTop extends ExploreSubcommandData {
             sendMessage("Trait not found");
             return;
         }
-        // KEEPING CODE INCASE NEED TO SEE ALL OR MORE AT ONCE
-        // ArrayList<String> traitsToLookAt = new ArrayList<String>();
-        // OptionMapping reqType = event.getOption(Constants.TRAIT);
-        // if (reqType != null) {
-        //     traitsToLookAt.add(reqType.getAsString());
-        // }
-        // else {
-        //     traitsToLookAt.add(Constants.CULTURAL);
-        //     traitsToLookAt.add(Constants.INDUSTRIAL);
-        //     traitsToLookAt.add(Constants.HAZARDOUS);
-        //     traitsToLookAt.add(Constants.FRONTIER);
-        // }
-        // for (String currentType : traitsToLookAt) {
-        //     StringBuilder info = new StringBuilder();
-        //     ArrayList<String> deck = activeMap.getExploreDeck(currentType);
-        //     Collections.sort(deck);
-        //     Integer deckCount = deck.size();
-        //     Double deckDrawChance = deckCount == 0 ? 0.0 : 1.0 / deckCount;
-        //     NumberFormat formatPercent = NumberFormat.getPercentInstance();
-        //     formatPercent.setMaximumFractionDigits(1);
-        //     ArrayList<String> discard = activeMap.getExploreDiscard(currentType);
-        //     Collections.sort(discard);
-        //     Integer discardCount = discard.size();
-
-        //     info.append(Helper.getEmojiFromDiscord(currentType)).append("**").append(currentType.toUpperCase()).append(" EXPLORE DECK** (").append(String.valueOf(deckCount)).append(") _").append(formatPercent.format(deckDrawChance)).append("_\n");
-        //     info.append(listNames(deck)).append("\n");
-        //     info.append(Helper.getEmojiFromDiscord(currentType)).append("**").append(currentType.toUpperCase()).append(" EXPLORE DISCARD** (").append(String.valueOf(discardCount)).append(")\n");
-        //     info.append(listNames(discard)).append("\n_ _\n");
-        //     sendMessage(info.toString());
-        // }
 
         ArrayList<String> deck = activeMap.getExploreDeck(trait);
         ArrayList<String> discardPile = activeMap.getExploreDiscard(trait);
 
-        // @jrkd 23/07 - This code will only be triggered for existing games where the
-        // explore deck is already empty. New games, or games without an empty explore
-        // deck will auto-shuffle the discard when drawing the last card.
         String traitNameWithEmoji = Helper.getEmojiFromDiscord(trait) + trait;
         String playerFactionNameWithEmoji = Helper.getFactionIconFromDiscord(player.getFaction());
-        if (deck.isEmpty() && !discardPile.isEmpty()) {
-            activeMap.shuffleDiscardsIntoExploreDeck(trait);
-            sendMessage(traitNameWithEmoji + " explore deck is empty, reshuffling discards into deck");
-        }
         if (deck.isEmpty() && discardPile.isEmpty()) {
             sendMessage(traitNameWithEmoji + " explore deck & discard is empty - nothing to look at.");
         }
