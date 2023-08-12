edb685a160ed3d3bea74338a6da97280aadc5b74
Mooloor <mooloor_alt@gmail.com>
2023-08-12 23:18:22 +0100

    Added command to actually reverse speaker order

--- src/main/java/ti4/helpers/Constants.java
+++ src/main/java/ti4/helpers/Constants.java
@@ -64,6 +64,7 @@ public class Constants {
     public static final String STATISTICS = "statistics";
     public static final String LOG_MESSAGE = "log_message";
     public static final String SET_GAME = "set_game";
+    public static final String REVERSE_SPEAKER_ORDER = "reverse_speaker_order";
     public static final String ADD_BORDER_ANOMALY = "add_border_anomaly";
     public static final String REMOVE_BORDER_ANOMALY = "remove_border_anomaly";
     public static final String SET_STATUS = "set_status";
