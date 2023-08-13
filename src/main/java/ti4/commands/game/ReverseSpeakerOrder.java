c162f0ad5828b2fe689b19475c0c3d48d29a8e0b
Mooloor <mooloor_alt@gmail.com>
2023-08-13 22:00:30 +0100

    Added saving to map file

--- src/main/java/ti4/commands/game/ReverseSpeakerOrder.java
+++ src/main/java/ti4/commands/game/ReverseSpeakerOrder.java
@@ -4,28 +4,18 @@ import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
-import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
-import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
-import org.jetbrains.annotations.NotNull;
-import ti4.commands.Command;
-import ti4.generator.GenerateMap;
 import ti4.helpers.Constants;
 import ti4.map.Map;
 import ti4.map.MapManager;
-import ti4.map.MapSaveLoadManager;
 import ti4.message.MessageHelper;
-import ti4.model.BorderAnomalyModel;
-
-import java.io.File;
 
 public class ReverseSpeakerOrder extends GameSubcommandData {
-
     public ReverseSpeakerOrder() {
         super(Constants.REVERSE_SPEAKER_ORDER, "Change the speaker order from clockwise to counterclockwise or vice versa");
         addOptions(new OptionData(OptionType.STRING, Constants.CONFIRM, "Type YES to confirm").setRequired(true));
     }
-    
+
     @Override
     public void execute(SlashCommandInteractionEvent event) {
         if(!event.getOption(Constants.CONFIRM, null, OptionMapping::getAsString).equals("YES")) {
