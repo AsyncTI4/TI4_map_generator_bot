c27a0b36754f4f42742f18b7b20d8edad0255fea
fireseal <fireseal@vx9.com>
2023-08-20 23:08:00 -0500

    Finish removing debug and add final text.

--- src/main/java/ti4/commands/bothelper/Observer.java
+++ src/main/java/ti4/commands/bothelper/Observer.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.IPermissionHolder;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
@@ -24,21 +23,20 @@ public class Observer extends BothelperSubcommandData {
     public void execute(SlashCommandInteractionEvent event) {
        Guild guild = event.getGuild();
        Member user = guild.getMemberById(event.getOption("player").getAsString());
-       // Check if game channels exist
        
         List<GuildChannel> channels = guild.getChannels();
-        //sendMessage("DEBUG: PlayerID: " + event.getOption("player").getAsString() + " Add/remove: " + event.getOption("add_remove").getAsString() + " playername: " + user.getAsMention() + " userID: " + user.getId());
+        
         for(GuildChannel channel : channels) {
             if(channel.getName().contains(event.getOption("game_name").getAsString())) {
             
                 if(event.getOption("add_remove").getAsString().equals("add")) {
                     channel.getPermissionContainer().upsertPermissionOverride(user).grant(Permission.VIEW_CHANNEL,Permission.MESSAGE_SEND).queue();
-                    //sendMessage("Permissions granted on " + user.getAsMention() + " to channel " + channel.getName());
+                    sendMessage("Permissions granted on " + user.getAsMention() + " to channel " + channel.getName());
                 }
 
                 if(event.getOption("add_remove").getAsString().equals("remove")) {
                     channel.getPermissionContainer().upsertPermissionOverride(user).deny(Permission.VIEW_CHANNEL, Permission.MESSAGE_SEND).queue();
-                    //sendMessage("Permissions revoked on " + user.getAsMention() + " to channel " + channel.getName());
+                    sendMessage("Permissions revoked on " + user.getAsMention() + " to channel " + channel.getName());
                 }
             }
         }
