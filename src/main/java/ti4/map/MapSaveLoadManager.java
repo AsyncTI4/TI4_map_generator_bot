8deedf2daa18099aec8c0a1f79d323f5e348883a
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-08-30 09:53:01 -0400

    rename constant

--- src/main/java/ti4/map/MapSaveLoadManager.java
+++ src/main/java/ti4/map/MapSaveLoadManager.java
@@ -531,7 +531,7 @@ public class MapSaveLoadManager {
             writer.write(Constants.COLOR + " " + playerColor);
             writer.write(System.lineSeparator());
 
-            writer.write(Constants.HOME_SYSTEM_LOCATION + " " + player.getPlayerStatsAnchorPosition());
+            writer.write(Constants.STATS_ANCHOR_LOCATION + " " + player.getPlayerStatsAnchorPosition());
             writer.write(System.lineSeparator());
 
              writer.write(Constants.ALLIANCE_MEMBERS + " " + player.getAllianceMembers());
@@ -1561,7 +1561,7 @@ public class MapSaveLoadManager {
             switch (data) {
                 case Constants.FACTION -> player.setFaction(tokenizer.nextToken());
                 case Constants.COLOR -> player.setColor(tokenizer.nextToken());
-                case Constants.HOME_SYSTEM_LOCATION -> player.setPlayerStatsAnchorPosition(tokenizer.nextToken());
+                case Constants.STATS_ANCHOR_LOCATION -> player.setPlayerStatsAnchorPosition(tokenizer.nextToken());
                 case Constants.ALLIANCE_MEMBERS -> player.setAllianceMembers(tokenizer.nextToken());
                 case Constants.ROLE_FOR_COMMUNITY -> player.setRoleIDForCommunity(tokenizer.nextToken());
                 case Constants.PLAYER_PRIVATE_CHANNEL -> player.setPrivateChannelID(tokenizer.nextToken());
