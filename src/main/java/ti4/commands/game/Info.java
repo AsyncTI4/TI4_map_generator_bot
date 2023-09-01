51196e62515897bcb85fe17623105ea3854f379f
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-09-01 12:15:40 -0400

    added deck sized, more settings, partitioned real/not real players

--- src/main/java/ti4/commands/game/Info.java
+++ src/main/java/ti4/commands/game/Info.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 
 import ti4.helpers.Constants;
+import ti4.helpers.Emojis;
 import ti4.helpers.FoWHelper;
 import ti4.helpers.Helper;
 import ti4.map.Map;
@@ -13,8 +14,6 @@ import ti4.map.MapManager;
 import ti4.map.Player;
 import ti4.message.MessageHelper;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Info extends GameSubcommandData{
     public static final String NEW_LINE = "\n";
@@ -43,14 +42,16 @@ public class Info extends GameSubcommandData{
             activeMap = mapManager.getMap(gameOption.getAsString());
         }
         sb.append("## Game Info:").append(NEW_LINE);
-        sb.append("### Game name: " + activeMap.getName()).append(NEW_LINE);
-        sb.append("Game owner: " + activeMap.getOwnerName()).append(NEW_LINE);
+        sb.append("### Name: " + activeMap.getName()).append(NEW_LINE);
+        sb.append("Owner: " + activeMap.getOwnerName()).append(NEW_LINE);
         sb.append("Created: " + activeMap.getCreationDate()).append(NEW_LINE);
         sb.append("Last Modified: " + Helper.getDateRepresentation(activeMap.getLastModifiedDate())).append(NEW_LINE);
-        sb.append("Game status: `" + activeMap.getMapStatus()).append("`").append(NEW_LINE);
-        sb.append("Game Ended: `" + activeMap.isHasEnded()).append("`").append(NEW_LINE);
+        sb.append("Status: `" + activeMap.getMapStatus()).append("`").append(NEW_LINE);
+        sb.append("Ended: `" + activeMap.isHasEnded()).append("`").append(NEW_LINE);
         if (activeMap.isHasEnded()) sb.append("> Date Ended: " + Helper.getDateRepresentation(activeMap.getEndedDate())).append(NEW_LINE);
+        
         sb.append("### Setup: ").append(NEW_LINE);
+        sb.append("VP Count: " + activeMap.getVp()).append(NEW_LINE);
         sb.append("Game Modes: " + activeMap.getGameModesText()).append(NEW_LINE);
         if (privateGame == null || privateGame == false) {
             sb.append("Map String: `" + Helper.getMapString(activeMap)).append("`").append(NEW_LINE);
@@ -59,31 +60,44 @@ public class Info extends GameSubcommandData{
         }
         sb.append("Strategy Card Set: `").append(activeMap.getScSetID()).append("`").append(NEW_LINE);
         sb.append("Decks: ").append(NEW_LINE);
-        sb.append("> Action Card Deck: `").append(activeMap.getAcDeckID()).append("` ").append(activeMap.getActionCards().size()).append("/").append(activeMap.getActionCardFullDeckSize()).append(NEW_LINE);
-        sb.append("> Secret Objective Deck: `").append(activeMap.getSoDeckID()).append("` ").append(activeMap.getSecretObjectives().size()).append(NEW_LINE);
-        sb.append("> Stage 1 Public Objective Deck: `").append(activeMap.getStage1PublicDeckID()).append("` ").append(activeMap.getPublicObjectives1().size()).append(NEW_LINE);
-        sb.append("> Stage 2 Public Objective Deck: `").append(activeMap.getStage2PublicDeckID()).append("` ").append(activeMap.getPublicObjectives2().size()).append(NEW_LINE);
-        sb.append("> Relic Deck: `").append(activeMap.getRelicDeckID()).append("` ").append(activeMap.getAllRelics().size()).append(NEW_LINE);
-        sb.append("> Agenda Deck: `").append(activeMap.getAgendaDeckID()).append("` ").append(activeMap.getAgendas().size()).append(NEW_LINE);
-        sb.append("> Exploration Deck: `").append(activeMap.getExplorationDeckID()).append("`").append(NEW_LINE);
+        sb.append("- ").append(Emojis.ActionCard).append("Action Card Deck: `").append(activeMap.getAcDeckID()).append("` ")
+            .append(activeMap.getActionCardDeckSize()).append("/").append(activeMap.getActionCardFullDeckSize()).append(NEW_LINE);
+        sb.append("- ").append(Emojis.SecretObjective).append("Secret Objective Deck: `").append(activeMap.getSoDeckID()).append("` ").append(activeMap.getSecretObjectiveDeckSize()).append("/").append(activeMap.getSecretObjectiveFullDeckSize()).append(NEW_LINE);
+        sb.append("- ").append(Emojis.Public1).append("Stage 1 Public Objective Deck: `").append(activeMap.getStage1PublicDeckID()).append("` ").append(activeMap.getPublicObjectives1DeckSize()).append("/").append(activeMap.getPublicObjectives1FullDeckSize()).append(NEW_LINE);
+        sb.append("- ").append(Emojis.Public2).append("Stage 2 Public Objective Deck: `").append(activeMap.getStage2PublicDeckID()).append("` ").append(activeMap.getPublicObjectives2DeckSize()).append("/").append(activeMap.getPublicObjectives2FullDeckSize()).append(NEW_LINE);
+        sb.append("- ").append(Emojis.Agenda).append("Agenda Deck: `").append(activeMap.getAgendaDeckID()).append("` ").append(activeMap.getAgendaDeckSize()).append("/").append(activeMap.getAgendaFullDeckSize()).append(NEW_LINE);
+        sb.append("- ").append(Emojis.RelicCard).append("Relic Deck: `").append(activeMap.getRelicDeckID()).append("` ").append(activeMap.getRelicDeckSize()).append("/").append(activeMap.getRelicFullDeckSize()).append(NEW_LINE);
+        sb.append("- Exploration Deck: `").append(activeMap.getExplorationDeckID()).append("`").append(NEW_LINE);
+        sb.append(" - ").append(Emojis.IndustrialCard).append("Industrial Deck: ").append(activeMap.getIndustrialExploreDeckSize()).append("/").append(activeMap.getIndustrialExploreFullDeckSize()).append(NEW_LINE);
+        sb.append(" - ").append(Emojis.HazardousCard).append("Hazardous Deck: ").append(activeMap.getHazardousExploreDeckSize()).append("/").append(activeMap.getHazardousExploreFullDeckSize()).append(NEW_LINE);
+        sb.append(" - ").append(Emojis.CulturalCard).append("Cultural Deck: ").append(activeMap.getCulturalExploreDeckSize()).append("/").append(activeMap.getCulturalExploreFullDeckSize()).append(NEW_LINE);
+        sb.append(" - ").append(Emojis.FrontierCard).append("Frontier Deck: ").append(activeMap.getFrontierExploreDeckSize()).append("/").append(activeMap.getFrontierExploreFullDeckSize()).append(NEW_LINE);
+        
         sb.append("### Settings: ").append(NEW_LINE);
         sb.append("Auto-Ping Time Interval (hrs): " + activeMap.getAutoPingSpacer()).append(NEW_LINE);
+        sb.append("Text Size: " + activeMap.getLargeText()).append(NEW_LINE);
+        sb.append("Output Verbosity: " + activeMap.getOutputVerbosity()).append(NEW_LINE);
+        sb.append("CC/Plastic warnings: " + activeMap.getCCNPlasticLimit()).append(NEW_LINE);
         if (privateGame == null || privateGame == false) {
             sb.append("### Players: ").append(NEW_LINE);
-            HashMap<String, Player> mapPlayers = activeMap.getPlayers();
             int index = 1;
-            ArrayList<Player> players = new ArrayList<>(mapPlayers.values());
-            for (Player player : players) {
-                if (player.getFaction() == null) continue;
-                
-                sb.append("> `").append(index).append(".` ").append(player.getUserName()).append(Helper.getFactionIconFromDiscord(player.getFaction())).append(Helper.getColourAsMention(event.getGuild(), player.getColor()));
+            for (Player player : activeMap.getRealPlayers()) {
+                sb.append("> `").append(index).append(".` ").append(Helper.getFactionIconFromDiscord(player.getFaction())).append(player.getUserName()).append(Helper.getColourAsMention(event.getGuild(), player.getColor()));
                 sb.append(" - *").append(player.getTotalVictoryPoints(activeMap)).append("VP* ");
+                sb.append(NEW_LINE);
+                index++;
+            }
+
+            sb.append("### Other Players: ").append(NEW_LINE);
+            for (Player player : activeMap.getNotRealPlayers()) {
+                sb.append("> `").append(index).append(".` ").append(player.getUserName());
                 sb.append(NEW_LINE);
                 index++;
             }
         } else {
             sb.append("### Players: Cannot show players for private games").append(NEW_LINE);
         }
+        
         sb.append("### Other Stats: ").append(NEW_LINE);
         sb.append("Current Phase: " + activeMap.getCurrentPhase()).append(NEW_LINE);
         sb.append("Ring Count: " +activeMap.getRingCount()).append(NEW_LINE);
