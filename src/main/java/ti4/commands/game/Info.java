f71c3a3f62c6d8b2a6031ef52d9d9b5241dea0ce
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-09-01 11:19:19 -0400

    restructure /game info, add some stuff

--- src/main/java/ti4/commands/game/Info.java
+++ src/main/java/ti4/commands/game/Info.java
@@ -42,36 +42,34 @@ public class Info extends GameSubcommandData{
         } else if (activeMap == null ){
             activeMap = mapManager.getMap(gameOption.getAsString());
         }
-        sb.append("Game Info:").append(NEW_LINE);
-        sb.append("Game name: " + activeMap.getName()).append(NEW_LINE);
+        sb.append("## Game Info:").append(NEW_LINE);
+        sb.append("### Game name: " + activeMap.getName()).append(NEW_LINE);
         sb.append("Game owner: " + activeMap.getOwnerName()).append(NEW_LINE);
-        sb.append("Game status: " + activeMap.getMapStatus());
-        if (activeMap.isHasEnded()) sb.append(" - GAME HAS ENDED");
-        sb.append(NEW_LINE);
-        sb.append("Game Modes: " + activeMap.getGameModesText()).append(NEW_LINE);
-        sb.append("Strategy Card Set: `").append(activeMap.getScSetID()).append("`").append(NEW_LINE);
-        sb.append("Decks: ").append(NEW_LINE);
-        sb.append("> Action Card Deck: `").append(activeMap.getAcDeckID()).append("`").append(NEW_LINE);
-        sb.append("> Secret Objective Deck: `").append(activeMap.getSoDeckID()).append("`").append(NEW_LINE);
-        sb.append("> Stage 1 Public Objective Deck: `").append(activeMap.getStage1PublicDeckID()).append("`").append(NEW_LINE);
-        sb.append("> Stage 2 Public Objective Deck: `").append(activeMap.getStage2PublicDeckID()).append("`").append(NEW_LINE);
-        sb.append("> Relic Deck: `").append(activeMap.getRelicDeckID()).append("`").append(NEW_LINE);
-        sb.append("> Agenda Deck: `").append(activeMap.getAgendaDeckID()).append("`").append(NEW_LINE);
-        sb.append("> Exploration Deck: `").append(activeMap.getExplorationDeckID()).append("`").append(NEW_LINE);
-        sb.append("Current Phase: " +activeMap.getCurrentPhase()).append(NEW_LINE);
-        sb.append("Ring Count: " +activeMap.getRingCount()).append(NEW_LINE);
-        sb.append("Auto-Ping Time Interval (hrs): " + activeMap.getAutoPingSpacer()).append(NEW_LINE);
         sb.append("Created: " + activeMap.getCreationDate()).append(NEW_LINE);
         sb.append("Last Modified: " + Helper.getDateRepresentation(activeMap.getLastModifiedDate())).append(NEW_LINE);
-        sb.append("Map Images Generated: " + activeMap.getMapImageGenerationCount()).append(NEW_LINE);
+        sb.append("Game status: `" + activeMap.getMapStatus()).append("`").append(NEW_LINE);
+        sb.append("Game Ended: `" + activeMap.isHasEnded()).append("`").append(NEW_LINE);
+        if (activeMap.isHasEnded()) sb.append("> Date Ended: " + Helper.getDateRepresentation(activeMap.getEndedDate())).append(NEW_LINE);
+        sb.append("### Setup: ").append(NEW_LINE);
+        sb.append("Game Modes: " + activeMap.getGameModesText()).append(NEW_LINE);
         if (privateGame == null || privateGame == false) {
             sb.append("Map String: `" + Helper.getMapString(activeMap)).append("`").append(NEW_LINE);
         } else {
             sb.append("Map String: Cannot show map string for private games").append(NEW_LINE);
         }
-        sb.append("Game player count: " + activeMap.getPlayerCountForMap()).append(NEW_LINE);
+        sb.append("Strategy Card Set: `").append(activeMap.getScSetID()).append("`").append(NEW_LINE);
+        sb.append("Decks: ").append(NEW_LINE);
+        sb.append("> Action Card Deck: `").append(activeMap.getAcDeckID()).append("` ").append(activeMap.getActionCards().size()).append("/").append(activeMap.getActionCardFullDeckSize()).append(NEW_LINE);
+        sb.append("> Secret Objective Deck: `").append(activeMap.getSoDeckID()).append("` ").append(activeMap.getSecretObjectives().size()).append(NEW_LINE);
+        sb.append("> Stage 1 Public Objective Deck: `").append(activeMap.getStage1PublicDeckID()).append("` ").append(activeMap.getPublicObjectives1().size()).append(NEW_LINE);
+        sb.append("> Stage 2 Public Objective Deck: `").append(activeMap.getStage2PublicDeckID()).append("` ").append(activeMap.getPublicObjectives2().size()).append(NEW_LINE);
+        sb.append("> Relic Deck: `").append(activeMap.getRelicDeckID()).append("` ").append(activeMap.getAllRelics().size()).append(NEW_LINE);
+        sb.append("> Agenda Deck: `").append(activeMap.getAgendaDeckID()).append("` ").append(activeMap.getAgendas().size()).append(NEW_LINE);
+        sb.append("> Exploration Deck: `").append(activeMap.getExplorationDeckID()).append("`").append(NEW_LINE);
+        sb.append("### Settings: ").append(NEW_LINE);
+        sb.append("Auto-Ping Time Interval (hrs): " + activeMap.getAutoPingSpacer()).append(NEW_LINE);
         if (privateGame == null || privateGame == false) {
-            sb.append("Players: ").append(NEW_LINE);
+            sb.append("### Players: ").append(NEW_LINE);
             HashMap<String, Player> mapPlayers = activeMap.getPlayers();
             int index = 1;
             ArrayList<Player> players = new ArrayList<>(mapPlayers.values());
@@ -84,11 +82,19 @@ public class Info extends GameSubcommandData{
                 index++;
             }
         } else {
-            sb.append("Players: Cannot show players for private games").append(NEW_LINE);
+            sb.append("### Players: Cannot show players for private games").append(NEW_LINE);
         }
-        sb.append("Public Objectives:\n> `").append(activeMap.getRevealedPublicObjectives()).append("`").append(NEW_LINE);
-        sb.append("Laws:\n> `").append(activeMap.getLaws()).append("`").append(NEW_LINE);
-        sb.append("Migrations Run:\n> `").append(activeMap.getRunMigrations()).append("`").append(NEW_LINE);
+        sb.append("### Other Stats: ").append(NEW_LINE);
+        sb.append("Current Phase: " + activeMap.getCurrentPhase()).append(NEW_LINE);
+        sb.append("Ring Count: " +activeMap.getRingCount()).append(NEW_LINE);
+        sb.append("Game Player Count: " + activeMap.getPlayerCountForMap()).append(NEW_LINE);
+        sb.append("Game Real Player Count: " + activeMap.getRealPlayers().size()).append(NEW_LINE);
+        sb.append("Map Images Generated: " + activeMap.getMapImageGenerationCount()).append(NEW_LINE);
+        sb.append("Strategy Card Trade Goods: `").append(activeMap.getScTradeGoods()).append("`").append(NEW_LINE);
+        sb.append("Public Objectives: `").append(activeMap.getRevealedPublicObjectives()).append("`").append(NEW_LINE);
+        sb.append("Laws: `").append(activeMap.getLaws()).append("`").append(NEW_LINE);
+        sb.append("Laws Info: `").append(activeMap.getLawsInfo()).append("`").append(NEW_LINE);
+        sb.append("Migrations Run: `").append(activeMap.getRunMigrations()).append("`").append(NEW_LINE);
 
         return sb;
     }
