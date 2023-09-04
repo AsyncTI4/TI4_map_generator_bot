bf51d607ebbcba3994ca44d029f11ebb737a7609
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-09-04 19:47:22 -0400

    show me relic text when purging

--- src/main/java/ti4/commands/explore/PurgeRelic.java
+++ src/main/java/ti4/commands/explore/PurgeRelic.java
@@ -7,6 +7,7 @@ import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import ti4.generator.Mapper;
 import ti4.helpers.Constants;
 import ti4.helpers.Emojis;
+import ti4.helpers.Helper;
 import ti4.map.Player;
 
 public class PurgeRelic extends GenericRelicAction {
@@ -19,19 +20,16 @@ public class PurgeRelic extends GenericRelicAction {
 
     @Override
     public void doAction(Player player, SlashCommandInteractionEvent event) {
-        OptionMapping option = event.getOption(Constants.RELIC);
-        if (option == null) {
-            sendMessage("Specify relic");
+        String relicId = event.getOption(Constants.RELIC, null, OptionMapping::getAsString);
+        if (relicId == null || !player.hasRelic(relicId)) {
+            sendMessage("Invalid relic or player does not have specified relic: " + relicId);
             return;
         }
-        String relicId = option.getAsString();
-        if (player.hasRelic(relicId)) {
-            player.removeRelic(relicId);
-            player.removeExhaustedRelic(relicId);
-            String relicName = Mapper.getRelic(relicId).split(";")[0];
-            sendMessage("Purged " + Emojis.Relic + " relic: " + relicName);
-        } else {
-            sendMessage("Invalid relic or player does not have specified relic");
-        }
+        player.removeRelic(relicId);
+        player.removeExhaustedRelic(relicId);
+        String[] relicData = Mapper.getRelic(relicId).split(";");
+        StringBuilder message = new StringBuilder();
+        message.append(Helper.getPlayerRepresentation(player, getActiveMap())).append(" purged relic Relic:\n").append(Emojis.Relic).append(" __**").append(relicData[0]).append("**__\n> ").append(relicData[1]).append("\n");
+        sendMessage(message.toString());
     }
 }
