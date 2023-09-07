c86b955a2287dd741897e456a8be56ea4d7707cf
Fingon00 <britainfan234@gmail.com>
2023-09-06 22:21:47 -0400

    Update ButtonListener.java

--- src/main/java/ti4/buttons/ButtonListener.java
+++ src/main/java/ti4/buttons/ButtonListener.java
@@ -1272,12 +1272,6 @@ public class ButtonListener extends ListenerAdapter {
                 MessageHelper.sendMessageToChannelWithButtons((MessageChannel)yssaril.getCardsInfoThread(activeMap), message, acButtons);
                 event.getMessage().delete().queue();
             }
-
-            acButtons.add(Button.success("takeAC_" + acID + "_"+player.getFaction(), acName).withEmoji(Emoji.fromFormatted(Emojis.ActionCard)));
-            acButtons.add(Button.danger("yssarilHeroRejection_"+player.getFaction(), "Reject " + acName +" and force them to discard of 3 random ACs"));
-            String message = Helper.getPlayerRepresentation(yssaril, activeMap, activeMap.getGuild(), true) + " "+offerName +" has offered you the action card "+ acName + " for your Yssaril Hero play. Use buttons to accept or reject it";
-            MessageHelper.sendMessageToChannelWithButtons((MessageChannel)yssaril.getCardsInfoThread(activeMap), message, acButtons);
-            event.getMessage().delete().queue();//"statusInfRevival_"
          } else if (buttonID.startsWith("statusInfRevival_")) {
             ButtonHelper.placeInfantryFromRevival(activeMap, event, player, buttonID);
         } else if (buttonID.startsWith("genericReact")) {
