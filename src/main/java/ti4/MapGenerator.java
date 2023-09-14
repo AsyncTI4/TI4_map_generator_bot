281c989c2c6952bd7d06a9d950491892c8c1f86b
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-09-14 16:16:43 -0400

    remove sleep before bot tries to save maps during shutdown

--- src/main/java/ti4/MapGenerator.java
+++ src/main/java/ti4/MapGenerator.java
@@ -265,7 +265,7 @@ public class MapGenerator {
                 MessageHelper.sendMessageToBotLogWebhook("`" + new Timestamp(System.currentTimeMillis()) + "` SHUTDOWN PROCESS STARTED");
                 readyToReceiveCommands = false;
                 MessageHelper.sendMessageToBotLogWebhook("`" + new Timestamp(System.currentTimeMillis()) + "` BOT IS NO LONGER ACCEPTING COMMANDS ");
-                TimeUnit.SECONDS.sleep(10);
+                // TimeUnit.SECONDS.sleep(10);
                 GameSaveLoadManager.saveMaps();
                 MessageHelper.sendMessageToBotLogWebhook("`" + new Timestamp(System.currentTimeMillis()) + "` MAPS HAVE BEEN SAVED");
                 MessageHelper.sendMessageToBotLogWebhook("`" + new Timestamp(System.currentTimeMillis()) + "` SHUTDOWN PROCESS COMPLETE");
