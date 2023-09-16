141ea9bdb452db09892da8175bfc82c52dbe6900
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-09-16 10:52:14 -0400

    olradin policy exhaust messages

--- src/main/java/ti4/helpers/DiscordantStarsHelper.java
+++ src/main/java/ti4/helpers/DiscordantStarsHelper.java
@@ -1,12 +1,14 @@
 package ti4.helpers;
 
-import ti4.commands.ds.SetPolicy;
+import org.apache.commons.lang3.StringUtils;
+
 import ti4.generator.Mapper;
 import ti4.map.Game;
 import ti4.map.Planet;
 import ti4.map.Player;
 import ti4.map.Tile;
 import ti4.map.UnitHolder;
+import ti4.message.MessageHelper;
 import ti4.model.PlanetModel;
 
 public class DiscordantStarsHelper {
@@ -90,54 +92,70 @@ public class DiscordantStarsHelper {
     }
 
     public static void handleOlradinPoliciesWhenExhaustingPlanets(Game activeGame, Player player, String planet) {
-        if (activeGame == null || !activeGame.isDiscordantStarsMode() || player == null || !player.hasOlradinPolicies()) return;
+        if (activeGame == null || !activeGame.isDiscordantStarsMode() || !"action".equalsIgnoreCase(activeGame.getCurrentPhase()) || player == null || !player.hasOlradinPolicies()) return;
         PlanetModel planetModel = Mapper.getPlanet(planet);
         if (planetModel == null) return;
         StringBuilder sb = new StringBuilder();
 
         if (planetModel.isLegendary()) {
             sb.append("You exhausted ").append(Emojis.LegendaryPlanet).append(planetModel.getName()).append(" and triggered the following abilities:\n");
-            if (player.getHasUsedEnvironmentPreserveAbility() && player.hasAbility("policy_the_environment_preserve")) {
-                player.setHasUsedEnvironmentPreserveAbility(true);
-                sb.append("");
-            }
-            if (player.getHasUsedEconomyEmpowerAbility() && player.hasAbility("policy_the_economy_empower")) {
-                player.setHasUsedEconomyEmpowerAbility(true);
-                sb.append("");
-            }
-            if (player.getHasUsedPeopleConnectAbility() && player.hasAbility("policy_the_people_connect")) {
-                player.setHasUsedPeopleConnectAbility(true);
-                sb.append("");
-            }
+            resolveEnvironmentPreserveAbility(player, planetModel, sb);
+            resolveEconomyEmpowerAbility(player, sb);
+            resolvePeopleConnectAbility(player, planetModel, sb);
+            MessageHelper.sendMessageToPlayerCardsInfoThread(player, activeGame, sb.toString());
+            sb.append("Please be mindful that these abilities can only be used 'Once per Action' and this suggestion *may* be incorrectly timed.\nPlease resolve these abilities yourself and report them to all players.");
             return;
         }
 
         switch (planetModel.getPlanetType()) {
             case HAZARDOUS -> {
-                if (player.getHasUsedEnvironmentPreserveAbility() && player.hasAbility("policy_the_environment_preserve")) {
-                    player.setHasUsedEnvironmentPreserveAbility(true);
-                    sb.append("");
-                }
+                sb.append("You exhausted ").append(Emojis.Hazardous).append(planetModel.getName()).append(" and triggered the following abilities:\n");
+                resolveEnvironmentPreserveAbility(player, planetModel, sb);
             }
             case INDUSTRIAL -> {
-                if (player.getHasUsedEconomyEmpowerAbility() && player.hasAbility("policy_the_economy_empower")) {
-                    player.setHasUsedEconomyEmpowerAbility(true);
-                    sb.append("");
-                }
-                if (player.getHasUsedEconomyExploitAbility() && player.hasAbility("policy_the_economy_exploit")) { //add a fighter with ships
-                    player.setHasUsedEconomyExploitAbility(true);
-                }
+                sb.append("You exhausted ").append(Emojis.Industrial).append(planetModel.getName()).append(" and triggered the following abilities:\n");
+                resolveEconomyEmpowerAbility(player, sb);
+                resolveEconomyExploitAbility(player, sb);
             }
             case CULTURAL -> {
-                if (player.getHasUsedPeopleConnectAbility() && player.hasAbility("policy_the_people_connect")) {
-                    player.setHasUsedPeopleConnectAbility(true);
-                    sb.append("");
-                }
+                sb.append("You exhausted ").append(Emojis.Cultural).append(planetModel.getName()).append(" and triggered the following abilities:\n");
+                resolvePeopleConnectAbility(player, planetModel, sb);
             }
             default -> {
-                //do nothing
+                return;
             }
         }
+        sb.append("Please be mindful that these abilities can only be used 'Once per Action' and this suggestion *may* be incorrectly timed.\nPlease resolve these abilities yourself and report them to all players.");
+        MessageHelper.sendMessageToPlayerCardsInfoThread(player, activeGame, sb.toString());
+    }
 
+    private static void resolveEconomyExploitAbility(Player player, StringBuilder sb) {
+        if (!player.getHasUsedEconomyExploitAbility() && player.hasAbility("policy_the_economy_exploit")) { //add a fighter with ships
+            player.setHasUsedEconomyExploitAbility(true);
+            sb.append("**The Economy - Exploit (+)**: You may place 1 " + Emojis.fighter + "Fighter from your reinforcements in a system that contains 1 or more of your ships.\n");
+        }
+    }
+
+    private static void resolvePeopleConnectAbility(Player player, PlanetModel planetModel, StringBuilder sb) {
+        if (!player.getHasUsedPeopleConnectAbility() && player.hasAbility("policy_the_people_connect")) {
+            player.setHasUsedPeopleConnectAbility(true);
+            sb.append("> **The People - Connect (+)**: You may move 1 " + Emojis.infantry + "Infantry on " + planetModel.getName() + " to another planet you control.\n");
+        }
+    }
+
+    private static void resolveEconomyEmpowerAbility(Player player, StringBuilder sb) {
+        if (!player.getHasUsedEconomyEmpowerAbility() && player.hasAbility("policy_the_economy_empower")) {
+            player.setHasUsedEconomyEmpowerAbility(true);
+            sb.append("> **The Economy - Empower (+)**: You gain 1 " + Emojis.comm + "commodity.\n");
+        }
+    }
+
+    private static void resolveEnvironmentPreserveAbility(Player player, PlanetModel planetModel, StringBuilder sb) {
+        if (!player.getHasUsedEnvironmentPreserveAbility() && player.hasAbility("policy_the_environment_preserve")) {
+            player.setHasUsedEnvironmentPreserveAbility(true);
+            String planetType = planetModel.getPlanetType().toString();
+            String fragmentType = Helper.getEmojiFromDiscord(StringUtils.left(planetType, 1) + "frag");
+            sb.append("> **The Environment - Preserve (+)**: You may reveal the top card of the " + Helper.getEmojiFromDiscord(planetType) + planetType + " deck; if it is a " + fragmentType + " relic fragment, gain it, otherwise discard that card.\n");
+        }
     }
 }
