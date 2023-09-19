5a503300b0e6793fb79ea993caf38c70224c0e34
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-09-19 14:32:48 -0400

    relic Representation

--- src/main/java/ti4/commands/explore/RelicLookAtTop.java
+++ src/main/java/ti4/commands/explore/RelicLookAtTop.java
@@ -3,11 +3,13 @@ package ti4.commands.explore;
 import java.util.List;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import ti4.generator.Mapper;
 import ti4.helpers.Constants;
 import ti4.helpers.Helper;
 import ti4.map.Game;
 import ti4.map.Player;
 import ti4.message.MessageHelper;
+import ti4.model.RelicModel;
 
 public class RelicLookAtTop extends GenericRelicAction {
     public RelicLookAtTop() {
@@ -22,8 +24,8 @@ public class RelicLookAtTop extends GenericRelicAction {
             return;
         }
         String relicID = relicDeck.get(0);
-      String sb = "**Relic - Look at Top**\n" + Helper.getPlayerRepresentation(player, activeGame) + "\n" +
-          Helper.getRelicRepresentation(relicID);
+        RelicModel relicModel = Mapper.getRelic(relicID);
+        String sb = "**Relic - Look at Top**\n" + Helper.getPlayerRepresentation(player, activeGame) + "\n" + relicModel.getSimpleRepresentation();
         MessageHelper.sendMessageToPlayerCardsInfoThread(player, activeGame, sb);
     }
 }
--- src/main/java/ti4/model/RelicModel.java
+++ src/main/java/ti4/model/RelicModel.java
@@ -1,8 +1,12 @@
 package ti4.model;
 
+import java.awt.Color;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import ti4.helpers.Emojis;
 
 @Data
@@ -33,5 +37,16 @@ public class RelicModel implements ModelInterface {
             case "ds" -> Emojis.DiscordantStars;
             default -> "";
         };
+    }
+
+    @JsonIgnore
+    public MessageEmbed getRepresentationEmbed() {
+        EmbedBuilder eb = new EmbedBuilder();
+        String name = getName() == null ? "" : getName();
+        eb.setTitle(Emojis.Relic + "__" + name + "__" + getSourceEmoji(), null);
+        eb.setColor(Color.yellow);
+        eb.setDescription(getText());
+        eb.setFooter("ID: " + getAlias());
+        return eb.build();
     }
 }
