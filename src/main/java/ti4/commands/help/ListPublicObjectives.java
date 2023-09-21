1269408eee604be0677755f77f70d00181c17bd8
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-09-21 13:37:40 -0400

    PO & SO embeds

--- src/main/java/ti4/commands/help/ListPublicObjectives.java
+++ src/main/java/ti4/commands/help/ListPublicObjectives.java
@@ -1,14 +1,16 @@
 package ti4.commands.help;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import ti4.generator.Mapper;
 import ti4.helpers.Constants;
+import ti4.helpers.Helper;
 import ti4.message.MessageHelper;
 import ti4.model.PublicObjectiveModel;
 
@@ -22,19 +24,18 @@ public class ListPublicObjectives extends HelpSubcommandData {
     @Override
     public void execute(SlashCommandInteractionEvent event) {
         String searchString = event.getOption(Constants.SEARCH, null, OptionMapping::getAsString);
-        HashMap<String, PublicObjectiveModel> poList = Mapper.getPublicObjectives();
-        List<String> searchedList = poList.entrySet().stream()
-            .map(e -> e.getKey() + " = " + e.getValue().getRepresentation())
-            .filter(s -> searchString == null || s.toLowerCase().contains(searchString.toLowerCase()))
-            .sorted().toList();
-        
-        String searchDescription = searchString == null ? "" : " search: " + searchString;
-        String message = "**__Public Objective List__**" + searchDescription + "\n" + String.join("\n", searchedList);
-        if (searchedList.size() > 5) {
-            String threadName = "/help list_public_objectives" + searchDescription;
-            MessageHelper.sendMessageToThread(event.getChannel(), threadName, message);
-        } else if (searchedList.size() > 0) {
-            event.getChannel().sendMessage(message).queue();
+
+        List<MessageEmbed> messageEmbeds = new ArrayList<>();
+
+        for (PublicObjectiveModel poModel : Mapper.getPublicObjectives().values().stream().sorted(PublicObjectiveModel.sortByPointsAndName).toList()) {
+            MessageEmbed representationEmbed = poModel.getRepresentationEmbed(true);
+            if (Helper.embedContainsSearchTerm(representationEmbed, searchString)) messageEmbeds.add(representationEmbed);
+        }
+        if (messageEmbeds.size() > 3) {
+            String threadName = event.getFullCommandName() + (searchString == null ? "" : " search: " + searchString);
+            MessageHelper.sendMessageEmbedsToThread(event.getChannel(), threadName, messageEmbeds);
+        } else if (messageEmbeds.size() > 0) {
+            event.getChannel().sendMessageEmbeds(messageEmbeds).queue();
         }
     }
 }
--- src/main/java/ti4/model/PublicObjectiveModel.java
+++ src/main/java/ti4/model/PublicObjectiveModel.java
@@ -1,5 +1,11 @@
 package ti4.model;
 
+import java.awt.Color;
+import java.util.Comparator;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import ti4.helpers.Emojis;
 import ti4.helpers.Helper;
 
 public class PublicObjectiveModel implements ModelInterface {
@@ -7,7 +13,7 @@ public class PublicObjectiveModel implements ModelInterface {
     private String name;
     private String phase;
     private String text;
-    private int points;
+    private Integer points;
     private String source;
 
   public boolean isValid() {
@@ -45,5 +51,51 @@ public class PublicObjectiveModel implements ModelInterface {
     public String getRepresentation() {
         String emoji = Helper.getEmojiFromDiscord("Public" + points + "alt");
         return emoji + "**__" + name + "__**: " + text + " (" + points + " VP)";
+    }
+
+    public static final Comparator<PublicObjectiveModel> sortByPointsAndName = (po1, po2) -> {
+        if (po1.getPoints() == po2.getPoints()) {
+            return po1.getName().compareTo(po2.getName());
+        } else {
+            return po1.getPoints() < po2.getPoints() ? -1 : 1;
+        }
+    };
+
+    public MessageEmbed getRepresentationEmbed(boolean includeID) {
+        EmbedBuilder eb = new EmbedBuilder();
+
+        //TITLE
+        StringBuilder title = new StringBuilder();
+        title.append(Helper.getEmojiFromDiscord("Public" + points + "alt"));
+        title.append("__**").append(getName()).append("**__");
+        title.append(getSourceEmoji());
+        eb.setTitle(title.toString());
+
+        //DESCRIPTION
+        StringBuilder description = new StringBuilder();
+        description.append(getText());
+        eb.setDescription(description.toString());
+
+        //FOOTER
+        StringBuilder footer = new StringBuilder();
+        if (includeID) footer.append("ID: ").append(getAlias()).append("    Source: ").append(getSource());
+        eb.setFooter(footer.toString());
+        
+        eb.setColor(getEmbedColour());
+        return eb.build();
+    }
+
+    private String getSourceEmoji() {
+        return switch (getSource()) {
+            default -> "";
+        };
+    }
+
+    public Color getEmbedColour() {
+        return switch (getPoints()) {
+            case 1 -> Color.ORANGE;
+            case 2 -> Color.BLUE;
+            default -> Color.WHITE;
+        };
     }
 }
