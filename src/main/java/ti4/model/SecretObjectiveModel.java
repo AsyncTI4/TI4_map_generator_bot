eb26fc9d8d4753666b6e951a8744a881105e575c
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-09-21 13:37:40 -0400

    PO & SO embeds

--- src/main/java/ti4/model/SecretObjectiveModel.java
+++ src/main/java/ti4/model/SecretObjectiveModel.java
@@ -1,5 +1,13 @@
 package ti4.model;
 
+import java.awt.Color;
+import java.util.Comparator;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import ti4.helpers.Emojis;
+import ti4.helpers.Helper;
+
 public class SecretObjectiveModel implements ModelInterface {
     private String alias;
     private String name;
@@ -39,5 +47,49 @@ public class SecretObjectiveModel implements ModelInterface {
 
     public String getSource() { 
         return source;
+    }
+
+    public static final Comparator<SecretObjectiveModel> sortByPointsAndName = (po1, po2) -> {
+        if (po1.getPoints() == po2.getPoints()) {
+            return po1.getName().compareTo(po2.getName());
+        } else {
+            return po1.getPoints() < po2.getPoints() ? -1 : 1;
+        }
+    };
+
+    public MessageEmbed getRepresentationEmbed(boolean includeID) {
+        EmbedBuilder eb = new EmbedBuilder();
+
+        //TITLE
+        StringBuilder title = new StringBuilder();
+        title.append(Helper.getEmojiFromDiscord(Emojis.SecretObjectiveAlt));
+        title.append("__**").append(getName()).append("**__");
+        title.append(getSourceEmoji());
+        eb.setTitle(title.toString());
+
+        //DESCRIPTION
+        StringBuilder description = new StringBuilder();
+        description.append(getText());
+        eb.setDescription(description.toString());
+
+        //FOOTER
+        StringBuilder footer = new StringBuilder();
+        if (includeID) footer.append("ID: ").append(getAlias()).append("    Source: ").append(getSource());
+        eb.setFooter(footer.toString());
+        
+        eb.setColor(getEmbedColour());
+        return eb.build();
+    }
+
+    private String getSourceEmoji() {
+        return switch (getSource()) {
+            default -> "";
+        };
+    }
+
+    public Color getEmbedColour() {
+        return switch (getPoints()) {
+            default -> Color.RED;
+        };
     }
 }
