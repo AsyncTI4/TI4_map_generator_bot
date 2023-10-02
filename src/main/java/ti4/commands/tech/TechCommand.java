4048b129c6bb55fc2d9cc9e410c363f4f7c3cde7
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-10-02 15:25:56 -0400

    skip FileIO for website-only map images

--- src/main/java/ti4/commands/tech/TechCommand.java
+++ src/main/java/ti4/commands/tech/TechCommand.java
@@ -8,6 +8,7 @@ import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEve
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import ti4.commands.Command;
+import ti4.generator.GenerateMap;
 import ti4.helpers.Constants;
 import ti4.map.Game;
 import ti4.map.GameManager;
@@ -65,6 +66,8 @@ public class TechCommand implements Command {
         String userID = event.getUser().getId();
         Game activeGame = GameManager.getInstance().getUserActiveGame(userID);
         GameSaveLoadManager.saveMap(activeGame, event);
+
+        GenerateMap.getInstance().saveImageToWebsiteOnly(activeGame, event);
     }
 
     protected String getActionDescription() {
