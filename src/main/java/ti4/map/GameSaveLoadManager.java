22423fe150749c5ec842a8ff84712f18ae523d49
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-10-14 19:05:32 -0400

    disable json saving, see if it helps with lagging

--- src/main/java/ti4/map/GameSaveLoadManager.java
+++ src/main/java/ti4/map/GameSaveLoadManager.java
@@ -121,14 +121,14 @@ public class GameSaveLoadManager {
             }
         }
 
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            mapper.writerWithDefaultPrettyPrinter().writeValue(Storage.getMapsJSONStorage(activeGame.getName() + JSON), activeGame);
-        } catch (IOException e) {
-            BotLogger.log(activeGame.getName() + ": IOException with JSON SAVER - likely a Role/Channel object - JSON SAVED INCORRECTLY", e);
-        } catch (Exception e) {
-            BotLogger.log("JSON SAVER", e);
-        }
+        // ObjectMapper mapper = new ObjectMapper();
+        // try {
+        //     mapper.writerWithDefaultPrettyPrinter().writeValue(Storage.getMapsJSONStorage(activeGame.getName() + JSON), activeGame);
+        // } catch (IOException e) {
+        //     BotLogger.log(activeGame.getName() + ": IOException with JSON SAVER - likely a Role/Channel object - JSON SAVED INCORRECTLY", e);
+        // } catch (Exception e) {
+        //     BotLogger.log("JSON SAVER", e);
+        // }
 
         if (loadFromJSON) return; //DON'T SAVE OVER OLD TXT SAVES IF LOADING AND SAVING FROM JSON
 
