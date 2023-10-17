bf68165863622aef9f9dc1062e3b00953c75a373
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-10-17 11:13:01 -0400

    Add Events Deck (#929)
    * initial copy of agenda
    
    * fix commands
    
    * rename Agenda -> Event
    
    * event deck options
    
    * EventModel and started converting commands
    
    * drop votecount
    
    * draw events
    
    * save/load events
    
    * delete unneeded
    
    * event reveal and show discarded
    
    * permanent/law events
    
    * event put top/bottom
    
    * discard to event deck, and convert event discards to linkedhashmap
    
    * fix event add
    
    * paint Event on Map
    
    * drop text2 from Event

---
+++ src/main/java/ti4/commands/event/EventSubcommandData.java
@@ -0,0 +1,48 @@
+package ti4.commands.event;
+
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+import org.jetbrains.annotations.NotNull;
+import ti4.map.Game;
+import ti4.map.GameManager;
+import ti4.message.MessageHelper;
+
+public abstract class EventSubcommandData extends SubcommandData {
+
+    private SlashCommandInteractionEvent event;
+    private Game activeGame;
+    private User user;
+
+    public String getActionID() {
+        return getName();
+    }
+
+    public EventSubcommandData(@NotNull String name, @NotNull String description) {
+        super(name, description);
+    }
+
+    public Game getActiveGame() {
+        return activeGame;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    /**
+     * Send a message to the event's channel, handles large text
+     * @param messageText new message
+     */
+    public void sendMessage(String messageText) {
+        MessageHelper.sendMessageToChannel(event.getMessageChannel(), messageText);
+    }
+
+    abstract public void execute(SlashCommandInteractionEvent event);
+
+    public void preExecute(SlashCommandInteractionEvent event) {
+        this.event = event;
+        user = event.getUser();
+        activeGame = GameManager.getInstance().getUserActiveGame(user.getId());
+    }
+}
