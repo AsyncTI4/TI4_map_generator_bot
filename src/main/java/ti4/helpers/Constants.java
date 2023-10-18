8385c4f35d27f96aebab9ba8f4ec6530c188eee1
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-10-17 23:30:07 -0400

    add check distance command

--- src/main/java/ti4/helpers/Constants.java
+++ src/main/java/ti4/helpers/Constants.java
@@ -955,4 +955,7 @@ public class Constants {
     public static final String RESET_EVENTS = "reset_events";
     public static final String REMOVE_EVENT = "remove_event";
     public static final String EVENTS_IN_EFFECT = "events_in_effect";
+    public static final String CHECK_DISTANCE = "check_distance";
+    public static final String MAX_DISTANCE = "max_distance";
+    public static final String SHOW_DISTANCES = "show_distances";
 }
