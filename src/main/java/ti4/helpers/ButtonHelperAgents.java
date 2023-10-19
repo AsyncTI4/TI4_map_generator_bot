a9cb82e91160120669aefbaebe70491c07cb940c
Chase Hinesman <chasehinesman@gmail.com>
2023-10-18 20:47:19 -0500

    null check on arbo agent, don't NPE on bad explores

--- src/main/java/ti4/helpers/ButtonHelperAgents.java
+++ src/main/java/ti4/helpers/ButtonHelperAgents.java
@@ -189,7 +189,7 @@ public class ButtonHelperAgents {
         UnitModel removedUnit = player.getUnitsByAsyncID(unitKey.asyncID()).get(0);
         for (String asyncID : allowedUnits) {
             UnitModel ownedUnit = player.getUnitFromAsyncID(asyncID);
-            if (ownedUnit.getCost() <= removedUnit.getCost() + 2) {
+            if (ownedUnit != null && ownedUnit.getCost() <= removedUnit.getCost() + 2) {
                 String buttonID = finChecker + "arboAgentPutShip_" + ownedUnit.getAsyncId() + "_" + tile.getPosition();
                 String buttonText = "Place " + ownedUnit.getName();
                 buttons.add(Button.danger(buttonID, buttonText).withEmoji(Emoji.fromFormatted(ownedUnit.getUnitEmoji())));
