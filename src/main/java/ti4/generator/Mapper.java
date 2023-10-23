cfe2e2ec619601acf6956c7f97afc08435e9929b
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-10-23 12:55:18 -0400

    fix optional usage

--- src/main/java/ti4/generator/Mapper.java
+++ src/main/java/ti4/generator/Mapper.java
@@ -305,7 +305,7 @@ public class Mapper {
 
     public static UnitModel getUnitModelByTechUpgrade(String techID) {
         return units.values().stream()
-            .filter(unitModel -> techID.equals(unitModel.getRequiredTechId()))
+            .filter(unitModel -> techID.equals(unitModel.getRequiredTechId().orElse("")))
             .findFirst()
             .orElse(null);
     }
