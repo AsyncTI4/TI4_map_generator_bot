cbf0b5d745b54d4ce36775e7254e852b92cdd7c8
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-10-23 13:15:45 -0400

    fix UnitModel validation

--- src/main/java/ti4/model/UnitModel.java
+++ src/main/java/ti4/model/UnitModel.java
@@ -63,7 +63,7 @@ public class UnitModel implements ModelInterface, EmbeddableModel {
             // && (requiredTechId == null || Mapper.isValidTech(requiredTechId))
             // && (upgradesFromUnitId == null || Mapper.isValidUnit(upgradesFromUnitId))
             // && (upgradesToUnitId == null || Mapper.isValidUnit(upgradesToUnitId))
-            && (Mapper.isFaction(getFaction().orElse("").toLowerCase()));
+            && (!getFaction().isPresent() || Mapper.isFaction(getFaction().orElse("").toLowerCase()));
     }
 
     public String getAlias() {
