d6d9fc61bf2d5c953bfc807672479c83fdd7bb5d
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-11-15 17:37:52 -0500

    working example

--- src/main/java/ti4/selections/SelectionMenuProvider.java
+++ src/main/java/ti4/selections/SelectionMenuProvider.java
@@ -1,19 +1,34 @@
 package ti4.selections;
 
 import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent;
+import ti4.message.BotLogger;
 
 public class SelectionMenuProvider {
     public static void resolveSelectionMenu(StringSelectInteractionEvent event) {
-        event.getChannel().sendMessage(getSelectionMenuDebugText(event)).queue();
+        event.reply(getSelectionMenuDebugText(event)).setEphemeral(true).queue();
 
-
-        String selectionMenuID = event.getComponentId();
+        SelectionManager selectionManager = SelectionManager.getInstance();
+        for (Selection selection : selectionManager.getSelectionMenuList()) {
+            if (selection.accept(event)) {
+                try {
+                    selection.execute(event);
+                    selection.postExecute(event);
+                } catch (Exception e) {
+                    String messageText = "Error trying to execute selection: " + event.getComponentId();
+                    BotLogger.log(event, messageText, e);
+                }
+                return;
+            }
+        }
+        event.getHook().deleteOriginal().queue();
     }
 
     private static String getSelectionMenuDebugText(StringSelectInteractionEvent event) {
         StringBuilder sb = new StringBuilder();
-        sb.append(event.getComponentId()).append("\n");
+        sb.append("You selected:\n```\n");
+        sb.append("MenuID: ").append(event.getComponentId()).append("\n");
         sb.append("Values: ").append(event.getValues()).append("\n");
+        sb.append("\n```");
         return sb.toString();
     }
 }
