af932e3667a9c22665086ebefd44993e753b3c13
James Rudolph <jimrudoph561@hotmail.com>
2023-11-28 22:44:27 -0500

    disable null e print

--- src/main/java/ti4/message/BotLogger.java
+++ src/main/java/ti4/message/BotLogger.java
@@ -60,15 +60,15 @@ public class BotLogger {
         logger.info(msg);
 
         //Adding so we dont cause an exception by attempting to log 
-        if(msg.length() > 2000){
+        if (msg.length() > 2000){
             String ellipses = "...(log message too long)";
             msg = msg.substring(0, 2000 - ellipses.length() - 1) + ellipses;
         }
 
         if (botLogChannel == null) {
             String name;
-            if(event == null) {
-              System.out.println(e);
+            if (event == null) {
+                // System.out.println(e);
                 return;
             } else {
                 name = event.getGuild().getName();
