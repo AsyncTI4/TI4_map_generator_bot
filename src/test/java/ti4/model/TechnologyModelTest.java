125cad47d2744e19cbba32cb1b3b4d4ba6a4483d
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2023-12-04 15:19:33 -0500

    add faction validation & fix "bad" faction in techs

--- src/test/java/ti4/model/TechnologyModelTest.java
+++ src/test/java/ti4/model/TechnologyModelTest.java
@@ -27,9 +27,17 @@ public class TechnologyModelTest {
     public void testTechnologies() {
         for (TechnologyModel techModel : Mapper.getTechs().values()) {
             assertTrue(techModel.isValid());
+            assertTrue(validateFaction(techModel));
             assertTrue(validateBaseUpgrade(techModel));
             assertTrue(validateHomebrewReplacesID(techModel));
         }
+    }
+
+    private boolean validateFaction(TechnologyModel techModel) {
+        if (techModel.getFaction().isEmpty()) return true;
+        if (Mapper.isFaction(techModel.getFaction().get())) return true;
+        BotLogger.log("Tech **" + techModel.getAlias() + "** failed validation due to invalid FactionID: `" + techModel.getFaction().get() + "`");
+        return false;
     }
 
     private boolean validateBaseUpgrade(TechnologyModel techModel) {
