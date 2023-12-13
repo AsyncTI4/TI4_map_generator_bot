ffc78fa0bf37cf2404a7066eac5ed1462b493fc1
Aaron Bruckner <aaronbruckner@gmail.com>
2023-12-12 19:37:46 -0800

    Removed unused Faction.java file (#1206)

--- src/main/java/ti4/draft/items/AbilityDraftItem.java
+++ src/main/java/ti4/draft/items/AbilityDraftItem.java
@@ -3,7 +3,6 @@ package ti4.draft.items;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import ti4.draft.DraftItem;
 import ti4.generator.Mapper;
-import ti4.map.Faction;
 import ti4.model.AbilityModel;
 import ti4.model.DraftErrataModel;
 import ti4.model.FactionModel;
