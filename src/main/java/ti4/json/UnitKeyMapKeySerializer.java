1f33e5674ba24116394ff4a40f91440d3bed0475
Aaron Bruckner <aaronbruckner@gmail.com>
2023-12-18 19:04:49 -0800

    Space JSON Serialization with Map-key special logic (#1228)

---
+++ src/main/java/ti4/json/UnitKeyMapKeySerializer.java
@@ -0,0 +1,24 @@
+package ti4.json;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import ti4.helpers.Units.UnitKey;
+
+/**
+ * JSON map keys can only be strings. So when UnitKey objects are used as Java map keys, we have to
+ * use the literal JSON string as the map key.
+ */
+public class UnitKeyMapKeySerializer extends JsonSerializer<UnitKey> {
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public void serialize(UnitKey value, JsonGenerator gen, SerializerProvider serializers) throws IOException, JsonProcessingException {
+        gen.writeFieldName(mapper.writeValueAsString(value));
+    }
+}
