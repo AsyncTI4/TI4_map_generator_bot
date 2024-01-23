e27d7d4c0b944a806e8aabf6324c6ca6c4a558c8
James Rudolph <jimrudoph561@hotmail.com>
2024-01-23 14:29:11 -0500

    more standard buttons

--- src/main/java/ti4/buttons/Buttons.java
+++ src/main/java/ti4/buttons/Buttons.java
@@ -4,6 +4,7 @@ import net.dv8tion.jda.api.interactions.components.buttons.Button;
 
 public class Buttons {
     public static final Button GET_A_TECH = Button.success("acquireATech", "Get a Tech");
+    public static final Button GET_A_FREE_TECH = Button.success("acquireAFreeTech", "Get a Tech");
     public static final Button REDISTRIBUTE_CCs = Button.success("redistributeCCButtons", "Redistribute CCs");
 
 
