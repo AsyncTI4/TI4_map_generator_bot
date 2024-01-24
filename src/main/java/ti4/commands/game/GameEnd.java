4350ff6fb692cb9d576f158803b9d743297c48ed
Will Holton <willholton1@gmail.com>
2024-01-23 21:52:51 -0500

    Chronicle stats (#1383)
    * chronicle stats
    
    * Update GameEnd.java

--- src/main/java/ti4/commands/game/GameEnd.java
+++ src/main/java/ti4/commands/game/GameEnd.java
@@ -236,10 +236,17 @@ public class GameEnd extends GameSubcommandData {
             int gamesWithWinnerCount = winningPathCounts.values().stream().reduce(0, Integer::sum);
             String winningPath = GameStats.getWinningPath(game, game.getWinner().get());
             sb.append("**Winning Path:** ").append(winningPath).append("\n");
-            int winningPathCount = winningPathCounts.get(winningPath);
-            sb.append("Out of ").append(gamesWithWinnerCount).append(" similar games, this path has been seen ")
-                .append(winningPathCount).append(" (").append(100 * winningPathCount / (double) gamesWithWinnerCount)
-                .append("%) times before!").append("\n");
+            int winningPathCount = winningPathCounts.get(winningPath) - 1;
+            double winningPathPercent = gamesWithWinnerCount == 0 ? 0 : Math.round(100 * winningPathCount / (double) gamesWithWinnerCount);
+            sb.append("Out of ").append(gamesWithWinnerCount).append(" similar games, this path has been seen ").append(winningPathCount)
+                .append(" times before! That's ").append(winningPathPercent).append("% of games!").append("\n");
+            if (winningPathPercent == 1) {
+              sb.append("🎉__**Congratulations on your unique victory!**__🎉").append("\n");
+            } else if (winningPathPercent == 0) {
+              sb.append("🤯__**What an incredible path, good job!**__🤯").append("\n");
+            } else if (winningPathCount == 0) {
+              sb.append("🥳__**An async first - may your victory live on in the halls of fame!**__🥳").append("\n");
+            }
         }
 
         return sb.toString();
