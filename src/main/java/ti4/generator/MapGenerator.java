8c0b315aea4dc312de3dfe7d502defd0791cb529
Fingon00 <britainfan234@gmail.com>
2024-02-18 00:58:24 -0500

    Revert "Revert "Lotsa stuffs""
    This reverts commit d8c6ead2da842064ec118d7e34db9e0b02d29c1f.

--- src/main/java/ti4/generator/MapGenerator.java
+++ src/main/java/ti4/generator/MapGenerator.java
@@ -563,10 +563,15 @@ public class MapGenerator {
                         if (sc == ButtonHelper.getKyroHeroSC(game)) {
                             graphics.drawString("" + (game.getSCList().size() + 1), x + 90, y + 70 + yDelta);
                         } else {
-                            graphics.drawString(scText, x + 90, y + 70 + yDelta);
+                            if(sc==1){
+                                graphics.drawString(scText, x + 102, y + 70 + yDelta);
+                            }else{
+                                graphics.drawString(scText, x + 90, y + 70 + yDelta);
+                            }
                             if (getSCColor(sc, game).equals(Color.GRAY)) {
                                 graphics.setFont(Storage.getFont40());
                                 graphics.setColor(Color.RED);
+                                
                                 graphics.drawString("X", x + 100, y + 60 + yDelta);
                             }
                             
