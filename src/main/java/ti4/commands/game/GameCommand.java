c11916a1cbb2400a213d83b76851d0e7f21a7bb4
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2024-02-21 14:53:46 -0500

    Refresh Info Buttons (#1456)
    * default buttons
    
    * add info buttons
    
    * relic embeds

--- src/main/java/ti4/commands/game/GameCommand.java
+++ src/main/java/ti4/commands/game/GameCommand.java
@@ -15,6 +15,7 @@ import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import net.dv8tion.jda.api.utils.FileUpload;
+import ti4.buttons.Buttons;
 import ti4.commands.Command;
 import ti4.generator.MapGenerator;
 import ti4.helpers.Constants;
@@ -85,6 +86,7 @@ public class GameCommand implements Command {
                     buttons.add(linkToWebsite);
                 }
                 buttons.add(Button.success("cardsInfo", "Cards Info"));
+                buttons.add(Buttons.REFRESH_INFO);
                 buttons.add(Button.primary("offerDeckButtons", "Show Decks"));
                 buttons.add(Button.secondary("showGameAgain", "Show Game"));
                 MessageHelper.sendFileToChannelWithButtonsAfter(event.getMessageChannel(), fileUpload, "", buttons);
