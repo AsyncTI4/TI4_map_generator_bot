29abd067bb7a10351477eb3227f57e0a916eeb67
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2024-02-27 11:08:33 -0500

    refactor TechExhaust to prep for further refactor

--- src/main/java/ti4/commands/tech/TechExhaust.java
+++ src/main/java/ti4/commands/tech/TechExhaust.java
@@ -2,6 +2,7 @@ package ti4.commands.tech;
 
 import java.util.List;
 
+import net.dv8tion.jda.api.events.interaction.GenericInteractionCreateEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import ti4.generator.Mapper;
@@ -11,6 +12,7 @@ import ti4.helpers.Constants;
 import ti4.map.Game;
 import ti4.map.Player;
 import ti4.message.MessageHelper;
+import ti4.model.TemporaryCombatModifierModel;
 
 public class TechExhaust extends TechAddRemove {
     public TechExhaust() {
@@ -19,21 +21,24 @@ public class TechExhaust extends TechAddRemove {
 
     @Override
     public void doAction(Player player, String techID, SlashCommandInteractionEvent event) {
-        Game activeGame = getActiveGame();
+        exhaustTechAndResolve(event, getActiveGame(), player, techID);
+        checkAndApplyCombatMods(event, player, techID);
+    }
+
+    private void exhaustTechAndResolve(GenericInteractionCreateEvent event, Game game, Player player, String techID) {
         player.exhaustTech(techID);
         if ("mi".equalsIgnoreCase(techID)) {
-            List<Button> buttons = AgendaHelper.getPlayerOutcomeButtons(activeGame, null, "getACFrom", null);
-            MessageHelper.sendMessageToChannelWithButtons(event.getMessageChannel(),
-                "Select who you would like to mageon.", buttons);
+            List<Button> buttons = AgendaHelper.getPlayerOutcomeButtons(game, null, "getACFrom", null);
+            MessageHelper.sendMessageToChannelWithButtons(event.getMessageChannel(), "Select who you would like to Mageon.", buttons);
         }
+        MessageHelper.sendMessageToChannel(event.getMessageChannel(), player.getRepresentation() + " exhausted tech: " + Mapper.getTech(techID).getRepresentation(false));
+    }
 
-        var posssibleCombatMod = CombatTempModHelper.GetPossibleTempModifier(Constants.TECH, techID,
-            player.getNumberTurns());
-        if (posssibleCombatMod != null) {
-            player.addNewTempCombatMod(posssibleCombatMod);
-            sendMessage("Combat modifier will be applied next time you push the combat roll button.");
+    private void checkAndApplyCombatMods(GenericInteractionCreateEvent event, Player player, String techID) {
+        TemporaryCombatModifierModel possibleCombatMod = CombatTempModHelper.GetPossibleTempModifier(Constants.TECH, techID, player.getNumberTurns());
+        if (possibleCombatMod != null) {
+            player.addNewTempCombatMod(possibleCombatMod);
+            MessageHelper.sendMessageToChannel(event.getMessageChannel(), "Combat modifier will be applied next time you push the combat roll button.");
         }
-
-        sendMessage(player.getRepresentation() + " exhausted tech: " + Mapper.getTech(techID).getRepresentation(false));
     }
 }
