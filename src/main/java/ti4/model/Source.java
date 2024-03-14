e03939f7c1f7db52b380efdc511501efec73d66f
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2024-03-13 22:21:43 -0400

    add Project Pi (#1522)

--- src/main/java/ti4/model/Source.java
+++ src/main/java/ti4/model/Source.java
@@ -13,7 +13,7 @@ public class Source {
         ds, absol, franken,
 
         // lil homebrew
-        lazax, action_deck_2, action_deck_2_old, keleresplus, little_omega, projectpi,
+        lazax, action_deck_2, action_deck_2_old, keleresplus, little_omega, project_pi,
 
         // async homebrew
         admins, pbd100, testsource, pbd2000,
@@ -48,7 +48,7 @@ public class Source {
                 case admins -> Emojis.AdminsFaction;
                 case ignis_aurora, pbd2000 -> Emojis.IgnisAurora;
                 case keleresplus -> Emojis.KeleresPlus;
-                case projectpi -> Emojis.ProjectPi;
+                case project_pi -> Emojis.ProjectPi;
                 case miltymod -> Emojis.MiltyMod;
                 case lazax -> Emojis.Lazax;
                 default -> "";
