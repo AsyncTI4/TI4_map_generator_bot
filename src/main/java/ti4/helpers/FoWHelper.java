bb9b16851ee2f956bf02a65f4692c298c0107c9a
Chase Hinesman <chasehinesman@gmail.com>
2024-03-13 21:02:13 -0500

    Update FoWHelper.java again

--- src/main/java/ti4/helpers/FoWHelper.java
+++ src/main/java/ti4/helpers/FoWHelper.java
@@ -27,6 +27,7 @@ import ti4.map.Tile;
 import ti4.map.UnitHolder;
 import ti4.message.BotLogger;
 import ti4.message.MessageHelper;
+import ti4.model.BorderAnomalyHolder;
 import ti4.model.WormholeModel;
 
 public class FoWHelper {
