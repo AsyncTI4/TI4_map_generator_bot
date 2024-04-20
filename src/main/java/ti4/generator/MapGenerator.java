5b6d7a0589aca6d8e3a2d47991d1daa9572f23f4
ForlornGeas <amenvault@gmail.com>
2024-04-20 12:24:15 -0500

    Add logic to draw peaked POs onto the map.

--- src/main/java/ti4/generator/MapGenerator.java
+++ src/main/java/ti4/generator/MapGenerator.java
@@ -3225,6 +3225,24 @@ public class MapGenerator {
 
             graphics.drawRect(x - 4, y - 5, 785, 38);
 
+            if (activeGame.getPublicObjectives1Peaked().containsKey(unRevealed)) {
+                List<Player> players = activeGame.getPublicObjectives1Peaked().get(unRevealed);
+
+                int xPos = x + 750;
+                for (Player player : players) {
+                    graphics.drawString("hi", xPos, y + 23);
+                    xPos -= 18;
+                }
+            } else if (activeGame.getPublicObjectives2Peaked().containsKey(unRevealed)) {
+                List<Player> players = activeGame.getPublicObjectives2Peaked().get(unRevealed);
+
+                int xPos = x + 750;
+                for (Player player : players) {
+                    graphics.drawString(player.getDisplayName().substring(0, 0), xPos, y + 23);
+                    xPos -= 18;
+                }
+            }
+
             y += 43;
         }
         return y;
