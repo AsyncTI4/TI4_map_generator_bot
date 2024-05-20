1a625e236657b844957647db2eae2bff98cc0479
Esko Poutanen <esko.pylsy@gmail.com>
2024-05-20 19:23:10 +0300

    Franken draft version to pick 1 at a time (#1780)

--- src/main/java/ti4/commands/franken/StartFrankenDraft.java
+++ src/main/java/ti4/commands/franken/StartFrankenDraft.java
@@ -5,6 +5,7 @@ import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import ti4.draft.FrankenDraft;
+import ti4.draft.OnePickFrankenDraft;
 import ti4.draft.PoweredFrankenDraft;
 import ti4.helpers.Constants;
 import ti4.helpers.FrankenDraftHelper;
@@ -16,8 +17,9 @@ import ti4.message.MessageHelper;
 public class StartFrankenDraft extends FrankenSubcommandData {
     public StartFrankenDraft() {
         super(Constants.START_FRANKEN_DRAFT, "Start a franken draft");
-        addOptions(new OptionData(OptionType.BOOLEAN, Constants.POWERED, "'True' to add 1 extra faction tech/ability (Default: False)"));
         addOptions(new OptionData(OptionType.BOOLEAN, Constants.FORCE, "'True' to forcefully overwrite existing faction setups (Default: False)"));
+        addOptions(new OptionData(OptionType.BOOLEAN, Constants.POWERED, "'True' to add 1 extra faction tech/ability (Default: False)"));
+        addOptions(new OptionData(OptionType.BOOLEAN, Constants.ONEPICK, "'True' to draft 1 item at a time. Default mode is first 3 then 2"));
     }
 
     @Override
@@ -30,13 +32,21 @@ public class StartFrankenDraft extends FrankenSubcommandData {
             MessageHelper.sendMessageToChannel(event.getMessageChannel(), message);
             return;
         }
+        
+        boolean powered = event.getOption(Constants.POWERED, false, OptionMapping::getAsBoolean);
+        boolean onepick = event.getOption(Constants.ONEPICK, false, OptionMapping::getAsBoolean);
+        if (powered && onepick) {
+          MessageHelper.sendMessageToChannel(event.getMessageChannel(), "Choose only one mode, Powered or OnePick, not both.");
+          return;
+        }
 
         FrankenDraftHelper.setUpFrankenFactions(game, event, force);
         FrankenDraftHelper.clearPlayerHands(game);
 
-        boolean powered = event.getOption(Constants.POWERED, false, OptionMapping::getAsBoolean);
         if (powered) {
             game.setBagDraft(new PoweredFrankenDraft(game));
+        } else if (onepick) {
+            game.setBagDraft(new OnePickFrankenDraft(game));
         } else {
             game.setBagDraft(new FrankenDraft(game));
         }
