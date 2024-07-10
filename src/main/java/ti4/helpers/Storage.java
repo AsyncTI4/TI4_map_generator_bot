df843bcaa0aa44b12b9d4ed398fa0ca98e875e24
Will Holton <willholton1@gmail.com>
2024-07-09 21:02:07 -0400

    parallel-load (#1894)
    * parallel-load
    
    * Update GameSaveLoadManager.java
    
    * Update GameSaveLoadManager.java
    
    * Update AsyncTI4DiscordBot.java
    
    * Update GameSaveLoadManager.java
    
    * parallel load - do more concurrency fixes
    
    * concurrenthashmap
    
    * Update GameManager.java
    
    * Update GameSaveLoadManager.java
    
    * Update GameSaveLoadManager.java
    
    ---------
    
    Co-authored-by: Chase Hinesman <chasehinesman@gmail.com>

--- src/main/java/ti4/helpers/Storage.java
+++ src/main/java/ti4/helpers/Storage.java
@@ -234,52 +234,42 @@ public class Storage {
         return tiFont;
     }
 
-    @Nullable
     public static File getMapUndoStorage(String mapName) {
         return new File(getStoragePath() + MAPS_UNDO + mapName);
     }
 
-    @Nullable
     public static File getMapUndoDirectory() {
         return new File(getStoragePath() + MAPS_UNDO);
     }
 
-    @Nullable
     public static File getMapImageStorage(String mapName) {
         return new File(getStoragePath() + MAPS + mapName);
     }
 
-    @Nullable
     public static File getMapImageDirectory() {
         return new File(getStoragePath() + MAPS);
     }
 
-    @Nullable
     public static File getMapStorage(String mapName) {
         return new File(getStoragePath() + MAPS + mapName);
     }
 
-    @Nullable
     public static File getDeletedMapStorage(String mapName) {
         return new File(getStoragePath() + DELETED_MAPS + mapName);
     }
 
-    @Nullable
     public static File getTTPGExportDirectory() {
         return new File(getStoragePath() + TTPG_EXPORTS);
     }
 
-    @Nullable
     public static File getTTPGExportStorage(String fileName) {
         return new File(getStoragePath() + TTPG_EXPORTS + fileName);
     }
 
-    @Nullable
     public static File getMapsJSONDirectory() {
         return new File(getStoragePath() + MAPS_JSON);
     }
 
-    @Nullable
     public static File getMapsJSONStorage(String fileName) {
         return new File(getStoragePath() + MAPS_JSON + fileName);
     }
