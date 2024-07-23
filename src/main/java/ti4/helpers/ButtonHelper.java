f8e668888fbc9ebdcfc09eda2c90c34e43e685d8
Fingon00 <britainfan234@gmail.com>
2024-07-22 22:02:55 -0400

    Last push tonight, I promse

--- src/main/java/ti4/helpers/ButtonHelper.java
+++ src/main/java/ti4/helpers/ButtonHelper.java
@@ -8042,6 +8042,21 @@ public class ButtonHelper {
         }
     }
 
+    public static boolean isPlayerNew(Game gameOG, Player player) {
+
+        Map<String, Game> mapList = GameManager.getInstance().getGameNameToGame();
+        for (Game game : mapList.values()) {
+            if (!game.getName().equalsIgnoreCase(gameOG.getName())) {
+                for (Player player2 : game.getRealPlayers()) {
+                    if (player2.getUserID().equalsIgnoreCase(player.getUserID())) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
     public static void offerAFKTimeOptions(Game game, Player player) {
         List<Button> buttons = getSetAFKButtons(game);
         player.setHoursThatPlayerIsAFK("");
