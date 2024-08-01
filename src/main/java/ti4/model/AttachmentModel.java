a4bb46cce521cba409875fca1a6e30abdde37d19
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2024-07-31 22:33:17 -0400

    split attachments

--- src/main/java/ti4/model/AttachmentModel.java
+++ src/main/java/ti4/model/AttachmentModel.java
@@ -3,11 +3,14 @@ package ti4.model;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
 import lombok.Data;
+import ti4.model.Source.ComponentSource;
 
 @Data
 public class AttachmentModel implements ModelInterface {
     private String id;
+    private String name;
     private String imagePath;
     private List<String> techSpeciality = new ArrayList<>();
     private List<String> planetTypes = new ArrayList<>();
@@ -20,12 +23,13 @@ public class AttachmentModel implements ModelInterface {
 
     private int spaceCannonHitsOn;
     private int spaceCannonDieCount;
-    private String source;
+    private ComponentSource source;
 
     @Override
     public boolean isValid() {
         return id != null
-            && imagePath != null;
+            && imagePath != null
+            && source != null;
     }
 
     @Override
@@ -33,8 +37,28 @@ public class AttachmentModel implements ModelInterface {
         return getId(); // looks like were using the attachment_<name>.png for identification for now.
     }
 
+    public String getName() {
+        return Optional.ofNullable(name).orElse(id);
+    }
+
+    public String getRepresentation() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getId());
+        if (name != null) sb.append (" ").append(getName());
+        if (resourcesModifier != 0) sb.append(" R" + resourcesModifier);
+        if (influenceModifier != 0) sb.append(" I" + influenceModifier);
+
+        if (isLegendary) sb.append(" Legendary ");
+        if (isFakeAttachment) sb.append(" [FAKE]");
+        return getName() + " ";
+    }
+
     public boolean isFakeAttachment() {
         return Optional.ofNullable(isFakeAttachment).orElse(false);
+    }
+
+    public boolean isRealAttachment() {
+        return !isFakeAttachment();
     }
 
     public boolean isLegendary() {
