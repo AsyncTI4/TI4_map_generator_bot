ca62285ee04382052be48969a71a7eeadd56fd1b
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2024-09-18 15:45:09 -0400

    PlayerPreferenceHelper

--- src/main/java/ti4/helpers/ButtonHelper.java
+++ src/main/java/ti4/helpers/ButtonHelper.java
@@ -4816,45 +4816,6 @@ public class ButtonHelper {
         return buttons;
     }
 
-    public static void offerSetAutoPassOnSaboButtons(Game game, Player player2) {
-        List<Button> buttons = new ArrayList<>();
-        int x = 1;
-        buttons.add(Buttons.gray("setAutoPassMedian_" + x, "" + x));
-        x = 2;
-        buttons.add(Buttons.gray("setAutoPassMedian_" + x, "" + x));
-        x = 4;
-        buttons.add(Buttons.gray("setAutoPassMedian_" + x, "" + x));
-        x = 6;
-        buttons.add(Buttons.gray("setAutoPassMedian_" + x, "" + x));
-        x = 8;
-        buttons.add(Buttons.gray("setAutoPassMedian_" + x, "" + x));
-        x = 16;
-        buttons.add(Buttons.gray("setAutoPassMedian_" + x, "" + x));
-        x = 24;
-        buttons.add(Buttons.gray("setAutoPassMedian_" + x, "" + x));
-        x = 36;
-        buttons.add(Buttons.gray("setAutoPassMedian_" + x, "" + x));
-        buttons.add(Buttons.red("deleteButtons", "Decline"));
-        x = 0;
-        buttons.add(Buttons.red("setAutoPassMedian_" + x, "Turn off (if already on)"));
-        if (player2 == null) {
-            for (Player player : game.getRealPlayers()) {
-                String message = player.getRepresentation(true, true)
-                    + " you may choose to automatically pass on Sabos after a random amount of time if you don't have a Sabo/Instinct Training/Watcher mechs."
-                    + " How it works is you secretly set a median time (in hours) here, and then from now on when an AC is played, the bot will randomly react for you, 50% of the time being above that amount of time and 50% below."
-                    + " It's random so people can't derive much information from it. You are free to decline, no-one will ever know either way, but if necessary you may change your time later with /player stats.";
-                MessageHelper.sendMessageToChannelWithButtons(player.getCardsInfoThread(), message, buttons);
-            }
-        } else {
-            Player player = player2;
-            String message = player.getRepresentation(true, true)
-                + " you may choose to automatically pass on Sabos after a random amount of time if you don't have a Sabo/Instinct Training/Watcher mechs. "
-                + " How it works is you secretly set a median time (in hours) here, and then from now on when an AC is played, the bot will randomly react for you, 50% of the time being above that amount of time and 50% below."
-                + " It's random so people can't derive much information from it. You are free to decline, no-one will ever know either way, but if necessary you may change your time later with /player stats.";
-            MessageHelper.sendMessageToChannelWithButtons(player.getCardsInfoThread(), message, buttons);
-        }
-    }
-
     public static UnitHolder getUnitHolderFromPlanetName(String planetName, Game game) {
         Tile tile = game.getTileFromPlanet(AliasHandler.resolvePlanet(planetName.toLowerCase()));
         if (tile == null) {
@@ -7343,30 +7304,6 @@ public class ButtonHelper {
         return true;
     }
 
-    public static void offerAFKTimeOptions(Game game, Player player) {
-        List<Button> buttons = getSetAFKButtons(game);
-        player.setHoursThatPlayerIsAFK("");
-        MessageHelper.sendMessageToChannelWithButtons(player.getCardsInfoThread(), player.getRepresentation(true, true)
-            + " your afk times (if any) have been reset. Use buttons to select the hours (note they are in UTC) in which you're afk. If you select 8 for example, you will be set as AFK from 8:00 UTC to 8:59 UTC in every game you are in.",
-            buttons);
-    }
-
-    public static void offerDirectHitManagementOptions(Game game, Player player) {
-        List<Button> buttons = getDirectHitManagementButtons(game, player);
-        MessageHelper.sendMessageToChannelWithButtons(player.getCardsInfoThread(), player.getRepresentation(true, true)
-            + " select the units you would like to either risk or not risk Direct Hit. Upgraded dreadnoughts will automatically risk Direct Hits.  ",
-            buttons);
-    }
-
-    public static List<Button> getSetAFKButtons(Game game) {
-        List<Button> buttons = new ArrayList<>();
-        for (int x = 0; x < 24; x++) {
-            buttons.add(Buttons.gray("setHourAsAFK_" + x, "" + x));
-        }
-        buttons.add(Buttons.red("deleteButtons", "Done"));
-        return buttons;
-    }
-
     public static boolean anyoneHaveInPlayArea(Game game, String pnID) {
         for (Player player : game.getRealPlayers()) {
             if (player.getPromissoryNotesInPlayArea().contains(pnID)) {
@@ -7374,64 +7311,6 @@ public class ButtonHelper {
             }
         }
         return false;
-    }
-
-    public static List<Button> getDirectHitManagementButtons(Game game, Player player) {
-        List<Button> buttons = new ArrayList<>();
-        String stuffNotToSustain = game
-            .getStoredValue("stuffNotToSustainFor" + player.getFaction());
-        if (stuffNotToSustain.isEmpty()) {
-            game.setStoredValue("stuffNotToSustainFor" + player.getFaction(), "warsun");
-            stuffNotToSustain = "warsun";
-        }
-        String unit = "warsun";
-        if (stuffNotToSustain.contains(unit)) {
-            buttons.add(Buttons.red("riskDirectHit_" + unit + "_yes", "Risk " + StringUtils.capitalize(unit)));
-        } else {
-            buttons.add(Buttons.green("riskDirectHit_" + unit + "_no", "Don't Risk " + StringUtils.capitalize(unit)));
-        }
-        unit = "flagship";
-        if (stuffNotToSustain.contains(unit)) {
-            buttons.add(Buttons.red("riskDirectHit_" + unit + "_yes", "Risk " + StringUtils.capitalize(unit)));
-        } else {
-            buttons.add(Buttons.green("riskDirectHit_" + unit + "_no", "Don't Risk " + StringUtils.capitalize(unit)));
-        }
-        unit = "dreadnought";
-        if (stuffNotToSustain.contains(unit)) {
-            buttons.add(Buttons.red("riskDirectHit_" + unit + "_yes", "Risk " + StringUtils.capitalize(unit)));
-        } else {
-            buttons.add(Buttons.green("riskDirectHit_" + unit + "_no", "Don't Risk " + StringUtils.capitalize(unit)));
-        }
-        unit = "cruiser";
-        if (player.hasTech("se2")) {
-            if (stuffNotToSustain.contains(unit)) {
-                buttons.add(Buttons.red("riskDirectHit_" + unit + "_yes", "Risk " + StringUtils.capitalize(unit)));
-            } else {
-                buttons.add(
-                    Buttons.green("riskDirectHit_" + unit + "_no", "Don't Risk " + StringUtils.capitalize(unit)));
-            }
-        }
-        buttons.add(Buttons.gray("deleteButtons", "Done"));
-        return buttons;
-    }
-
-    public static void resolveRiskDirectHit(Game game, Player player, ButtonInteractionEvent event, String buttonID) {
-        String yesOrNo = buttonID.split("_")[2];
-        String unit = buttonID.split("_")[1];
-        String stuffNotToSustain = game
-            .getStoredValue("stuffNotToSustainFor" + player.getFaction());
-        if ("yes".equalsIgnoreCase(yesOrNo)) {
-            stuffNotToSustain = stuffNotToSustain.replace(unit, "");
-        } else {
-            stuffNotToSustain = stuffNotToSustain + unit;
-        }
-        if (stuffNotToSustain.isEmpty()) {
-            stuffNotToSustain = "none";
-        }
-        game.setStoredValue("stuffNotToSustainFor" + player.getFaction(), stuffNotToSustain);
-        List<Button> systemButtons = getDirectHitManagementButtons(game, player);
-        event.getMessage().editMessage(event.getMessage().getContentRaw())
-            .setComponents(turnButtonListIntoActionRowList(systemButtons)).queue();
     }
 
     public static List<Button> getAllPossibleCompButtons(Game game, Player p1, GenericInteractionCreateEvent event) {
@@ -8273,89 +8152,6 @@ public class ButtonHelper {
                 player.removeRelic(relic);
             }
         }
-    }
-
-    public static void offerPlayerPreferences(Player player, ButtonInteractionEvent event) {
-        List<Button> buttons = new ArrayList<>();
-        buttons.add(Buttons.gray("playerPref_autoSaboReact", "Change Auto No-Sabo React Time", Emojis.ActionCard));
-        buttons.add(Buttons.gray("playerPref_afkTimes", "Change AFK Times"));
-        buttons.add(Buttons.gray("playerPref_tacticalAction", "Change Distance-Based Tactical Action Preference"));
-        buttons.add(Buttons.gray("playerPref_autoNoWhensAfters", "Change Auto No Whens/Afters React", Emojis.Agenda));
-        buttons.add(SetPersonalPingInterval.OFFER_PING_OPTIONS_BUTTON);
-        buttons.add(Buttons.gray("playerPref_directHitManagement", "Tell The Bot What Units Not To Risk Direct Hit On"));
-        MessageHelper.sendMessageToChannel(player.getCardsInfoThread(), player.getRepresentation() + " Choose the thing you wish to change", buttons);
-    }
-
-    public static void resolvePlayerPref(Player player, ButtonInteractionEvent event, String buttonID, Game game) {
-        String thing = buttonID.split("_")[1];
-        switch (thing) {
-            case "autoSaboReact" -> {
-                offerSetAutoPassOnSaboButtons(game, player);
-            }
-            case "afkTimes" -> {
-                offerAFKTimeOptions(game, player);
-            }
-            case "tacticalAction" -> {
-                List<Button> buttons = new ArrayList<>();
-                String msg = player.getRepresentation()
-                    + " Choose whether you want your tactical action buttons to be distance based (offer you 0 tiles away initially, then 1, 2, 3 tiles away upon more button presses) or ring based (choose what ring the active system is in). Default is ring based. This will apply to all your games";
-                buttons.add(Buttons.green("playerPrefDecision_true_distance", "Make it distance based"));
-                buttons.add(Buttons.green("playerPrefDecision_false_distance", "Make it ring based"));
-                MessageHelper.sendMessageToChannel(player.getCardsInfoThread(), msg, buttons);
-            }
-            case "autoNoWhensAfters" -> {
-                List<Button> buttons = new ArrayList<>();
-                String msg = player.getRepresentation()
-                    + " Choose whether you want the game to auto react no whens/afters after a random amount of time for you when you have no whens/afters. Default is off. This will only apply to this game. If you have any whens or afters or related when/after abilities, it will not do anything. ";
-                buttons.add(Buttons.green("playerPrefDecision_true_agenda", "Turn on"));
-                buttons.add(Buttons.green("playerPrefDecision_false_agenda", "Turn off"));
-                MessageHelper.sendMessageToChannel(player.getCardsInfoThread(), msg, buttons);
-            }
-            case "directHitManagement" -> {
-                offerDirectHitManagementOptions(game, player);
-            }
-        }
-        deleteMessage(event);
-    }
-
-    public static void resolvePlayerPrefDecision(Player player, ButtonInteractionEvent event, String buttonID,
-        Game game) {
-        String trueOrFalse = buttonID.split("_")[1];
-        String distanceOrAgenda = buttonID.split("_")[2];
-        if ("true".equals(trueOrFalse)) {
-            if ("distance".equals(distanceOrAgenda)) {
-                player.setPreferenceForDistanceBasedTacticalActions(true);
-                Map<String, Game> mapList = GameManager.getInstance().getGameNameToGame();
-                for (Game game2 : mapList.values()) {
-                    for (Player player2 : game2.getRealPlayers()) {
-                        if (player2.getUserID().equalsIgnoreCase(player.getUserID())) {
-                            player2.setPreferenceForDistanceBasedTacticalActions(true);
-                            GameSaveLoadManager.saveMap(game2, player2.getUserName() + " Updated Player Settings");
-                        }
-                    }
-                }
-            } else {
-                player.setAutoPassWhensAfters(true);
-            }
-        } else {
-            if ("distance".equals(distanceOrAgenda)) {
-                player.setPreferenceForDistanceBasedTacticalActions(false);
-                Map<String, Game> mapList = GameManager.getInstance().getGameNameToGame();
-                for (Game game2 : mapList.values()) {
-                    for (Player player2 : game2.getRealPlayers()) {
-                        if (player2.getUserID().equalsIgnoreCase(player.getUserID())) {
-                            player2.setPreferenceForDistanceBasedTacticalActions(false);
-                            GameSaveLoadManager.saveMap(game2, player2.getUserName() + " Updated Player Settings");
-                        }
-                    }
-                }
-            } else {
-                player.setAutoPassWhensAfters(false);
-            }
-        }
-        MessageHelper.sendMessageToChannel(player.getCardsInfoThread(), "Set setting successfully");
-
-        deleteMessage(event);
     }
 
     public static void offerSpeakerButtons(Game game, Player player) {
