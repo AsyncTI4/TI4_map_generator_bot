50ad9ca22ac49c961b817080e5319f2fa32d4631
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2024-10-16 23:16:41 -0400

    add javadoc to ButtonHandler outlining what variables will auto-populate via AnnotationHandler

--- src/main/java/ti4/listeners/annotations/ButtonHandler.java
+++ src/main/java/ti4/listeners/annotations/ButtonHandler.java
@@ -8,6 +8,26 @@ import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.events.interaction.GenericInteractionCreateEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import ti4.map.Game;
+import ti4.map.Player;
+
+/**
+ * <p>Supported Variables (see {@link AnnotationHandler})</p>
+ * <ul>
+ *   <li>events<ul>
+ *     <li>{@link GenericInteractionCreateEvent}</li>
+ *     <li>{@link ButtonInteractionEvent}</li>
+ *   </ul>
+ *   <li>{@link Game} -&gt game derived from channels</li>
+ *   <li>{@link Player} -&gt user who pressed button</li>
+ *   <li>{@link MessageChannel} -&gt; event.getMessageChannel()</li>
+ *   <li>{@link String} -&gt; buttonID</li>
+ * </ul>
+ * <p>To handle multiple String parameter you mus use {@link NamedParam}</p>
+ */
 @Documented
 @Retention(RUNTIME)
 @Repeatable(ManyButtonHandlers.class)
