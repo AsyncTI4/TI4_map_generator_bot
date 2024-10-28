8c0f07009778556ecf6880e4f95bf88906d42879
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2024-10-28 14:58:47 -0400

    flesh out PlayerStatsDashboardPayload

--- src/main/java/ti4/map/PlayerStatsDashboardPayload.java
+++ src/main/java/ti4/map/PlayerStatsDashboardPayload.java
@@ -1,9 +1,18 @@
 package ti4.map;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import ti4.generator.Mapper;
 import ti4.message.BotLogger;
+import ti4.model.RelicModel;
+import ti4.model.StrategyCardModel;
+import ti4.model.TechnologyModel;
 
 public class PlayerStatsDashboardPayload {
 
@@ -23,4 +32,136 @@ public class PlayerStatsDashboardPayload {
             return null;
         }
     }
+
+    public boolean isActive() {
+        return !player.isEliminated(); // TODO: verify what "active" actually means here
+    }
+
+    public List<String> getAlliances() {
+        return Collections.emptyList(); // TODO: list of colours
+    }
+
+    public String getColor() {
+        return player.getColor();
+    }
+
+    public String getColorActual() {
+        return getColor(); // I think TTPG has an underlying TTPG based colour ID, and a "TI4" colour which can be controlled via colour picker
+    }
+
+    public Map<String, Integer> getCommandTokens() {
+        return Map.of(
+            "fleet", player.getFleetCC(),
+            "strategy", player.getStrategicCC(),
+            "tactics", player.getTacticalCC()
+        /**/);
+    }
+
+    public int getCommodities() {
+        return player.getCommodities();
+    }
+
+    public int getMaxCommodities() {
+        return player.getCommoditiesTotal();
+    }
+
+    public int getCustodianPoints() {
+        return -1; //TODO: get custodian points
+    }
+
+    public String getFactionName() {
+        return player.getFactionModel().getFactionName();
+    }
+
+    public Map<String, Integer> getHandSummary() {
+        return Map.of(
+            "Secret Objectives", player.getSecrets().size(),
+            "Actions", player.getActionCards().size(), //TODO: add more card types? 
+            "Promissory", player.getPromissoryNotes().size()
+        /**/);
+    }
+
+    public List<String> getLaws() {
+        return Collections.emptyList(); //TODO
+    }
+
+    public List<Leader> getLeaders() {
+        return player.getLeaders();
+    }
+
+    public List<String> getObjectives() {
+        return Collections.emptyList(); //TODO
+    }
+
+    public Map<String, Object> getPlanetTotals() {
+        return Collections.emptyMap(); //TODO
+        /*
+         * "planetTotals": {
+         * "influence": {
+         * "avail": 2,
+         * "total": 15
+         * },
+         * "legendary": 1,
+         * "resources": {
+         * "avail": 0,
+         * "total": 16
+         * },
+         * "techs": {
+         * "blue": 0,
+         * "green": 2,
+         * "red": 0,
+         * "yellow": 0
+         * },
+         * "traits": {
+         * "cultural": 3,
+         * "hazardous": 0,
+         * "industrial": 3
+         * }
+         * }
+         */
+    }
+
+    public List<String> getRelicCards() {
+        return player.getRelics().stream()
+            .map(Mapper::getRelic)
+            .map(RelicModel::getName)
+            .toList();
+    }
+
+    public int getScore() {
+        return player.getTotalVictoryPoints();
+    }
+
+    public List<String> getStrategyCards() {
+        return player.getSCs().stream()
+            .map(sc -> player.getGame().getStrategyCardModelByInitiative(sc))
+            .filter(Optional::isPresent)
+            .map(Optional::get)
+            .map(StrategyCardModel::getName)
+            .toList();
+    }
+
+    public List<String> getTechnologies() {
+        return player.getTechs().stream()
+            .map(Mapper::getTech)
+            .map(TechnologyModel::getName)
+            .toList();
+    }
+
+    public int getTradeGoods() {
+        return player.getTg();
+    }
+
+    public int getTurnOrder() {
+        return -1; //TODO
+    }
+
+    public List<Object> getUnitModifiers() {
+        return Collections.emptyList(); //TODO maybe not? is this used?
+    }
+
+    public List<String> getUnitUpgrades() {
+        return Collections.emptyList(); //TODO maybe not needed, getTechnologies is good?
+    }
+
 }
