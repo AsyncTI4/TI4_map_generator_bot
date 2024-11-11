85df8714a708d788f0403c592076fca5bee9bc01
Will <willholton1@gmail.com>
2024-11-11 17:27:08 -0500

    f

--- src/main/java/ti4/commands/developer/DeveloperCommand.java
+++ src/main/java/ti4/commands/developer/DeveloperCommand.java
@@ -1,24 +1,28 @@
 package ti4.commands.developer;
 
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Objects;
+import java.util.List;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.interactions.commands.build.Commands;
-import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import ti4.AsyncTI4DiscordBot;
 import ti4.commands.Command;
+import ti4.commands.Subcommand;
 import ti4.helpers.Constants;
 import ti4.helpers.SlashCommandAcceptanceHelper;
 
 public class DeveloperCommand implements Command {
 
-    private final Collection<DeveloperSubcommandData> subcommandData = getSubcommands();
+    private final Collection<Subcommand> subcommands = List.of(
+            new SetGlobalSetting(),
+            new RunManualDataMigration());
 
     @Override
     public String getActionId() {
         return Constants.DEVELOPER;
+    }
+
+    public String getActionDescription() {
+        return "Developer";
     }
 
     @Override
@@ -28,32 +32,7 @@ public class DeveloperCommand implements Command {
     }
 
     @Override
-    public void execute(SlashCommandInteractionEvent event) {
-        String subcommandName = event.getInteraction().getSubcommandName();
-        for (DeveloperSubcommandData subcommand : subcommandData) {
-            if (Objects.equals(subcommand.getName(), subcommandName)) {
-                subcommand.preExecute(event);
-                subcommand.execute(event);
-                break;
-            }
-        }
-    }
-
-    protected String getActionDescription() {
-        return "Developer";
-    }
-
-    private Collection<DeveloperSubcommandData> getSubcommands() {
-        Collection<DeveloperSubcommandData> subcommands = new HashSet<>();
-        subcommands.add(new SetGlobalSetting());
-        subcommands.add(new RunManualDataMigration());
+    public Collection<Subcommand> getSubcommands() {
         return subcommands;
-    }
-
-    @Override
-    public void registerCommands(CommandListUpdateAction commands) {
-        commands.addCommands(
-            Commands.slash(getActionId(), getActionDescription())
-                .addSubcommands(getSubcommands()));
     }
 }
