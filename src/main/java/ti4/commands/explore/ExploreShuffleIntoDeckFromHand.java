a7a3520a74a5595c19e9175f72ea493fcf941b60
Will <willholton1@gmail.com>
2024-11-12 07:59:51 -0500

    f

--- src/main/java/ti4/commands/explore/ExploreShuffleIntoDeckFromHand.java
+++ src/main/java/ti4/commands/explore/ExploreShuffleIntoDeckFromHand.java
@@ -1,30 +1,27 @@
 package ti4.commands.explore;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import ti4.commands.PlayerGameStateSubcommand;
 import ti4.generator.Mapper;
 import ti4.helpers.Constants;
-import ti4.helpers.Helper;
 import ti4.map.Game;
 import ti4.map.Player;
 import ti4.message.MessageHelper;
 import ti4.model.ExploreModel;
 
-public class ExploreShuffleIntoDeckFromHand extends ExploreSubcommandData {
+public class ExploreShuffleIntoDeckFromHand extends PlayerGameStateSubcommand {
 
     public ExploreShuffleIntoDeckFromHand() {
-        super(Constants.SHUFFLE_INTO_DECK_FROM_HAND, "Discard an Exploration Card from the hand to deck.");
-        addOptions(idOption.setRequired(true));
+        super(Constants.SHUFFLE_INTO_DECK_FROM_HAND, "Discard an Exploration Card from the hand to deck.", true, true);
+        addOptions(new OptionData(OptionType.STRING, Constants.EXPLORE_CARD_ID, "Explore card ids. May include multiple comma-separated ids.").setRequired(true));
     }
 
     @Override
     public void execute(SlashCommandInteractionEvent event) {
-        Game game = getActiveGame();
-        Player activePlayer = game.getPlayer(getUser().getId());
-        activePlayer = Helper.getGamePlayer(game, activePlayer, event, null);
-        if (activePlayer == null) {
-            MessageHelper.sendMessageToEventChannel(event, "Player not found in game.");
-            return;
-        }
+        Game game = getGame();
+        Player activePlayer = getPlayer();
         String ids = event.getOption(Constants.EXPLORE_CARD_ID).getAsString().replaceAll(" ", "");
         String[] idList = ids.split(",");
         StringBuilder sb = new StringBuilder();
--- src/main/java/ti4/commands/explore/ExploreUse.java
+++ src/main/java/ti4/commands/explore/ExploreUse.java
@@ -2,16 +2,16 @@ package ti4.commands.explore;
 
 import java.util.Map;
 
-import org.apache.commons.lang3.StringUtils;
-
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import org.apache.commons.lang3.StringUtils;
+import ti4.commands.PlayerGameStateSubcommand;
 import ti4.generator.Mapper;
 import ti4.helpers.AliasHandler;
 import ti4.helpers.Constants;
-import ti4.helpers.Helper;
+import ti4.helpers.ExploreHelper;
 import ti4.map.Game;
 import ti4.map.Planet;
 import ti4.map.Player;
@@ -19,11 +19,11 @@ import ti4.map.Tile;
 import ti4.map.UnitHolder;
 import ti4.message.MessageHelper;
 
-public class ExploreUse extends ExploreSubcommandData {
+public class ExploreUse extends PlayerGameStateSubcommand {
 
     public ExploreUse() {
-        super(Constants.USE, "Draw and activate an explore card from the deck or discard");
-        addOptions(idOption.setRequired(true).setAutoComplete(true));
+        super(Constants.USE, "Draw and activate an explore card from the deck or discard", true, true);
+        addOptions(new OptionData(OptionType.STRING, Constants.EXPLORE_CARD_ID, "Explore card ID").setRequired(true).setAutoComplete(true));
         addOptions(new OptionData(OptionType.STRING, Constants.PLANET, "Planet to explore").setRequired(false).setAutoComplete(true));
         addOptions(new OptionData(OptionType.STRING, Constants.FACTION_COLOR, "Faction or Color").setAutoComplete(true));
         addOptions(new OptionData(OptionType.BOOLEAN, Constants.FORCE, "True to force the draw, even if none are in the deck"));
@@ -31,7 +31,7 @@ public class ExploreUse extends ExploreSubcommandData {
 
     @Override
     public void execute(SlashCommandInteractionEvent event) {
-        Game game = getActiveGame();
+        Game game = getGame();
         String id = event.getOption(Constants.EXPLORE_CARD_ID, "", OptionMapping::getAsString);
         boolean force = event.getOption(Constants.FORCE, false, OptionMapping::getAsBoolean);
         id = StringUtils.substringBefore(id, " ");
@@ -40,39 +40,39 @@ public class ExploreUse extends ExploreSubcommandData {
             MessageHelper.sendMessageToEventChannel(event, "Invalid ID specified: " + id);
         }
 
-        if (force || game.pickExplore(id) != null) {
-            OptionMapping planetOption = event.getOption(Constants.PLANET);
-            String planetName = null;
-            if (planetOption != null) {
-                planetName = planetOption.getAsString();
-                planetName = AliasHandler.resolvePlanet(planetName);
-            }
-            Tile tile = null;
-            if (game.getPlanets().contains(planetName)) {
-                for (Tile tile_ : game.getTileMap().values()) {
-                    if (tile != null) {
+        if (!force && game.pickExplore(id) == null) {
+            MessageHelper.sendMessageToEventChannel(event, "Explore Card ID: `" + id + "` is not in the deck or discard pile.");
+            return;
+        }
+
+        OptionMapping planetOption = event.getOption(Constants.PLANET);
+        String planetName = null;
+        if (planetOption != null) {
+            planetName = planetOption.getAsString();
+            planetName = AliasHandler.resolvePlanet(planetName);
+        }
+        Tile tile = null;
+        if (game.getPlanets().contains(planetName)) {
+            for (Tile tile_ : game.getTileMap().values()) {
+                if (tile != null) {
+                    break;
+                }
+                for (Map.Entry<String, UnitHolder> unitHolderEntry : tile_.getUnitHolders().entrySet()) {
+                    if (unitHolderEntry.getValue() instanceof Planet && unitHolderEntry.getKey().equals(planetName)) {
+                        tile = tile_;
                         break;
                     }
-                    for (Map.Entry<String, UnitHolder> unitHolderEntry : tile_.getUnitHolders().entrySet()) {
-                        if (unitHolderEntry.getValue() instanceof Planet && unitHolderEntry.getKey().equals(planetName)) {
-                            tile = tile_;
-                            break;
-                        }
-                    }
-                }
-                if (tile == null) {
-                    MessageHelper.sendMessageToEventChannel(event, "System not found that contains planet: " + planetName);
-                    return;
                 }
             }
-            Player player = game.getPlayer(event.getUser().getId());
-            player = Helper.getGamePlayer(game, player, event, null);
-            String messageText = player.getRepresentation() + " used explore card: " + id;
-            if (force) messageText += "\nTHIS CARD WAS DRAWN FORCEFULLY (if the card wasn't in the deck, it was created from thin air)";
-            resolveExplore(event, id, tile, planetName, messageText, player, game);
-        } else {
-            MessageHelper.sendMessageToEventChannel(event, "Explore Card ID: `" + id + "` is not in the deck or discard pile.");
+            if (tile == null) {
+                MessageHelper.sendMessageToEventChannel(event, "System not found that contains planet: " + planetName);
+                return;
+            }
         }
+        Player player = getPlayer();
+        String messageText = player.getRepresentation() + " used explore card: " + id;
+        if (force) messageText += "\nTHIS CARD WAS DRAWN FORCEFULLY (if the card wasn't in the deck, it was created from thin air)";
+        ExploreHelper.resolveExplore(event, id, tile, planetName, messageText, player, game);
     }
 
 }
