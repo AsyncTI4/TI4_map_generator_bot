3d62bfb28f37296b42324db910e973a9a6db112b
Will <willholton1@gmail.com>
2024-11-11 20:30:53 -0500

    stored values cache

--- src/main/java/ti4/commands/user/UserSettings.java
+++ src/main/java/ti4/commands/user/UserSettings.java
@@ -6,20 +6,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-
 import lombok.Data;
-import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.MessageEmbed;
-import net.dv8tion.jda.api.interactions.components.buttons.Button;
-import ti4.AsyncTI4DiscordBot;
-import ti4.buttons.Buttons;
 
 @Data
 public class UserSettings {
+
     private String userId;
     private List<String> preferredColourList = new ArrayList<>();
     private Integer personalPingInterval = 0;
@@ -27,41 +18,6 @@ public class UserSettings {
 
     public UserSettings(String userId) {
         this.userId = userId;
-    }
-
-    public UserSettings() {
-    }
-
-    @JsonIgnore
-    public String getJSONRaw() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.enable(SerializationFeature.INDENT_OUTPUT);
-        try {
-            return mapper.writeValueAsString(this);
-        } catch (JsonProcessingException e) {
-            return "Failed to serialize UserSettings";
-        }
-    }
-
-    @JsonIgnore
-    public void save() {
-        UserSettingsManager.getInstance().saveUserSetting(this);
-    }
-
-    @JsonIgnore
-    public MessageEmbed getSettingEmbed() {
-        EmbedBuilder eb = new EmbedBuilder();
-        String userName = AsyncTI4DiscordBot.jda.getUserById(getUserId()).getName();
-        eb.setTitle(userName + "'s User Settings");
-        eb.addField("", "```json\n" + getJSONRaw() + "```", false);
-        return eb.build();
-    }
-
-    @JsonIgnore
-    public List<Button> getUserSettingsButtons() {
-        return List.of(
-            SetPersonalPingInterval.OFFER_PING_OPTIONS_BUTTON,
-            Buttons.DONE_DELETE_BUTTONS);
     }
 
     public void putStoredValue(String settingKey, String settingValue) {
