3ae81c36e9f573d3c77be01251b1fb042a740ec8
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2024-11-13 12:02:59 -0500

    add overlay for res/inf summary and add alternative overlay constructors to test things out

--- src/main/java/ti4/website/WebsiteOverlay.java
+++ src/main/java/ti4/website/WebsiteOverlay.java
@@ -4,16 +4,29 @@ import java.util.List;
 
 import lombok.Getter;
 import lombok.Setter;
+import ti4.model.ModelInterface;
 
 @Getter @Setter
 public class WebsiteOverlay {
     private String cardType;
     private String cardID;
     private List<Integer> boxXYWH;
+    private String text;
 
     public WebsiteOverlay(String cardType, String cardID, List<Integer> boxXYWH) {
         this.cardType = cardType;
         this.cardID = cardID;
+        this.boxXYWH = boxXYWH;
+    }
+
+    public WebsiteOverlay(String text, List<Integer> boxXYWH) {
+        this.text = text;
+        this.boxXYWH = boxXYWH;
+    }
+
+    public WebsiteOverlay(ModelInterface model, List<Integer> boxXYWH) {
+        this.cardType = model.getClass().getSimpleName();
+        this.cardID = model.getAlias();
         this.boxXYWH = boxXYWH;
     }
 }
