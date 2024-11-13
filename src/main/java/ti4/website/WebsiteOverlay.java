fb8f5f9afcc9198fef614d7c669a99ab60a8bc82
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2024-11-13 14:53:32 -0500

    no nulls please

--- src/main/java/ti4/website/WebsiteOverlay.java
+++ src/main/java/ti4/website/WebsiteOverlay.java
@@ -2,11 +2,16 @@ package ti4.website;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import lombok.Getter;
 import lombok.Setter;
 import ti4.model.ModelInterface;
 
-@Getter @Setter
+@Getter
+@Setter
+@JsonInclude(Include.NON_NULL)
 public class WebsiteOverlay {
     private String cardType;
     private String cardID;
