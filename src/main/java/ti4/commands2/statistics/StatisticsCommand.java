490c8e3555e42579ec8539fde83c1b0acc59fabf
Will <willholton1@gmail.com>
2024-11-17 15:17:28 -0500

    Update StatisticsCommand.java

--- src/main/java/ti4/commands2/statistics/StatisticsCommand.java
+++ src/main/java/ti4/commands2/statistics/StatisticsCommand.java
@@ -4,6 +4,8 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import ti4.commands2.CommandHelper;
 import ti4.commands2.ParentCommand;
 import ti4.commands2.Subcommand;
 import ti4.helpers.Constants;
@@ -34,5 +36,16 @@ public class StatisticsCommand implements ParentCommand {
 
     public String getDescription() {
         return "Statistics";
+    }
+
+    @Override
+    public boolean accept(SlashCommandInteractionEvent event) {
+        return ParentCommand.super.accept(event) &&
+            CommandHelper.acceptIfPlayerInGameAndGameChannel(event);
+    }
+
+    @Override
+    public Map<String, Subcommand> getSubcommands() {
+        return subcommands;
     }
 }
