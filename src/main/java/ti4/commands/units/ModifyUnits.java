e4fd8ab529ea4a92c9a6ccd93e5e425c7d013532
Will <willholton1@gmail.com>
2024-11-20 18:10:17 -0500

    f

--- src/main/java/ti4/commands/units/ModifyUnits.java
+++ src/main/java/ti4/commands/units/ModifyUnits.java
@@ -10,7 +10,6 @@ import ti4.commands.Command;
 import ti4.commands2.CommandHelper;
 import ti4.helpers.ButtonHelper;
 import ti4.helpers.Constants;
-import ti4.listeners.annotations.ButtonHandler;
 import ti4.map.Game;
 import ti4.map.GameManager;
 import ti4.map.Player;
@@ -39,16 +38,8 @@ public class ModifyUnits implements Command {
         getModifyTiles(player, game);
     }
 
-    @ButtonHandler("getModifyTiles")
     public static void getModifyTiles(Player player, Game game) {
         List<Button> buttons = ButtonHelper.getTilesToModify(player, game);
-        String message = player.getRepresentation() + " Use the buttons to select the tile in which you wish to modify units. ";
-        MessageHelper.sendMessageToChannelWithButtons(player.getCorrectChannel(), message, buttons);
-    }
-
-    @ButtonHandler("modifyUnitsAllTiles")
-    public static void modifyUnitsAllTiles(Player player, Game game) {
-        List<Button> buttons = ButtonHelper.getAllTilesToModify(player, game, "genericModifyAllTiles", true);
         String message = player.getRepresentation() + " Use the buttons to select the tile in which you wish to modify units. ";
         MessageHelper.sendMessageToChannelWithButtons(player.getCorrectChannel(), message, buttons);
     }
