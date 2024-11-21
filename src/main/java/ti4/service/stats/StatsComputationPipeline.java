e9fd372f6f93dbdac80133c5dfc16b230fe3c1fb
Will <willholton1@gmail.com>
2024-11-21 15:51:41 -0500

    Update StatsComputationPipeline.java

--- src/main/java/ti4/service/stats/StatsComputationPipeline.java
+++ src/main/java/ti4/service/stats/StatsComputationPipeline.java
@@ -13,7 +13,6 @@ public class StatsComputationPipeline {
     // SearchGames
     // GameStatisticsFilter*****
     // GameStats
-    // ListTitlesGiven
     // StellarConverter
     // ListTitlesGiven
 
