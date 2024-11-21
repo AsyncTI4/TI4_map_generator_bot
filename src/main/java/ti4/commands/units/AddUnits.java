c4e68b1f23225f3d77500e6b679053abdda16c77
Will <willholton1@gmail.com>
2024-11-21 00:16:12 -0500

    f

--- src/main/java/ti4/commands/units/AddUnits.java
+++ src/main/java/ti4/commands/units/AddUnits.java
@@ -1,77 +1,24 @@
 package ti4.commands.units;
 
-import net.dv8tion.jda.api.events.interaction.GenericInteractionCreateEvent;
+import java.util.List;
+
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
-import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
-import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import ti4.commands2.CommandHelper;
-import ti4.helpers.CommandCounterHelper;
+import ti4.commands2.GameStateCommand;
 import ti4.helpers.Constants;
-import ti4.helpers.Helper;
-import ti4.helpers.Units.UnitKey;
+import ti4.image.Mapper;
 import ti4.map.Game;
-import ti4.map.GameManager;
-import ti4.map.Player;
 import ti4.map.Tile;
-import ti4.map.UnitHolder;
 import ti4.message.MessageHelper;
+import ti4.service.unit.AddUnitService;
 
-public class AddUnits extends AddRemoveUnits {
-    @Override
-    protected void unitAction(SlashCommandInteractionEvent event, Tile tile, int count, String planetName, UnitKey unitID, String color, Game game) {
-        tile.addUnit(planetName, unitID, count);
-        actionAfterAll(event, tile, color, game);
-    }
+public class AddUnits extends GameStateCommand {
 
-    @Override
-    protected void unitAction(GenericInteractionCreateEvent event, Tile tile, int count, String planetName, UnitKey unitID, String color, Game game) {
-        tile.addUnit(planetName, unitID, count);
-    }
-
-    public void actionAfterAll(SlashCommandInteractionEvent event, Tile tile, String color, Game game) {
-        OptionMapping option = event.getOption(Constants.CC_USE);
-        if (option != null) {
-            String value = option.getAsString().toLowerCase();
-            if (!event.getInteraction().getName().equals(Constants.MOVE_UNITS)) {
-                switch (value) {
-                    case "t/tactics", "t", "tactics", "tac", "tact" -> {
-                        MoveUnits.removeTacticsCC(event, color, tile, GameManager.getUserActiveGame(event.getUser().getId()));
-                        CommandCounterHelper.addCC(event, color, tile);
-                        Helper.isCCCountCorrect(event, game, color);
-                    }
-                    case "r/retreat/reinforcements", "r", "retreat", "reinforcements" -> {
-                        CommandCounterHelper.addCC(event, color, tile);
-                        Helper.isCCCountCorrect(event, game, color);
-                    }
-                }
-            }
-        }
-        OptionMapping optionSlingRelay = event.getOption(Constants.SLING_RELAY);
-        if (optionSlingRelay != null) {
-            boolean useSlingRelay = optionSlingRelay.getAsBoolean();
-            if (useSlingRelay) {
-                Player player = CommandHelper.getPlayerFromEvent(game, event);
-                if (player != null) {
-                    player.exhaustTech("sr");
-                }
-            }
-        }
-    }
-
-    @Override
-    protected void unitParsingForTile(SlashCommandInteractionEvent event, String color, Tile tile, Game game) {
-        tile = MoveUnits.flipMallice(event, tile, game);
-        if (tile == null) {
-            MessageHelper.sendMessageToChannel(event.getChannel(), "Could not flip Mallice");
-            return;
-        }
-        super.unitParsingForTile(event, color, tile, game);
-        for (UnitHolder unitHolder_ : tile.getUnitHolders().values()) {
-            addPlanetToPlayArea(event, tile, unitHolder_.getName(), game);
-        }
+    public AddUnits() {
+        super(true, true);
     }
 
     @Override
@@ -80,20 +27,69 @@ public class AddUnits extends AddRemoveUnits {
     }
 
     @Override
-    protected String getActionDescription() {
+    public String getDescription() {
         return "Add units to map";
     }
 
-    @SuppressWarnings("ResultOfMethodCallIgnored")
     @Override
-    public void register(CommandListUpdateAction commands) {
-        commands.addCommands(
-            Commands.slash(getName(), getActionDescription())
-                .addOptions(new OptionData(OptionType.STRING, Constants.TILE_NAME, "System/Tile name").setRequired(true).setAutoComplete(true))
-                .addOptions(new OptionData(OptionType.STRING, Constants.UNIT_NAMES, "Comma separated list of '{count} unit {planet}' Eg. 2 infantry primor, carrier, 2 fighter, mech pri").setRequired(true))
-                .addOptions(new OptionData(OptionType.STRING, Constants.CC_USE, "Type tactics or t, retreat, reinforcements or r - default is 'no'").setAutoComplete(true))
-                .addOptions(new OptionData(OptionType.STRING, Constants.FACTION_COLOR, "Faction or Color for unit").setAutoComplete(true))
-                .addOptions(new OptionData(OptionType.BOOLEAN, Constants.SLING_RELAY, "Declare use of and exhaust Sling Relay Tech"))
-                .addOptions(new OptionData(OptionType.BOOLEAN, Constants.NO_MAPGEN, "'True' to not generate a map update with this command")));
+    public boolean accept(SlashCommandInteractionEvent event) {
+        return super.accept(event) &&
+            CommandHelper.acceptIfPlayerInGameAndGameChannel(event);
+    }
+
+    @Override
+    public List<OptionData> getOptions() {
+        return List.of(
+            new OptionData(OptionType.STRING, Constants.CONFIRM, "Type YES to confirm")
+                .setRequired(true),
+            new OptionData(OptionType.STRING, Constants.TILE_NAME, "System/Tile name")
+                .setRequired(true)
+                .setAutoComplete(true),
+            new OptionData(OptionType.STRING, Constants.UNIT_NAMES, "Comma separated list of '{count} unit {planet}' Eg. 2 infantry primor, carrier, 2 fighter, mech pri")
+                .setRequired(true),
+            new OptionData(OptionType.STRING, Constants.CC_USE, "Type tactics or t, retreat, reinforcements or r - default is 'no'")
+                .setAutoComplete(true),
+            new OptionData(OptionType.STRING, Constants.FACTION_COLOR, "Faction or Color for unit")
+                .setAutoComplete(true),
+            new OptionData(OptionType.BOOLEAN, Constants.SLING_RELAY, "Declare use of and exhaust Sling Relay Tech"),
+            new OptionData(OptionType.BOOLEAN, Constants.NO_MAPGEN, "'True' to not generate a map update with this command")
+        );
+    }
+
+    @Override
+    public void execute(SlashCommandInteractionEvent event) {
+        Game game = getGame();
+
+        String color = CommandHelper.getColor(game, event);
+        if (!Mapper.isValidColor(color)) {
+            MessageHelper.replyToMessage(event, "Color/Faction not valid");
+            return;
+        }
+
+        if (game.getPlayerFromColorOrFaction(color) == null && !game.getPlayerIDs().contains(Constants.dicecordId)) {
+            game.setupNeutralPlayer(color);
+        }
+
+        Tile tile = CommandHelper.getTile(event, game);
+        if (tile == null) return;
+
+        String unitList = event.getOption(Constants.UNIT_NAMES).getAsString();
+        AddUnitService.addUnits(event, tile, game, color, unitList);
+
+        handleSlingRelayOption(event);
+        UnitCommandHelper.handleCcUseOption(event, tile, color, game);
+        UnitCommandHelper.handleGenerateMapOption(event, game);
+    }
+
+    private void handleSlingRelayOption(SlashCommandInteractionEvent event) {
+        OptionMapping optionSlingRelay = event.getOption(Constants.SLING_RELAY);
+        if (optionSlingRelay == null) {
+            return;
+        }
+        boolean useSlingRelay = optionSlingRelay.getAsBoolean();
+        if (!useSlingRelay) {
+            return;
+        }
+        getPlayer().exhaustTech("sr");
     }
 }
