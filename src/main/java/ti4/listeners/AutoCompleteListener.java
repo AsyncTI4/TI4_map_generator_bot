b42d0a83f5d72c75f9cb090b43ebc20a2b9a5c56
Will Holton <willholton1@gmail.com>
2024-11-28 18:09:34 -0500

    autocomplete executor (#2424)

--- src/main/java/ti4/listeners/AutoCompleteListener.java
+++ src/main/java/ti4/listeners/AutoCompleteListener.java
@@ -6,7 +6,6 @@ import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInterac
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import ti4.AsyncTI4DiscordBot;
 import ti4.autocomplete.AutoCompleteProvider;
-import ti4.message.BotLogger;
 
 public class AutoCompleteListener extends ListenerAdapter {
 
@@ -16,11 +15,6 @@ public class AutoCompleteListener extends ListenerAdapter {
             event.replyChoice("Please try again in a moment. The bot is not ready to serve AutoComplete.", 0).queue();
             return;
         }
-        try {
-            AutoCompleteProvider.startNewAutoCompleteThread(event);
-        } catch (Exception e) {
-            String message = "Auto complete issue in event: " + event.getName() + "\n> Channel: " + event.getChannel().getAsMention() + "\n> Command: " + event.getCommandString();
-            BotLogger.log(message, e);
-        }
+        AsyncTI4DiscordBot.runAsync(() -> AutoCompleteProvider.handleAutoCompleteEvent(event));
     }
 }
