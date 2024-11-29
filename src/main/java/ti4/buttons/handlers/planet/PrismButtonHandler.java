4a2d5e21acc835410c39af1a9264f0ee276b66ef
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2024-11-29 12:56:34 -0500

    Prism purge

--- src/main/java/ti4/buttons/handlers/planet/PrismButtonHandler.java
+++ src/main/java/ti4/buttons/handlers/planet/PrismButtonHandler.java
@@ -23,7 +23,7 @@ class PrismButtonHandler {
     @ButtonHandler("newPrism@")
     public static void newPrismPart2(Game game, Player player, String buttonID, ButtonInteractionEvent event) {
         String techOut = buttonID.split("@")[1];
-        player.removeTech(techOut);
+        player.purgeTech(techOut);
         TechnologyModel techM1 = Mapper.getTech(techOut);
         MessageHelper.sendMessageToChannel(player.getCorrectChannel(), player.getFactionEmoji() + " removed the tech " + techM1.getName());
         MessageHelper.sendMessageToChannelWithButton(event.getMessageChannel(), player.getRepresentation() + " Use the button to get a tech with the same number of prerequisites", Buttons.GET_A_FREE_TECH);
