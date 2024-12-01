ffa5920f9b45bd28cfa7aaf8c444adc5b40634c0
Will <willholton1@gmail.com>
2024-11-30 20:37:07 -0500

    f

--- src/main/java/ti4/commands2/CommandGameState.java
+++ src/main/java/ti4/commands2/CommandGameState.java
@@ -1,11 +1,14 @@
 package ti4.commands2;
 
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import org.jetbrains.annotations.NotNull;
 import ti4.map.Game;
 import ti4.map.GameManager;
 import ti4.map.GameSaveLoadManager;
 import ti4.map.Player;
+import ti4.message.BotLogger;
+import ti4.service.SusSlashCommandService;
 
 class CommandGameState {
 
@@ -25,12 +28,23 @@ class CommandGameState {
             throw new IllegalArgumentException("Invalid game name: " + gameName + " while attempting to run event " + event.getName() +
                     " in channel " + event.getChannel().getName());
         }
-        game.set(GameManager.getGame(gameName));
+        Game eventGame = GameManager.getGame(gameName);
+        eventGame.incrementSpecificSlashCommandCount(event.getFullCommandName());
+        game.set(eventGame);
+
+        Member member = event.getMember();
+        if (member != null) {
+            String commandText = "```fix\n" + member.getEffectiveName() + " used " + event.getCommandString() + "\n```";
+            event.getChannel().sendMessage(commandText).queue(message -> {
+                BotLogger.logSlashCommand(event, message);
+                SusSlashCommandService.checkIfShouldReportSusSlashCommand(event, event.getUser().getId(), message, GameManager.getManagedGame(gameName));
+            }, BotLogger::catchRestError);
+        }
 
         if (!isPlayerCommand) {
             return;
         }
-        var gamePlayer = CommandHelper.getPlayerFromEvent(getGame(), event);
+        var gamePlayer = CommandHelper.getPlayerFromEvent(eventGame, event);
         if (gamePlayer == null) {
             throw new IllegalArgumentException("Unable to determine player while attempting to run event " + event.getName() +
                     " in channel " + event.getChannel().getName() + " for game " + gameName);
