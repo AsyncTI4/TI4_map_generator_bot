80a9bb59b326a217660ae66b676f200553fe02de
Will <willholton1@gmail.com>
2024-12-07 02:54:55 -0500

    f

--- src/main/java/ti4/service/unit/RemoveUnitService.java
+++ src/main/java/ti4/service/unit/RemoveUnitService.java
@@ -6,7 +6,6 @@ import java.util.List;
 import lombok.experimental.UtilityClass;
 import net.dv8tion.jda.api.events.interaction.GenericInteractionCreateEvent;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
-import ti4.helpers.AliasHandler;
 import ti4.helpers.Constants;
 import ti4.helpers.Units;
 import ti4.map.Game;
@@ -76,8 +75,7 @@ public class RemoveUnitService {
 
     private static List<UnitHolder> getUnitHoldersToRemoveFrom(Tile tile, ParsedUnit parsedUnit) {
         if (!parsedUnit.getLocation().equals(Constants.SPACE)) { // We are removing from a specific planet.
-            String planetName = AliasHandler.resolvePlanet(parsedUnit.getLocation().toLowerCase());
-            var planet = tile.getUnitHolders().get(planetName);
+            var planet = tile.getUnitHolders().get(parsedUnit.getLocation());
             return planet == null ? Collections.emptyList() : List.of(planet);
         }
         // Otherwise, the location was not specified, so we check everywhere
