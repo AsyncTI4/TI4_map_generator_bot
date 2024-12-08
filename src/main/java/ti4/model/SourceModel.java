26eb71ef1c6fa64036a7d40703c6240c68b35cf2
schluik <loic.sermadiras.18@eigsi.fr>
2024-12-08 18:48:39 +0100

    ended search work -- pending sources.json completion

--- src/main/java/ti4/model/SourceModel.java
+++ src/main/java/ti4/model/SourceModel.java
@@ -1,7 +1,9 @@
 package ti4.model;
 
 import java.awt.*;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import lombok.Data;
 import net.dv8tion.jda.api.EmbedBuilder;
@@ -15,7 +17,8 @@ public class SourceModel implements ModelInterface, EmbeddableModel {
     private String name; // Long fancy name
     private String canal; // Must be "official" or "community"
     private String subcanal; // Sub value of canal when canal = "community"
-    private String credits; // Creator/Responsible
+    private String credits; // Creator/Responsible.s
+    private String description; // Content list (aggregated by component types)
     private List<String> data; // Links to rules, content, discussion channels, etc.
 
     public boolean isValid() {
@@ -29,21 +32,24 @@ public class SourceModel implements ModelInterface, EmbeddableModel {
 
     @Override
     public MessageEmbed getRepresentationEmbed() {
-
+        return getRepresentationEmbed(null);
+    }
+    public MessageEmbed getRepresentationEmbed(HashMap<String, Integer> occurrences) {
         EmbedBuilder eb = new EmbedBuilder();
 
         StringBuilder title = new StringBuilder();
         title.append(getSource().emoji()).append(" ").append(getName());
         eb.setTitle(title.toString());
 
-        StringBuilder description = new StringBuilder();
-        description.append("Content:\n").append(getDataFormatted());
-        description.append("Implementation:\n").append("WIP");
-        eb.setDescription(description);
+        StringBuilder content = new StringBuilder();
+        if (getDescription() != null) content.append("*").append(getDescription()).append("*\n\n");
+        if (getData() != null) content.append("Links:\n").append(getDataFormatted()).append("\n");
+        if (occurrences != null) content.append("Implementation: ").append(compTypeOccurrences(occurrences));
+        eb.setDescription(content);
 
         StringBuilder footer = new StringBuilder();
         footer.append("Source: ").append(getSource()).append("    Type: ").append(getCanal());
-        if(subcanal != null) footer.append(" > ").append(getSubcanal());
+        if(getSubcanal() != null) footer.append(" > ").append(getSubcanal());
         footer.append("\nCredits: ").append(getCredits());
         eb.setFooter(footer.toString());
 
@@ -66,8 +72,7 @@ public class SourceModel implements ModelInterface, EmbeddableModel {
 
     @Override
     public String getAutoCompleteName() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAutoCompleteName'");
+        return getName();
     }
 
     public String getDataFormatted() {
@@ -81,6 +86,17 @@ public class SourceModel implements ModelInterface, EmbeddableModel {
     public boolean isCanalOfficial() {
         boolean official = (getCanal().equals("official")) ? true : false;
         return official;
+    }
+
+    private String compTypeOccurrences(HashMap<String, Integer> occurrences){
+        StringBuilder implementation = new StringBuilder("");
+        for (Map.Entry<String, Integer> entry : occurrences.entrySet()) {
+            if (entry.getValue() != 0) {
+                if (!implementation.toString().equals("")) implementation.append(", ");
+                implementation.append(entry.getKey()).append(" (").append(entry.getValue()).append(")");
+            }
+        }
+        return implementation.toString();
     }
 
 }
