0773f4c6b98486e529703eb9d8a06dfa7b02a93a
Will <willholton1@gmail.com>
2024-12-08 23:26:54 -0500

    log stats cron

---
+++ src/main/java/ti4/cron/LogButtonRuntimeStatisticsCron.java
@@ -0,0 +1,24 @@
+package ti4.cron;
+
+import java.util.concurrent.TimeUnit;
+
+import lombok.experimental.UtilityClass;
+import ti4.message.BotLogger;
+import ti4.processors.ButtonProcessor;
+
+@UtilityClass
+public class LogButtonRuntimeStatisticsCron {
+
+    public static void register() {
+        CronManager.register(LogButtonRuntimeStatisticsCron.class, LogButtonRuntimeStatisticsCron::logButtonRuntimeStatistics, 1, 4, TimeUnit.HOURS);
+    }
+
+    private static void logButtonRuntimeStatistics() {
+        try {
+            String buttonProcessingStatistics = ButtonProcessor.getButtonProcessingStatistics();
+            BotLogger.log(buttonProcessingStatistics);
+        } catch (Exception e) {
+            BotLogger.log("**LogButtonRuntimeStatisticsCron failed.**", e);
+        }
+    }
+}
