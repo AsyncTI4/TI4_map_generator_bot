81eea03f75e238f22a6881c9703dc86f6b70a89d
Will <willholton1@gmail.com>
2024-12-09 01:52:50 -0500

    f

--- src/main/java/ti4/cron/LogCacheStatsCron.java
+++ src/main/java/ti4/cron/LogCacheStatsCron.java
@@ -1,22 +1,14 @@
 package ti4.cron;
 
-import java.text.DecimalFormat;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
-import com.github.benmanes.caffeine.cache.Cache;
-import com.github.benmanes.caffeine.cache.stats.CacheStats;
 import lombok.experimental.UtilityClass;
-import ti4.AsyncTI4DiscordBot;
 import ti4.cache.CacheManager;
-import ti4.helpers.ToStringHelper;
+import ti4.cache.CacheStatsToStringConverter;
 import ti4.message.BotLogger;
-import ti4.message.MessageHelper;
 
 @UtilityClass
 public class LogCacheStatsCron {
-
-    private static final ThreadLocal<DecimalFormat> percentFormatter = ThreadLocal.withInitial(() -> new DecimalFormat("##.##%"));
 
     public static void register() {
         CronManager.schedulePeriodically(LogCacheStatsCron.class, LogCacheStatsCron::logCacheStats, 1, 4, TimeUnit.HOURS);
@@ -24,37 +16,10 @@ public class LogCacheStatsCron {
 
     private static void logCacheStats() {
         try {
-            var cacheStats = CacheManager.getNamesToCaches().stream()
-                .map(entry -> cacheStatsToString(entry.getKey(), entry.getValue()))
-                .collect(Collectors.joining("\n\n"));
-            MessageHelper.sendMessageToPrimaryBotLogChannel("```\n" + cacheStats + "\n```");
+            String cacheStats = CacheStatsToStringConverter.convert(CacheManager.getNamesToCaches());
+            BotLogger.log("```\n" + cacheStats + "\n```");
         } catch (Exception e) {
             BotLogger.log("**LogCacheStatsCron failed.**", e);
         }
-    }
-
-    private static String cacheStatsToString(String name, Cache<?, ?> cache) {
-        CacheStats stats = cache.stats();
-        return ToStringHelper.of(name)
-                .add("liveTime", getLiveTime())
-                .add("hitCount", stats.hitCount())
-                .add("hitRate", formatPercent(stats.hitRate()))
-                .add("loadCount", stats.loadCount())
-                .add("loadFailureCount", stats.loadFailureCount())
-                .add("averageLoadPenaltyMilliseconds", TimeUnit.MILLISECONDS.convert((long) stats.averageLoadPenalty(), TimeUnit.NANOSECONDS))
-                .add("evictionCount", stats.evictionCount())
-                .add("currentSize", cache.estimatedSize())
-                .toString();
-    }
-
-    private static String getLiveTime() {
-        long millisecondsSinceBotStarted = System.currentTimeMillis() - AsyncTI4DiscordBot.START_TIME_MILLISECONDS;
-        long liveTimeHours = TimeUnit.HOURS.convert(millisecondsSinceBotStarted, TimeUnit.MILLISECONDS);
-        long liveTimeMinutes = TimeUnit.MINUTES.convert(millisecondsSinceBotStarted, TimeUnit.MILLISECONDS) - liveTimeHours * 60;
-        return liveTimeHours + "h" + liveTimeMinutes + "m";
-    }
-
-    private static String formatPercent(double d) {
-        return percentFormatter.get().format(d);
     }
 }
