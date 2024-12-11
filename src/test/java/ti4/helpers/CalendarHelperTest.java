dd5bb69045b31828f17b85cf149d748d4c43bba5
Will <willholton1@gmail.com>
2024-12-11 14:33:10 -0500

    Update CalendarHelperTest.java

--- src/test/java/ti4/helpers/CalendarHelperTest.java
+++ src/test/java/ti4/helpers/CalendarHelperTest.java
@@ -1,17 +1,20 @@
 package ti4.helpers;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.util.Calendar;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class CalendarHelperTest {
+
     @Test
     void testIsBetween() {
-        assertTrue(CalendarHelper.isBetween(Calendar.MONTH - 1, Calendar.DAY_OF_MONTH, Calendar.MONTH + 1, Calendar.DAY_OF_MONTH));
-        assertTrue(CalendarHelper.isBetween(Calendar.MONTH, Calendar.DAY_OF_MONTH, Calendar.MONTH, Calendar.DAY_OF_MONTH));
-        assertFalse(CalendarHelper.isBetween(Calendar.MONTH - 2, Calendar.DAY_OF_MONTH, Calendar.MONTH - 1, Calendar.DAY_OF_MONTH));
+        int currentMonth = Calendar.getInstance().get(Calendar.MONTH);
+        int currentDay = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+        assertTrue(CalendarHelper.isBetween(currentMonth - 1, currentDay, currentMonth + 1, currentDay));
+        assertTrue(CalendarHelper.isBetween(currentMonth, currentDay, currentMonth, currentDay));
+        assertFalse(CalendarHelper.isBetween(currentMonth - 2, currentDay, currentMonth - 1, currentDay));
     }
 }
