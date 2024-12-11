4d093d8034a6ada7b8209ae7ae814fce2bcf6525
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2024-12-11 12:39:27 -0500

    add base tile emojis

---
+++ src/main/java/ti4/service/emoji/TileEmojis.java
@@ -0,0 +1,27 @@
+package ti4.service.emoji;
+
+public enum TileEmojis implements TI4Emoji {
+
+    // base
+    Jord_01, MollPrimus_02, Darien_03, Muaat_04, Nestphar_05, L1_000_06, Winnu_07, MordaiII_08, //
+    Maaluuk_09, ArcPime_10, LisisII_11, Nar_12, Trenlak_13, ArchonRen_14, Retillion_15, Arretze_16, //
+    DeltaWH_17, MR_18, Wellon_19, VefutII_20, Thibah_21, Tarmann_22, Saudor_23, MeharXull_24, //
+    Quann_25, Lodor_26, NewAlbion_27, Tequran_28, Qucenn_29, Mellon_30, Lazar_31, DalBootha_32, //
+    Corneeq_33, Centauri_34, Bereg_35, Arnor_36, Arinam_37, Abyz_38, AlphaWH_39, BetaWH_40, //
+    GravityRift_41, Nebula_42, Supernova_43, Asteroids_44, Asteroids_45, Void_46, Void_47, Void_48, //
+    Void_49, Void_50, Creuss_51
+    
+    // pok
+
+    // codex 3
+
+    // discordant stars
+
+    // uncharted space
+    ;
+
+    @Override
+    public String toString() {
+        return emojiString();
+    }
+}
