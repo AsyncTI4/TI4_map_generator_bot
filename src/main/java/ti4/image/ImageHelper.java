a9befce9321dd10a26f0aac54c23385977dde32c
Will Holton <willholton1@gmail.com>
2024-12-13 11:45:52 -0500

    Reapply webp again (#2563)
    * Reapply "webp retry (#2560)" (#2561)
    
    This reverts commit 63d55fba59a7ca4f48bd569a8fa7049e246b9d6c.
    
    * Update ImageHelper.java

--- src/main/java/ti4/image/ImageHelper.java
+++ src/main/java/ti4/image/ImageHelper.java
@@ -1,17 +1,18 @@
 package ti4.image;
 
-import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
-import javax.imageio.ImageWriteParam;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.util.Arrays;
 
+import com.sksamuel.scrimage.ImmutableImage;
+import com.sksamuel.scrimage.nio.JpegWriter;
+import com.sksamuel.scrimage.webp.WebpWriter;
+import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 import net.dv8tion.jda.api.entities.emoji.CustomEmoji;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
@@ -20,6 +21,9 @@ import ti4.message.BotLogger;
 
 @UtilityClass
 public class ImageHelper {
+
+    private static final WebpWriter WEBP_WRITER = WebpWriter.DEFAULT.withMultiThread();
+    private static final JpegWriter JPG_WRITER = JpegWriter.Default;
 
     @Nullable
     public static BufferedImage read(String filePath) {
@@ -137,39 +141,16 @@ public class ImageHelper {
         return null;
     }
 
-    public static String writeWebpOrDefaultTo(BufferedImage image, ByteArrayOutputStream out, String defaultFormat, float compressionForDefault) throws IOException {
-        // max webp dimensions are 16383 x 16383
-        // if (image.getHeight() > 16383 || image.getWidth() > 16383) {
-        // TODO: Disabling WEBP until we find a new library. Old one was segfaulting in native code.
-        writeCompressedFormat(image, out, defaultFormat, compressionForDefault);
-        return defaultFormat;
-        //}
-        //ImageIO.write(image, "webp", out);
-        //return "webp";
+    public static byte[] writeImage(BufferedImage image) {
+        return writeImage(image, "webp");
     }
 
-    public static String writeWebpOrDefaultTo(BufferedImage image, ByteArrayOutputStream out, String defaultFormat) throws IOException {
-        return writeWebpOrDefaultTo(image, out, defaultFormat, .1f);
-    }
-
-    public static void writeCompressedFormat(BufferedImage image, ByteArrayOutputStream out, String format, float compressionQuality) throws IOException {
-        var imageWriter = ImageIO.getImageWritersByFormatName(format).next();
-        try (var imageOutputStream = ImageIO.createImageOutputStream(out)) {
-            imageWriter.setOutput(imageOutputStream);
-            ImageWriteParam param = imageWriter.getDefaultWriteParam();
-            if (param.canWriteCompressed()) {
-                param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-                param.setCompressionQuality(compressionQuality);
-            }
-            imageWriter.write(null, new IIOImage(image, null, null), param);
-        } finally {
-            imageWriter.dispose();
-        }
-    }
-
-    public static BufferedImage redrawWithoutAlpha(BufferedImage image) {
-        var imageWithoutAlpha = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
-        imageWithoutAlpha.createGraphics().drawImage(image, 0, 0, Color.BLACK, null);
-        return imageWithoutAlpha;
+    @SneakyThrows
+    public static byte[] writeImage(BufferedImage image, String format) {
+        var immutableImage = ImmutableImage.fromAwt(image).removeTransparency(Color.BLACK);
+        return switch (format) {
+            case "jpg", "jpeg" -> immutableImage.bytes(JPG_WRITER);
+            default -> immutableImage.bytes(WEBP_WRITER);
+        };
     }
 }
