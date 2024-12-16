4c3f4e1a5b03f304c55c2005120dbf9e13bdf58b
Will Holton <willholton1@gmail.com>
2024-12-16 12:56:49 -0500

    game per executor (#2576)
    * game per executor
    
    * Update UserJoinServerListener.java
    
    * p
    
    * Update EndGameService.java
    
    * Update UserLeaveServerListener.java
    
    * f
    
    * Update AutoPingCron.java
    
    * Update ExecutorManager.java
    
    * Update AutoPingCron.java

--- src/main/java/ti4/listeners/AutoCompleteListener.java
+++ src/main/java/ti4/listeners/AutoCompleteListener.java
@@ -6,8 +6,11 @@ import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInterac
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import ti4.AsyncTI4DiscordBot;
 import ti4.autocomplete.AutoCompleteProvider;
+import ti4.executors.ExecutorManager;
 
 public class AutoCompleteListener extends ListenerAdapter {
+
+    private static final int EXECUTION_TIME_WARNING_THRESHOLD_SECONDS = 1;
 
     @Override
     public void onCommandAutoCompleteInteraction(@Nonnull CommandAutoCompleteInteractionEvent event) {
@@ -15,6 +18,6 @@ public class AutoCompleteListener extends ListenerAdapter {
             event.replyChoice("Please try again in a moment. The bot is not ready to serve AutoComplete.", 0).queue();
             return;
         }
-        AsyncTI4DiscordBot.runAsync("AutoComplete task", 1, () -> AutoCompleteProvider.handleAutoCompleteEvent(event));
+        ExecutorManager.runAsync("AutoComplete task", EXECUTION_TIME_WARNING_THRESHOLD_SECONDS, () -> AutoCompleteProvider.handleAutoCompleteEvent(event));
     }
 }
