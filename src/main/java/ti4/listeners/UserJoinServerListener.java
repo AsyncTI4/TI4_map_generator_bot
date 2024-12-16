941592d955be65bd8afcdac3cdbb3980474e3708
Will Holton <willholton1@gmail.com>
2024-12-16 10:02:25 -0500

    User leave logs managed game (#2578)

--- src/main/java/ti4/listeners/UserJoinServerListener.java
+++ src/main/java/ti4/listeners/UserJoinServerListener.java
@@ -5,28 +5,21 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import net.dv8tion.jda.api.audit.ActionType;
-import net.dv8tion.jda.api.audit.AuditLogEntry;
 import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.ISnowflake;
 import net.dv8tion.jda.api.entities.User;
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
 import net.dv8tion.jda.api.events.guild.GenericGuildEvent;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
-import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import ti4.AsyncTI4DiscordBot;
 import ti4.helpers.ButtonHelper;
+import ti4.helpers.GameLaunchThreadHelper;
 import ti4.helpers.Helper;
-import ti4.helpers.ThreadGetter;
 import ti4.map.Game;
 import ti4.map.manage.GameManager;
 import ti4.map.manage.ManagedGame;
-import ti4.map.manage.ManagedPlayer;
 import ti4.message.BotLogger;
 import ti4.message.MessageHelper;
-import ti4.service.game.ManagedGameService;
 
 public class UserJoinServerListener extends ListenerAdapter {
 
@@ -36,46 +29,20 @@ public class UserJoinServerListener extends ListenerAdapter {
         AsyncTI4DiscordBot.runAsync("Guild member join task", () -> handleGuildMemberJoin(event));
     }
 
-    private void handleGuildMemberJoin(GuildMemberJoinEvent event) {
-        try {
-            checkIfNewUserIsInExistingGamesAndAutoAddRole(event.getGuild(), event.getUser());
-        } catch (Exception e) {
-            BotLogger.log("Error in `UserJoinServerListener.onGuildMemberJoin`", e);
-        }
-    }
-
-    @Override
-    public void onGuildMemberRemove(@Nonnull GuildMemberRemoveEvent event) {
-        if (!validateEvent(event)) return;
-        AsyncTI4DiscordBot.runAsync("Guild member remove task", () -> handleGuildMemberRemove(event));
-    }
-
-    private void handleGuildMemberRemove(GuildMemberRemoveEvent event) {
-        try {
-            event.getGuild().retrieveAuditLogs().queueAfter(1, TimeUnit.SECONDS, (logs) -> {
-                boolean voluntary = true;
-                for (AuditLogEntry log : logs) {
-                    if (log.getTargetIdLong() == event.getUser().getIdLong()) {
-                        if (log.getType() == ActionType.BAN || log.getType() == ActionType.KICK) {
-                            voluntary = false;
-                            break;
-                        }
-                    }
-                }
-
-                checkIfUserLeftActiveGames(event.getGuild(), event.getUser(), voluntary);
-            }, BotLogger::catchRestError);
-        } catch (Exception e) {
-            BotLogger.log("Error in `UserJoinServerListener.onGuildMemberRemove`", e);
-        }
-    }
-
     private static boolean validateEvent(GenericGuildEvent event) {
         if (!AsyncTI4DiscordBot.isReadyToReceiveCommands()) {
             return false;
         }
         String eventGuild = event.getGuild().getId();
         return AsyncTI4DiscordBot.isValidGuild(eventGuild);
+    }
+
+    private void handleGuildMemberJoin(GuildMemberJoinEvent event) {
+        try {
+            checkIfNewUserIsInExistingGamesAndAutoAddRole(event.getGuild(), event.getUser());
+        } catch (Exception e) {
+            BotLogger.log("Error in `UserJoinServerListener.onGuildMemberJoin`", e);
+        }
     }
 
     private void checkIfNewUserIsInExistingGamesAndAutoAddRole(Guild guild, User user) {
@@ -93,7 +60,7 @@ public class UserJoinServerListener extends ListenerAdapter {
             String gameMessage = user.getAsMention() + " has joined the server!";
             MessageHelper.sendMessageToChannel(managedGame.getTableTalkChannel(), gameMessage);
             Game game = managedGame.getGame();
-            checkIfCanCloseGameLaunchThread(game, true);
+            GameLaunchThreadHelper.checkIfCanCloseGameLaunchThread(game, true);
         }
     }
 
@@ -120,78 +87,5 @@ public class UserJoinServerListener extends ListenerAdapter {
             }
         }
         return true;
-    }
-
-    public static void checkIfCanCloseGameLaunchThread(Game game, boolean notify) {
-        Guild guild = game.getGuild();
-        if (guild == null) {
-            return;
-        }
-        String threadID = game.getLaunchPostThreadID();
-        if (!ButtonHelper.isNumeric(threadID)) {
-            return;
-        }
-        ThreadChannel threadChannel = AsyncTI4DiscordBot.guildPrimary.getThreadChannelById(threadID);
-        if (threadChannel == null) {
-            return;
-        }
-        List<String> guildMemberIDs = guild.getMembers().stream().map(ISnowflake::getId).toList();
-        for (String playerIds : game.getPlayerIDs()) {
-            if (!guildMemberIDs.contains(playerIds)) {
-                return;
-            }
-        }
-        if (notify) {
-            MessageHelper.sendMessageToChannel(game.getTableTalkChannel(), game.getPing() + " all users have now joined the server! Let the games begin!");
-            MessageHelper.sendMessageToChannel(threadChannel, "All users have joined the game, this thread will now be closed.");
-        }
-        threadChannel.getManager().setArchived(true).queue();
-    }
-
-    private void checkIfUserLeftActiveGames(Guild guild, User user, boolean voluntary) {
-        List<ManagedGame> gamesQuit = new ArrayList<>();
-
-        ManagedPlayer player = GameManager.getManagedPlayer(user.getId());
-        for (ManagedGame game : player.getGames()) {
-            if (game.isHasEnded() || game.isVpGoalReached()) continue;
-            Guild gameGuild = game.getGuild();
-            if (gameGuild != null && gameGuild.equals(guild)) {
-                gamesQuit.add(game);
-            }
-        }
-
-        if (!gamesQuit.isEmpty()) {
-            StringBuilder msg = new StringBuilder("User " + user.getName() + " has left the server " + guild.getName() + " with the following in-progress games:");
-            for (ManagedGame game : gamesQuit) {
-                String gameMessage = "Attention " + ManagedGameService.getPingAllPlayers(game) + ": " + user.getName();
-                if (voluntary) gameMessage += " has left the server.\n> If this was not a mistake, you may make ";
-                if (!voluntary) gameMessage += " was removed from the server.\n> Make ";
-                gameMessage += "a post in https://discord.com/channels/943410040369479690/1176191865188536500 to get a replacement player";
-                MessageHelper.sendMessageToChannel(game.getTableTalkChannel(), gameMessage);
-                msg.append("\n> ").append(game.getName()).append(" -> Link:").append(game.getTableTalkChannel().getJumpUrl());
-            }
-            reportUserLeftServer(msg.toString());
-
-            String inviteBack = Helper.getGuildInviteURL(guild, 1);
-            String primaryInvite = Helper.getGuildInviteURL(AsyncTI4DiscordBot.guildPrimary, 1, true);
-            String usermsg = "It looks like you left a server while playing in `" + gamesQuit.size() + "` games.";
-            usermsg += " Please consider making a post in https://discord.com/channels/943410040369479690/1176191865188536500 to get a replacement player.\n\n";
-            usermsg += "If this was a mistake, here is an invite back to the server you just left: " + inviteBack + "\n";
-            usermsg += "If you are just taking a break, here is an invite to the HUB server that will last until you're ready to come back: " + primaryInvite + "\n\n";
-            usermsg += "Take care!\n> - Async TI4 Admin Team";
-            if (voluntary) {
-                MessageHelper.sendMessageToUser(usermsg, user);
-            }
-        }
-    }
-
-    private static void reportUserLeftServer(String message) {
-        TextChannel bothelperLoungeChannel = AsyncTI4DiscordBot.guildPrimary.getTextChannelsByName("staff-lounge", true).stream().findFirst().orElse(null);
-        if (bothelperLoungeChannel == null) return;
-        List<ThreadChannel> threadChannels = bothelperLoungeChannel.getThreadChannels();
-        if (threadChannels.isEmpty()) return;
-        String threadName = "in-progress-games-left";
-        ThreadGetter.getThreadInChannel(bothelperLoungeChannel, threadName,
-            threadChannel -> MessageHelper.sendMessageToChannel(threadChannel, message));
     }
 }
---
+++ src/main/java/ti4/listeners/UserLeaveServerListener.java
@@ -0,0 +1,121 @@
+package ti4.listeners;
+
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import net.dv8tion.jda.api.audit.ActionType;
+import net.dv8tion.jda.api.audit.AuditLogEntry;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
+import net.dv8tion.jda.api.events.guild.GenericGuildEvent;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import ti4.AsyncTI4DiscordBot;
+import ti4.helpers.Helper;
+import ti4.helpers.ThreadGetter;
+import ti4.helpers.ToStringHelper;
+import ti4.map.manage.GameManager;
+import ti4.map.manage.ManagedGame;
+import ti4.map.manage.ManagedPlayer;
+import ti4.message.BotLogger;
+import ti4.message.MessageHelper;
+import ti4.service.game.ManagedGameService;
+
+public class UserLeaveServerListener extends ListenerAdapter {
+
+    @Override
+    public void onGuildMemberRemove(@Nonnull GuildMemberRemoveEvent event) {
+        if (!validateEvent(event)) return;
+        AsyncTI4DiscordBot.runAsync("Guild member remove task", () -> handleGuildMemberRemove(event));
+    }
+
+    private void handleGuildMemberRemove(GuildMemberRemoveEvent event) {
+        try {
+            event.getGuild().retrieveAuditLogs().queueAfter(1, TimeUnit.SECONDS, (logs) -> {
+                boolean voluntary = true;
+                for (AuditLogEntry log : logs) {
+                    if (log.getTargetIdLong() == event.getUser().getIdLong()) {
+                        if (log.getType() == ActionType.BAN || log.getType() == ActionType.KICK) {
+                            voluntary = false;
+                            break;
+                        }
+                    }
+                }
+
+                checkIfUserLeftActiveGames(event.getGuild(), event.getUser(), voluntary);
+            }, BotLogger::catchRestError);
+        } catch (Exception e) {
+            BotLogger.log("Error in `UserJoinServerListener.onGuildMemberRemove`", e);
+        }
+    }
+
+    private static boolean validateEvent(GenericGuildEvent event) {
+        if (!AsyncTI4DiscordBot.isReadyToReceiveCommands()) {
+            return false;
+        }
+        String eventGuild = event.getGuild().getId();
+        return AsyncTI4DiscordBot.isValidGuild(eventGuild);
+    }
+
+    private void checkIfUserLeftActiveGames(Guild guild, User user, boolean voluntary) {
+        List<ManagedGame> gamesQuit = new ArrayList<>();
+
+        ManagedPlayer player = GameManager.getManagedPlayer(user.getId());
+        for (ManagedGame game : player.getGames()) {
+            if (game.isHasEnded() || game.isVpGoalReached()) continue;
+            Guild gameGuild = game.getGuild();
+            if (gameGuild != null && gameGuild.equals(guild)) {
+                gamesQuit.add(game);
+            }
+        }
+
+        if (!gamesQuit.isEmpty()) {
+            StringBuilder msg = new StringBuilder("User " + user.getName() + " has left the server " + guild.getName() + " with the following in-progress games:");
+            for (ManagedGame game : gamesQuit) {
+                logManagedGame(game);//TODO: can be removed after debugging user leaving issues
+                String gameMessage = "Attention " + ManagedGameService.getPingAllPlayers(game) + ": " + user.getName();
+                if (voluntary) gameMessage += " has left the server.\n> If this was not a mistake, you may make ";
+                if (!voluntary) gameMessage += " was removed from the server.\n> Make ";
+                gameMessage += "a post in https://discord.com/channels/943410040369479690/1176191865188536500 to get a replacement player";
+                MessageHelper.sendMessageToChannel(game.getTableTalkChannel(), gameMessage);
+                msg.append("\n> ").append(game.getName()).append(" -> Link:").append(game.getTableTalkChannel().getJumpUrl());
+            }
+            reportUserLeftServer(msg.toString());
+
+            String inviteBack = Helper.getGuildInviteURL(guild, 1);
+            String primaryInvite = Helper.getGuildInviteURL(AsyncTI4DiscordBot.guildPrimary, 1, true);
+            String usermsg = "It looks like you left a server while playing in `" + gamesQuit.size() + "` games.";
+            usermsg += " Please consider making a post in https://discord.com/channels/943410040369479690/1176191865188536500 to get a replacement player.\n\n";
+            usermsg += "If this was a mistake, here is an invite back to the server you just left: " + inviteBack + "\n";
+            usermsg += "If you are just taking a break, here is an invite to the HUB server that will last until you're ready to come back: " + primaryInvite + "\n\n";
+            usermsg += "Take care!\n> - Async TI4 Admin Team";
+            if (voluntary) {
+                MessageHelper.sendMessageToUser(usermsg, user);
+            }
+        }
+    }
+
+    private void logManagedGame(ManagedGame managedGame) {
+        BotLogger.log("ManagedGame of quit game: " +
+            ToStringHelper.of(ManagedGame.class)
+                .add("gameName", managedGame.getGame())
+                .add("lastModifiedDate", managedGame.getLastModifiedDate())
+                .add("guild", managedGame.getGuild().getName())
+                .add("vpGoalReached", managedGame.isVpGoalReached())
+                .add("hasEnded", managedGame.isHasEnded()));
+    }
+
+    private static void reportUserLeftServer(String message) {
+        TextChannel bothelperLoungeChannel = AsyncTI4DiscordBot.guildPrimary.getTextChannelsByName("staff-lounge", true).stream().findFirst().orElse(null);
+        if (bothelperLoungeChannel == null) return;
+        List<ThreadChannel> threadChannels = bothelperLoungeChannel.getThreadChannels();
+        if (threadChannels.isEmpty()) return;
+        String threadName = "in-progress-games-left";
+        ThreadGetter.getThreadInChannel(bothelperLoungeChannel, threadName,
+            threadChannel -> MessageHelper.sendMessageToChannel(threadChannel, message));
+    }
+}
