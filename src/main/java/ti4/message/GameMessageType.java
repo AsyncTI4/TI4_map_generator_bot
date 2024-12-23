119c81ceb71393fdf99d6d31bd5b40a04b5f6987
Will <willholton1@gmail.com>
2024-12-23 09:24:11 -0500

    Update GameMessageType.java

--- src/main/java/ti4/message/GameMessageType.java
+++ src/main/java/ti4/message/GameMessageType.java
@@ -1,8 +1,8 @@
 package ti4.message;
 
 public enum GameMessageType {
-        AGENDA_WHEN,
-        AGENDA_AFTER,
-        ACTION_CARD,
-        SHENANIGANS_PASS
-    }
+    AGENDA_WHEN,
+    AGENDA_AFTER,
+    ACTION_CARD,
+    SHENANIGANS_PASS
+}
