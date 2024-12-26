ef574f30c7215f80328b46f68ebc6401acba3f3c
Will Holton <willholton1@gmail.com>
2024-12-26 14:36:32 -0500

    Update RemoveTitle.java (#2676)
    * Update RemoveTitle.java
    
    * Update RemoveTitle.java

--- src/main/java/ti4/commands2/bothelper/RemoveTitle.java
+++ src/main/java/ti4/commands2/bothelper/RemoveTitle.java
@@ -3,15 +3,16 @@ package ti4.commands2.bothelper;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
-import ti4.commands2.GameStateSubcommand;
+import ti4.commands2.Subcommand;
 import ti4.helpers.Constants;
 import ti4.map.Game;
+import ti4.map.manage.GameManager;
 import ti4.message.MessageHelper;
 
-class RemoveTitle extends GameStateSubcommand {
+class RemoveTitle extends Subcommand {
 
     public RemoveTitle() {
-        super(Constants.REMOVE_TITLE, "Remove a title", true, false);
+        super(Constants.REMOVE_TITLE, "Remove a title");
         addOptions(new OptionData(OptionType.STRING, Constants.GAME_NAME, "The game name where the title was given").setRequired(true).setAutoComplete(true));
         addOptions(new OptionData(OptionType.USER, Constants.PLAYER, "Player @playername").setRequired(true));
         addOptions(new OptionData(OptionType.STRING, Constants.TITLE, "Title to Remove").setRequired(true));
@@ -19,10 +20,16 @@ class RemoveTitle extends GameStateSubcommand {
 
     @Override
     public void execute(SlashCommandInteractionEvent event) {
-        Game game = getGame();
-        String title = event.getOption(Constants.TITLE).getAsString();
+        String gameName = event.getOption(Constants.GAME_NAME).getAsString();
+        if (!GameManager.isValid(gameName)) {
+            MessageHelper.sendMessageToChannel(event.getMessageChannel(), "Game " + gameName + " not found");
+            return;
+        }
+        Game game = GameManager.getManagedGame(gameName).getGame();
         String userId = event.getOption(Constants.PLAYER).getAsUser().getId();
+        String title = event.getOption(Constants.TITLE).getAsString();
         game.setStoredValue("TitlesFor" + userId, game.getStoredValue("TitlesFor" + userId).replace(title, ""));
+
         MessageHelper.sendMessageToChannel(event.getMessageChannel(), "Removed the title " + title + " in game " + game.getName());
     }
 }
