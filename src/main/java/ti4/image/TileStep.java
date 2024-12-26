54faf60c27098691d943e7cea747af356fc81cf6
Esko Poutanen <esko.pylsy@gmail.com>
2024-12-26 20:06:52 +0200

    Paint tile number last (#2675) [skip ci]

--- src/main/java/ti4/image/TileStep.java
+++ src/main/java/ti4/image/TileStep.java
@@ -2,5 +2,5 @@ package ti4.image;
 
 public enum TileStep {
     Setup, Tile, Extras, Units, Distance, Wormholes, Anomalies, Aetherstream,
-    Legendaries, Empties, SpaceCannon, Traits, TechSkips, Attachments
+    Legendaries, Empties, SpaceCannon, Traits, TechSkips, Attachments, LastStep
 }
