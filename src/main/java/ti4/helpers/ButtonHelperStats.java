7448b6d00eca370a4fccd59ee7c80d3374042dd1
bradleysigma <bradley.sigma@gmail.com>
2025-01-09 18:51:57 +1000

    Updates

--- src/main/java/ti4/helpers/ButtonHelperStats.java
+++ src/main/java/ti4/helpers/ButtonHelperStats.java
@@ -77,27 +77,31 @@ public class ButtonHelperStats {
     }
 
     public static void gainComms(GenericInteractionCreateEvent event, Game game, Player player, int amt, boolean deleteMsg, boolean skipOutput) {
-        String message, ident = player.getFactionEmojiOrColor();
+        String message = player.getRepresentationNoPing();
+        String fogMessage;
         int initComm = player.getCommodities();
-        if (player.getCommodities() + amt >= player.getCommoditiesTotal()) {
+        if (player.getCommodities() + amt > player.getCommoditiesTotal()) {
             player.setCommodities(player.getCommoditiesTotal());
             int gained = player.getCommodities() - initComm;
-            message = "Gained " + gained + " Commodities (" + initComm + "->" + player.getCommoditiesRepresentation() + ")";
+            message += " gained " + gained + " commodit" + (gained == 1 ? "y" : "ies") + " (" + initComm + "->" + player.getCommoditiesRepresentation() 
+                + ").\n-# They would have gained " + amt + " but were limited by their faction's commodity value.";
+            fogMessage = "Gained " + gained + " commodit" + (gained == 1 ? "y" : "ies") + " (" + initComm + "->" + player.getCommoditiesRepresentation() + ").";
         } else {
             player.setCommodities(player.getCommodities() + amt);
-            message = "Gained " + amt + " Commodities (" + initComm + "->" + player.getCommoditiesRepresentation() + ")";
+            message += " gained " + amt + " commodit" + (amt == 1 ? "y" : "ies") + " (" + initComm + "->" + player.getCommoditiesRepresentation() + ").";
+            fogMessage = "Gained " + amt + " commodi" + (amt == 1 ? "y" : "ies") + " (" + initComm + "->" + player.getCommoditiesRepresentation() + ").";
         }
         int finalComm = player.getCommodities();
 
-        if (!skipOutput) MessageHelper.sendMessageToChannel(player.getCorrectChannel(), ident + " " + message);
-        if (game.isFowMode()) FoWHelper.pingAllPlayersWithFullStats(game, event, player, message);
+        if (!skipOutput) MessageHelper.sendMessageToChannel(player.getCorrectChannel(), message);
+        if (game.isFowMode()) FoWHelper.pingAllPlayersWithFullStats(game, event, player, fogMessage);
 
         if (deleteMsg) ButtonHelper.deleteMessage(event);
         afterGainCommsChecks(game, player, finalComm - initComm);
     }
 
     public static void replenishComms(GenericInteractionCreateEvent event, Game game, Player player, boolean skipOutput) {
-        String message, ident = player.getFactionEmojiOrColor();
+        String message, ident = player.getRepresentationNoPing();
         int initComm = player.getCommodities();
         if (player.getCommodities() < player.getCommoditiesTotal()) {
             player.setCommodities(player.getCommoditiesTotal());
