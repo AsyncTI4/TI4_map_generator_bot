05bb705c4c5ec5c268cd0a66cd8d7ed918b883c7
Will Holton <willholton1@gmail.com>
2025-01-18 05:30:40 -0500

    reloadgame command (#2775)
    reloadgame command

---
+++ src/main/java/ti4/commands/admin/ReloadGame.java
@@ -0,0 +1,32 @@
+package ti4.commands.admin;
+
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import ti4.commands.Subcommand;
+import ti4.helpers.Constants;
+import ti4.map.Game;
+import ti4.map.manage.GameManager;
+import ti4.message.MessageHelper;
+
+public class ReloadGame extends Subcommand {
+
+    public ReloadGame()  {
+        super(Constants.RELOAD_GAME, "Reload game from disk");
+        addOptions(
+            new OptionData(OptionType.STRING, Constants.GAME_NAME, "Game name to load to").setRequired(true));
+    }
+
+    @Override
+    public void execute(SlashCommandInteractionEvent event) {
+        String gameName = event.getOption(Constants.GAME_NAME).getAsString();
+        Game game = GameManager.reload(gameName);
+        if (game == null) {
+            MessageHelper.sendMessageToEventChannel(event, "Failed to reload game.");
+            return;
+        }
+
+        MessageHelper.sendMessageToEventChannel(event, game.getName() + " reloaded.");
+        MessageHelper.sendMessageToChannel(game.getActionsChannel(), game.getName() + " was reloaded.");
+    }
+}
