fe1e69e83c95d5cd3dca436e92e6051b6c7b56ca
Solaxi <esko.pylsy@gmail.com>
2025-02-21 09:28:36 +0200

    Fixed comm thread ping in community games

--- src/main/java/ti4/service/fow/FowCommunicationThreadService.java
+++ src/main/java/ti4/service/fow/FowCommunicationThreadService.java
@@ -21,7 +21,6 @@ import ti4.listeners.annotations.ButtonHandler;
 import ti4.map.Game;
 import ti4.map.Player;
 import ti4.message.MessageHelper;
-import ti4.service.emoji.ColorEmojis;
 
 public class FowCommunicationThreadService {
 
@@ -136,8 +135,8 @@ public class FowCommunicationThreadService {
         String threadName = StringUtils.capitalize(inviteePlayer.getColor()) + " " + YES_CHAR + " " + StringUtils.capitalize(player.getColor());
         game.getMainGameChannel().createThreadChannel(threadName, true).queue(t -> {
             MessageHelper.sendMessageToChannel(t, "## Private communications thread opened\n"
-                + "Players: " + ColorEmojis.getColorEmojiWithName(inviteePlayer.getColor()) + " " + inviteePlayer.getPing() 
-                + " " + ColorEmojis.getColorEmojiWithName(player.getColor()) + " " + player.getPing() + "\n"
+                + "Players: " + inviteePlayer.getRepresentation(true, true, false, true)
+                + " " + player.getRepresentation(true, true, false, true) + "\n"
                 + "GM ping: " + game.getPlayersWithGMRole().stream().map(gm -> gm.getPing()).collect(Collectors.joining(" ")));
         });
 
