46ec3a30772fa34a837ad270144955261e38af09
Solaxi <esko.pylsy@gmail.com>
2025-03-15 18:54:40 +0200

    Added spaces

--- src/main/java/ti4/service/fow/RiftSetModeService.java
+++ src/main/java/ti4/service/fow/RiftSetModeService.java
@@ -173,11 +173,11 @@ public class RiftSetModeService {
         if (RandomHelper.isOneInX(CHANCE_TO_SPAWN_VORTEX)) {
             AddTokenCommand.addToken(event, tile, "vortex", game);
             MessageHelper.sendMessageToChannel(player.getCorrectChannel(), "## A strange Vortex has formed in " + tile.getPosition()
-                + "\n-#" + getGMs(game));
+                + "\n-# " + getGMs(game));
         } else  if (RandomHelper.isOneInX(CHANCE_TO_SPAWN_RIFT)) {
             AddTokenCommand.addToken(event, tile, "gravityrift", game);
             MessageHelper.sendMessageToChannel(player.getCorrectChannel(), "## A new Gravity Rift has formed in " + tile.getPosition()
-                + "\n-#" + getGMs(game));
+                + "\n-# " + getGMs(game));
         }
     }
 
@@ -194,7 +194,7 @@ public class RiftSetModeService {
 
         if (RandomHelper.isOneInX(Math.max(CHANCE_TO_STELLAR_CONVERT - (int) (16 * Math.pow(Math.min(game.getRound(), 6) - 1, 2)), CHANCE_TO_STELLAR_CONVERT_MIN))) {
             MessageHelper.sendMessageToChannel(player.getCorrectChannel(), "## While trying to explore the planet, you find something dark and dangerous..."
-                + "\n-#" + getGMs(game));
+                + "\n-# " + getGMs(game));
             StellarConverterService.secondHalfOfStellar(game, planetName, event);
             Tile tile = game.getTileFromPlanet(planetName);
             UnitHolder unitHolder = tile.getUnitHolderFromPlanet(planetName);
