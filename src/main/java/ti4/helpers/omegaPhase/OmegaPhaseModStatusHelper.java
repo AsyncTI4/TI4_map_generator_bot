1c9a317b5b3a913c7bf6465aa4662d2de8bebe31
Clayton Willey <willeyshm@gmail.com>
2025-04-23 20:42:00 -0400

    greeting update

--- src/main/java/ti4/helpers/omegaPhase/OmegaPhaseModStatusHelper.java
+++ src/main/java/ti4/helpers/omegaPhase/OmegaPhaseModStatusHelper.java
@@ -5,18 +5,17 @@ import ti4.message.MessageHelper;
 
 public class OmegaPhaseModStatusHelper {
     public static void PrintGreeting(Game game) {
-        String greeting = "### " + game.getPing() + " **Omega Phase v0.1**\n\n" +
+        String greeting = "### " + game.getPing() + " **Omega Phase v1**\n\n" +
             "Omega Phase has been enabled! This overhaul will apply several changes to the game, including:\n" +
-            "- A Priority Track which is used to determine the non-Initiative order of play\n" +
-            "- A new Agenda \"Voice of the Council\" which the Speaker can choose to resolve after the second agenda is resolved\n" +
-            "- Everyone must exhaust a planet to cast votes if they have any available during every agenda\n" +
-            "- Everyone but the Speaker votes simultaneously, without knowing how others will vote. Then the Speaker votes and proceeds to resolve the Agenda.\n" +
-            "- Scoring happens at the end of each round, after the Status and Agenda phases\n" +
-            "- Objectives have been revamped, and are all worth 1 VP. The Speaker peeks at the next Objective when a new one is revealed after scoring.\n\n" +
-            "This mod is in alpha, and you'll have to use some workarounds for the time being.\n" +
-            "- Voting is currently skipped, skipping straight to buttons to resolve the Agenda. Players should privately message the Speaker which planets they will exhaust, and the Speaker will reveal all player's exhausted planets together when all voting players have done so. At this time, players may use relevant abilities that apply when voting. Players should sum their planets' votes and then note their actual final vote tally after all effects are accounted for. Then the Speaker votes, and uses the appropriate button to resolve the correct outcome.\n"
-            +
-            "- After electing a new Voice of the Council, use `/omegaphase elect_voice_of_the_council` to elect them and update the points.\n";
+            "- When not in Strategy Card order, your turn happens in the order you last passed in, which is shown on the Priority Track.\n" +
+            "- The Priority Track is also used for scoring order.\n" +
+            "- A new Agenda \"Voice of the Council\" starts in the common play area. After the first 2 agendas fully resolve:" +
+            "  - A. if VotC is neutral, it must be voted on now like a third agenda.\n" +
+            "  - B. if VotC is held by a player, the Speaker may choose to initiate a re-vote on it.\n" +
+            "- During every agenda, everyone MUST vote if they have any unexhausted voting planets left.\n" +
+            "- Everyone but the Speaker votes simultaneously, without knowing how others will vote. Use the pre-vote buttons to do this.\n" +
+            "- Scoring happens at the end of each round, after the other Status and Agenda steps\n" +
+            "- Objectives have been revamped, and are all worth 1 VP. The Speaker peeks at the next Objective when a new one is revealed after scoring.";
         MessageHelper.sendMessageToChannelAndPin(game.getActionsChannel(), greeting);
     }
 }
