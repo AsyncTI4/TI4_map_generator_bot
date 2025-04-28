c1d8ba68fe8ee3cb3b79ac9a28cbf5457d340f14
Fingon00 <britainfan234@gmail.com>
2025-04-28 16:51:09 -0400

    Qhet automation

--- src/main/java/ti4/helpers/AgendaHelper.java
+++ src/main/java/ti4/helpers/AgendaHelper.java
@@ -921,7 +921,7 @@ public class AgendaHelper {
         }
         for (Player player : game.getRealPlayers()) {
             for (Player p2 : game.getRealPlayers()) {
-                if (p2 == player || (player.getTg() + player.getCommodities()) < 0 || p2.hasAbility("binding_debts")
+                if (p2 == player || (player.getTg() + player.getCommodities()) < 0 || p2.hasAbility("binding_debts") || p2.hasAbility("data_recovery")
                     || p2.hasAbility("fine_print") || p2.getDebtTokenCount(player.getColor()) < 1) {
                     continue;
                 }
