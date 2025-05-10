6a0e0abd543bb83118cacbf13e290517e606789c
Chase Hinesman <chasehinesman@gmail.com>
2025-05-10 12:03:45 -0500

    Fix botlogger logging discrepencies & enable search commands on other servers

--- src/main/java/ti4/commands/search/SearchCommand.java
+++ src/main/java/ti4/commands/search/SearchCommand.java
@@ -50,4 +50,27 @@ public class SearchCommand implements ParentCommand {
     public Map<String, Subcommand> getSubcommands() {
         return subcommands;
     }
+
+    private final Map<String, Subcommand> searchSubcommands = Stream.of(
+        new SearchAbilitiesSubcommand(),
+        new SearchPlanetsSubcommand(),
+        new SearchTilesSubcommand(),
+        new SearchUnitsSubcommand(),
+        new SearchAgendasSubcommand(),
+        new SearchSecretObjectivesSubcommand(),
+        new SearchPublicObjectivesSubcommand(),
+        new SearchRelicsSubcommand(),
+        new SearchActionCardsSubcommand(),
+        new SearchTechsSubcommand(),
+        new SearchLeadersSubcommand(),
+        new SearchPromissoryNotesSubcommand(),
+        new SearchExploresSubcommand(),
+        new SearchDecksSubcommand(),
+        new SearchFactionsSubcommand(),
+        new SearchStrategyCardsSubcommand()).collect(Collectors.toMap(Subcommand::getName, subcommand -> subcommand));
+
+    @Override
+    public Map<String, Subcommand> getSearchSubcommands() {
+        return searchSubcommands;
+    }
 }
