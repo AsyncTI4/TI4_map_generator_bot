acae08b3757b9f54308db9e7a312081ccec87c32
Solaxi <esko.pylsy@gmail.com>
2025-05-22 17:22:59 +0300

    Don't show votes in FoW

--- src/main/java/ti4/helpers/AgendaHelper.java
+++ src/main/java/ti4/helpers/AgendaHelper.java
@@ -2655,7 +2655,9 @@ public class AgendaHelper {
         List<Button> proceedButtons = new ArrayList<>();
         String msg = "Buttons for various things";
 
-        listVoteCount(game, game.getMainGameChannel());
+        if (!game.isFowMode()) {
+            listVoteCount(game, game.getMainGameChannel());
+        }
 
         proceedButtons.add(Buttons.red("proceedToVoting", "Skip Waiting"));
         proceedButtons.add(Buttons.blue("transaction", "Transaction"));
