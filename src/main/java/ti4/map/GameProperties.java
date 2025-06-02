bc23b7dbb0bbc1ecabca0ce817fdb97743fb008f
Cryypter <jwalkerjohnson@gmail.com>
2025-06-01 21:22:58 -0400

    Update GameProperties.java
    Removing duplicate entry

--- src/main/java/ti4/map/GameProperties.java
+++ src/main/java/ti4/map/GameProperties.java
@@ -112,7 +112,6 @@ public class GameProperties {
     private @ExportableField boolean omegaPhaseMode;
     private @ExportableField boolean homebrew;
     private @ExportableField boolean homebrewSCMode;
-    private @ExportableField boolean votcMode;
     private @ExportableField String spinMode = "OFF";
     private @ExportableField boolean fastSCFollowMode;
     private @ExportableField boolean extraSecretMode;
