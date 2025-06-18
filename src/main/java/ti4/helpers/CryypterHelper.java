05a232cb35e0c55fd11a06aa612959e447826eb7
Cryypter <jwalkerjohnson@gmail.com>
2025-06-18 10:14:37 -0400

    Update CryypterHelper.java

--- src/main/java/ti4/helpers/CryypterHelper.java
+++ src/main/java/ti4/helpers/CryypterHelper.java
@@ -3,6 +3,7 @@ package ti4.helpers;
 import java.util.List;
 
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.GenericInteractionCreateEvent;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import ti4.buttons.Buttons;
 import ti4.image.Mapper;
