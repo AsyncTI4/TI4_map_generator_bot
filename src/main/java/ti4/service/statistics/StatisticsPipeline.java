83c66af10a69387fb00cd6a5717c10d7e0ff86ff
Will Holton <willholton1@gmail.com>
2025-06-18 03:00:34 -0400

    Execution history (#3227)
    * Execution history
    
    * Update ExecutionHistoryManager.java
    
    * Update acd2.json

--- src/main/java/ti4/service/statistics/StatisticsPipeline.java
+++ src/main/java/ti4/service/statistics/StatisticsPipeline.java
@@ -1,42 +1,28 @@
 package ti4.service.statistics;
 
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 import net.dv8tion.jda.api.interactions.callbacks.IReplyCallback;
+import ti4.executors.CircuitBreaker;
+import ti4.executors.ExecutionHistoryManager;
 import ti4.helpers.TimedRunnable;
 import ti4.message.BotLogger;
-import ti4.message.MessageHelper;
 
 public class StatisticsPipeline {
 
-    private static final int TASK_TIMEOUT_SECONDS = 30;
     private static final int SHUTDOWN_TIMEOUT_SECONDS = 20;
     private static final int EXECUTION_TIME_SECONDS_WARNING_THRESHOLD = 10;
     private static final ExecutorService EXECUTOR_SERVICE = Executors.newSingleThreadExecutor();
 
     public static void queue(StatisticsPipeline.StatisticsEvent event) {
+        if (CircuitBreaker.checkIsOpenAndPostWarningIfTrue(event.event().getMessageChannel())) {
+            return;
+        }
         event.event.getHook().sendMessage("Your statistics are being processed, please hold...").setEphemeral(true).queue();
-
-        var timedRunnable = new TimedRunnable("Statistics event task for " + event.name,
-            EXECUTION_TIME_SECONDS_WARNING_THRESHOLD,
-            event.runnable);
-
-        CompletableFuture.runAsync(timedRunnable, EXECUTOR_SERVICE)
-            .orTimeout(TASK_TIMEOUT_SECONDS, TimeUnit.SECONDS)
-            .whenComplete((result, exception) -> {
-                String userMention = event.event.getUser().getAsMention();
-                if (exception instanceof TimeoutException) {
-                    BotLogger.error("Timeout occurred while processing statistics: " + event.name, exception);
-                    MessageHelper.sendMessageToChannel(event.event.getMessageChannel(),
-                        "A timeout occurred while processing statistics for " + userMention + " running task: " + event.name);
-                } else if (exception != null) {
-                    BotLogger.error("Error occurred while processing statistics for " + userMention + " running task: " + event.name, exception);
-                }
-            });
+        var timedRunnable = new TimedRunnable("Statistics event task for " + event.name, EXECUTION_TIME_SECONDS_WARNING_THRESHOLD, event.runnable);
+        ExecutionHistoryManager.runWithExecutionHistory(EXECUTOR_SERVICE, timedRunnable);
     }
 
     public static boolean shutdown() {
