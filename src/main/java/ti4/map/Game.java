a2c5e9fc4f32aee5313515d3d26317344b567c71
Will <willholton1@gmail.com>
2025-07-18 11:15:09 -0400

    Add new modes to homebrew

--- src/main/java/ti4/map/Game.java
+++ src/main/java/ti4/map/Game.java
@@ -1,9 +1,6 @@
 package ti4.map;
 
-import static java.util.function.Predicate.*;
-import static org.apache.commons.collections4.CollectionUtils.*;
-
-import java.awt.Point;
+import java.awt.*;
 import java.lang.reflect.Field;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
@@ -25,10 +22,6 @@ import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
-import org.apache.commons.lang3.StringUtils;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
 import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -36,7 +29,6 @@ import com.fasterxml.jackson.annotation.JsonSetter;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-
 import lombok.Getter;
 import lombok.Setter;
 import net.dv8tion.jda.api.entities.Guild;
@@ -49,6 +41,9 @@ import net.dv8tion.jda.api.events.interaction.GenericInteractionCreateEvent;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import net.dv8tion.jda.internal.utils.tuple.ImmutablePair;
 import net.dv8tion.jda.internal.utils.tuple.Pair;
+import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import ti4.AsyncTI4DiscordBot;
 import ti4.commands.planet.PlanetRemove;
 import ti4.draft.BagDraft;
@@ -98,6 +93,9 @@ import ti4.service.emoji.SourceEmojis;
 import ti4.service.leader.CommanderUnlockCheckService;
 import ti4.service.milty.MiltyDraftManager;
 import ti4.service.option.FOWOptionService.FOWOption;
+
+import static java.util.function.Predicate.not;
+import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
 
 public class Game extends GameProperties {
 
@@ -440,10 +438,7 @@ public class Game extends GameProperties {
                                 ally = p2;
                             }
                         }
-                        boolean allyGood = false;
-                        if (ally != null && ally.getTotalVictoryPoints() >= getVp() && (player.getTotalVictoryPoints() > 11 || ally.getTotalVictoryPoints() > 11)) {
-                            allyGood = true;
-                        }
+                        boolean allyGood = ally != null && ally.getTotalVictoryPoints() >= getVp() && (player.getTotalVictoryPoints() > 11 || ally.getTotalVictoryPoints() > 11);
                         if (!allyGood) {
                             continue;
                         }
@@ -4246,8 +4241,10 @@ public class Game extends GameProperties {
             || isExtraSecretMode()
             || isFowMode()
             || isAgeOfExplorationMode()
-            || isFacilitiesMode()
+            || isTotalWarMode()
+            || isAgeOfCommerceMode()
             || isMinorFactionsMode()
+            || isFacilitiesMode()
             || isLightFogMode()
             || isRedTapeMode()
             || isDiscordantStarsMode()
