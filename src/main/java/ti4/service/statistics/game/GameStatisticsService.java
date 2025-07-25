693d89d30d2dd4ef114b7339624c9143bbac5707
Will Holton <willholton1@gmail.com>
2025-07-25 17:04:12 -0400

    Update Game.java (#3377)
    * Update Game.java
    
    * Update GameStatisticsService.java
    
    * Update WinningPathsStatisticsService.java
    
    * F

--- src/main/java/ti4/service/statistics/game/GameStatisticsService.java
+++ src/main/java/ti4/service/statistics/game/GameStatisticsService.java
@@ -4,6 +4,7 @@ import lombok.experimental.UtilityClass;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import ti4.helpers.Constants;
+import ti4.message.BotLogger;
 import ti4.message.MessageHelper;
 import ti4.service.statistics.StatisticsPipeline;
 
@@ -21,28 +22,28 @@ public class GameStatisticsService {
     }
 
     private void getGameStatistics(SlashCommandInteractionEvent event, GameStatTypes statType) {
-        switch (statType) {
-            case UNLEASH_THE_NAMES -> AllNamesStatisticsService.sendAllNames(event);
-            case HIGHEST_SPENDERS -> SpendToWinCorrelationStatisticsService.calculateSpendToWinCorrelation(event);
-            case GAME_LENGTH -> GameLengthStatisticsService.showGameLengths(event, 3650);
-            case GAME_LENGTH_4MO -> GameLengthStatisticsService.showGameLengths(event, 120);
-            case FACTIONS_PLAYED -> MostPlayedFactionsStatisticsService.showMostPlayedFactions(event);
-            case AVERAGE_TURNS -> FactionAverageTurnsInGameStatisticsService.averageTurnsInAGameByFaction(event);
-            case COLOURS_PLAYED -> MostPlayerColorService.getMostPlayedColour(event);
-            case FACTION_WINS -> MostWinningFactionsStatisticsService.getMostWinningFactions(event);
-            case PHASE_TIMES -> RoundTimeStatisticsService.getRoundTimes(event);
-            case SOS_SCORED -> VictoryPointsScoredStatisticsService.listScoredVictoryPoints(event);
-            case FACTION_WIN_PERCENT -> FactionWinPercentStatisticsService.getFactionWinPercent(event);
-            case FACTION_PERFORMANCE -> FactionPerformanceStatisticsService.showFactionPerformance(event);
-            case COLOUR_WINS -> MostWinningColorStatisticsService.showMostWinningColor(event);
-            case GAME_COUNT -> GameCountStatisticsService.getGameCount(event);
-            case WINNING_PATH -> WinningPathsStatisticsService.showWinningPaths(event);
-            case SUPPORT_WIN_COUNT -> WinningPathsStatisticsService.showWinsWithSupport(event);
-            default -> MessageHelper.sendMessageToChannel(event.getChannel(), "Unknown Statistic: " + statType);
+        try {
+            switch (statType) {
+                case UNLEASH_THE_NAMES -> AllNamesStatisticsService.sendAllNames(event);
+                case HIGHEST_SPENDERS -> SpendToWinCorrelationStatisticsService.calculateSpendToWinCorrelation(event);
+                case GAME_LENGTH -> GameLengthStatisticsService.showGameLengths(event, 3650);
+                case GAME_LENGTH_4MO -> GameLengthStatisticsService.showGameLengths(event, 120);
+                case FACTIONS_PLAYED -> MostPlayedFactionsStatisticsService.showMostPlayedFactions(event);
+                case AVERAGE_TURNS -> FactionAverageTurnsInGameStatisticsService.averageTurnsInAGameByFaction(event);
+                case COLOURS_PLAYED -> MostPlayerColorService.getMostPlayedColour(event);
+                case FACTION_WINS -> MostWinningFactionsStatisticsService.getMostWinningFactions(event);
+                case PHASE_TIMES -> RoundTimeStatisticsService.getRoundTimes(event);
+                case SOS_SCORED -> VictoryPointsScoredStatisticsService.listScoredVictoryPoints(event);
+                case FACTION_WIN_PERCENT -> FactionWinPercentStatisticsService.getFactionWinPercent(event);
+                case FACTION_PERFORMANCE -> FactionPerformanceStatisticsService.showFactionPerformance(event);
+                case COLOUR_WINS -> MostWinningColorStatisticsService.showMostWinningColor(event);
+                case GAME_COUNT -> GameCountStatisticsService.getGameCount(event);
+                case WINNING_PATH -> WinningPathsStatisticsService.showWinningPaths(event);
+                case SUPPORT_WIN_COUNT -> WinningPathsStatisticsService.showWinsWithSupport(event);
+                default -> MessageHelper.sendMessageToChannel(event.getChannel(), "Unknown Statistic: " + statType);
+            }
+        } catch (Exception e) {
+            BotLogger.error("Failed to process statistic: " + statType, e);
         }
-    }
-
-    public String getWinningPathComparison(String winningPath, int playerCount, int totalVictoryPoints) {
-        return WinningPathsStatisticsService.compareWinningPathToAllOthers(winningPath, playerCount, totalVictoryPoints);
     }
 }
