48bf65b449f805474b557f4aa5d04724eace9701
Will <willholton1@gmail.com>
2025-07-26 09:00:28 -0400

    Fixes

--- src/main/java/ti4/controller/ActionCardController.java
+++ src/main/java/ti4/controller/ActionCardController.java
@@ -1,12 +1,13 @@
 package ti4.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ti4.controller.validator.GameNameValidator;
-import ti4.map.manage.GameManager;
+import ti4.map.persistence.GameManager;
 import ti4.message.MessageHelper;
 
 @RestController
@@ -15,6 +16,15 @@ public class ActionCardController {
 
     @PostMapping("/game/{gameName}/shuffle")
     public ResponseEntity<String> shuffle(@PathVariable String gameName) {
+        GameNameValidator.validate(gameName);
+        var game = GameManager.getManagedGame(gameName).getGame();
+        game.shuffleActionCards();
+        MessageHelper.sendMessageToChannel(game.getActionsChannel(), "Shuffled the action card deck.");
+        return ResponseEntity.ok("Shuffled the action card deck.");
+    }
+
+    @GetMapping("/game/{gameName}/{player}")
+    public ResponseEntity<String> getPlayerHand(@PathVariable String gameName, @PathVariable String player) {
         GameNameValidator.validate(gameName);
         var game = GameManager.getManagedGame(gameName).getGame();
         game.shuffleActionCards();
