976fb498dffec6dee6693e95c889c72302e130f3
bradleysigma <42644678+bradleysigma@users.noreply.github.com>
2025-07-29 01:41:23 +1000

    Updates (#3418)
    * Update StartPhaseService.java
    
    * Update action_cards.json
    
    * Updates
    
    * Update AgendaHelper.java

--- src/main/java/ti4/helpers/AgendaHelper.java
+++ src/main/java/ti4/helpers/AgendaHelper.java
@@ -1273,7 +1273,7 @@ public class AgendaHelper {
                     Helper.buildSpentThingsMessageForVoting(player, game, false));
             }
 
-            String message = " up to vote! Resolve using buttons.";
+            String message = " up to vote! Please use the buttons to choose the outcome you wish to vote for.";
             Button eraseAndReVote = Buttons.red("eraseMyVote", "Erase my vote & have me vote again");
             String revoteMsg = "You may press this button to revote if you made a mistake, ignore it otherwise.";
             MessageHelper.sendMessageToChannelWithButton(player.getCardsInfoThread(), revoteMsg, eraseAndReVote);
@@ -1736,7 +1736,6 @@ public class AgendaHelper {
                 return;
             }
             String realIdentity = nextInLine.getRepresentationUnfogged();
-            String message = getSummaryOfVotes(game, true) + "\n" + realIdentity + " up to vote! Resolve using buttons.";
             int[] voteInfo = getVoteTotal(nextInLine, game);
             int counter = 0;
             boolean willPrevote = !game.getStoredValue("preVoting" + nextInLine.getFaction()).isEmpty() && !game.getStoredValue("preVoting" + nextInLine.getFaction()).equalsIgnoreCase("0");
@@ -1780,6 +1779,7 @@ public class AgendaHelper {
                 counter += 1;
             }
 
+            String message = getSummaryOfVotes(game, true) + "\n" + realIdentity + " up to vote! Please use the buttons to choose the outcome you wish to vote for.";
             String pFaction = StringUtils.capitalize(nextInLine.getFaction());
             String finChecker = "FFCC_" + nextInLine.getFaction() + "_";
             Button Vote = Buttons.green(finChecker + "vote", pFaction + " Choose To Vote");
