ec565e08fa2216bb788075051da7740abbe2f946
Will <willholton1@gmail.com>
2025-08-15 17:23:32 -0400

    Update DiscordOpaqueTokenIntrospector.java

--- src/main/java/ti4/spring/service/auth/DiscordOpaqueTokenIntrospector.java
+++ src/main/java/ti4/spring/service/auth/DiscordOpaqueTokenIntrospector.java
@@ -1,5 +1,9 @@
 package ti4.spring.service.auth;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpRequest;
@@ -7,11 +11,6 @@ import java.net.http.HttpResponse;
 import java.util.Collections;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.github.benmanes.caffeine.cache.Cache;
-import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.core.DefaultOAuth2AuthenticatedPrincipal;
@@ -27,7 +26,7 @@ public class DiscordOpaqueTokenIntrospector implements OpaqueTokenIntrospector {
 
     private static final String ME_ENDPOINT = "https://discord.com/api/v10/users/@me";
     private static final Cache<String, OAuth2AuthenticatedPrincipal> AUTH_CACHE =
-        Caffeine.newBuilder().expireAfterWrite(30, TimeUnit.MINUTES).build();
+            Caffeine.newBuilder().expireAfterWrite(30, TimeUnit.MINUTES).build();
 
     @Override
     public OAuth2AuthenticatedPrincipal introspect(String token) {
@@ -52,34 +51,36 @@ public class DiscordOpaqueTokenIntrospector implements OpaqueTokenIntrospector {
         if (response.statusCode() == HttpStatus.OK.value()) {
             return handleSuccessfulAuthenticate(response, token);
         }
-        if (response.statusCode() == HttpStatus.UNAUTHORIZED.value() || response.statusCode() == HttpStatus.FORBIDDEN.value()) {
+        if (response.statusCode() == HttpStatus.UNAUTHORIZED.value()
+                || response.statusCode() == HttpStatus.FORBIDDEN.value()) {
             throw newAuthenticationFailureException();
         }
-        BotLogger.error(String.format("Received an unexpected status code from Discord during authentication: %s", response.body()));
+        BotLogger.error(String.format(
+                "Received an unexpected status code from Discord during authentication: %s", response.body()));
         throw newAuthenticationFailureException();
     }
 
     private HttpResponse<String> authenticateWithDiscord(String token) throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
-            .GET()
-            .uri(URI.create(ME_ENDPOINT))
-            .header("Accept", "application/json")
-            .header("Authorization", "Bearer " + token)
-            .build();
+                .GET()
+                .uri(URI.create(ME_ENDPOINT))
+                .header("Accept", "application/json")
+                .header("Authorization", "Bearer " + token)
+                .build();
 
         return EgressClientManager.getHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
     }
 
     private OAuth2AuthenticatedPrincipal handleSuccessfulAuthenticate(HttpResponse<String> response, String token)
-        throws JsonProcessingException {
+            throws JsonProcessingException {
         JsonNode node = EgressClientManager.getObjectMapper().readTree(response.body());
 
         OAuth2AuthenticatedPrincipal principal = new DefaultOAuth2AuthenticatedPrincipal(
-            node.get("id").asText(),
-            Map.of(
-                "id", node.get("id").asText(),
-                "username", node.get("username").asText()),
-            Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")));
+                node.get("id").asText(),
+                Map.of(
+                        "id", node.get("id").asText(),
+                        "username", node.get("username").asText()),
+                Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")));
 
         AUTH_CACHE.put(token, principal);
 
