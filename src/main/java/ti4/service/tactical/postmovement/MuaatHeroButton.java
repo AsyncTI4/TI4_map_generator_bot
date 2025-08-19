0afa1c4cfc70485f9af0b04825c2c8b1eed2fd98
James Strong <54325531+heisenbugged@users.noreply.github.com>
2025-08-19 16:56:25 -0500

    fixes

--- src/main/java/ti4/service/tactical/postmovement/MuaatHeroButton.java
+++ src/main/java/ti4/service/tactical/postmovement/MuaatHeroButton.java
@@ -3,18 +3,18 @@ package ti4.service.tactical.postmovement;
 import java.util.List;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import ti4.buttons.Buttons;
-import ti4.helpers.ButtonHelper;
 import ti4.helpers.Units.UnitType;
 import ti4.service.emoji.FactionEmojis;
 import ti4.service.tactical.PostMovementAbilityButton;
 import ti4.service.tactical.PostMovementButtonContext;
+import ti4.service.unit.CheckUnitContainmentService;
 
 public final class MuaatHeroButton implements PostMovementAbilityButton {
     public boolean enabled(PostMovementButtonContext ctx) {
         return ctx.player.hasLeaderUnlocked("muaathero")
                 && !ctx.tile.isMecatol()
                 && !ctx.tile.isHomeSystem(ctx.game)
-                && ButtonHelper.getTilesOfPlayersSpecificUnits(ctx.game, ctx.player, UnitType.Warsun)
+                && CheckUnitContainmentService.getTilesContainingPlayersUnits(ctx.game, ctx.player, UnitType.Warsun)
                         .contains(ctx.tile);
     }
 
