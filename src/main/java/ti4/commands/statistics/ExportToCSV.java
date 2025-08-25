e94c8a7f1d3285cedbabd18dd65142f56e732775
Will Holton <willholton1@gmail.com>
2025-08-25 18:38:36 -0400

    Centralize winning faction filter configuration (#3574)
    * Centralize winning faction filter
    
    * Update ExportToCSV.java

--- src/main/java/ti4/commands/statistics/ExportToCSV.java
+++ src/main/java/ti4/commands/statistics/ExportToCSV.java
@@ -1,8 +1,6 @@
 package ti4.commands.statistics;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.interactions.commands.OptionType;
-import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import ti4.commands.Subcommand;
 import ti4.service.statistics.ExportToCsvService;
 
@@ -11,11 +9,6 @@ class ExportToCSV extends Subcommand {
     ExportToCSV() {
         super("export_games_to_csv", "Export game data to a CSV file");
         addOptions(GameStatisticsFilterer.gameStatsFilters());
-        addOptions(new OptionData(
-                        OptionType.STRING,
-                        GameStatisticsFilterer.WINNING_FACTION_FILTER,
-                        "Filter games by if the game was won by said faction")
-                .setAutoComplete(true));
     }
 
     @Override
