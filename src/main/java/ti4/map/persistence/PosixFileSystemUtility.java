bfe851c8b11c3183f7e2cd49019125a94f65edf8
Will <willholton1@gmail.com>
2025-08-28 18:23:12 -0400

    Update PosixFileSystemUtility.java

--- src/main/java/ti4/map/persistence/PosixFileSystemUtility.java
+++ src/main/java/ti4/map/persistence/PosixFileSystemUtility.java
@@ -13,16 +13,18 @@ import ti4.message.logging.BotLogger;
 @UtilityClass
 class PosixFileSystemUtility {
 
-  private static final Set<PosixFilePermission> FILE_PERMISSIONS = PosixFilePermissions.fromString("rw-rw-r--"); // 0664
-  private static final boolean isPosix = FileSystems.getDefault().supportedFileAttributeViews().contains("posix");
+    private static final Set<PosixFilePermission> FILE_PERMISSIONS =
+            PosixFilePermissions.fromString("rw-rw-r--"); // 0664
+    private static final boolean isPosix =
+            FileSystems.getDefault().supportedFileAttributeViews().contains("posix");
 
-  static void setPermissionsIfPosix(Path path) {
-    try {
-      if (isPosix) {
-        Files.setPosixFilePermissions(path, FILE_PERMISSIONS);
-      }
-    } catch (IOException e) {
-      BotLogger.error("Failed to set Posix permissions", e);
+    static void setPermissionsIfPosix(Path path) {
+        try {
+            if (isPosix) {
+                Files.setPosixFilePermissions(path, FILE_PERMISSIONS);
+            }
+        } catch (IOException e) {
+            BotLogger.error("Failed to set Posix permissions", e);
+        }
     }
-  }
 }
