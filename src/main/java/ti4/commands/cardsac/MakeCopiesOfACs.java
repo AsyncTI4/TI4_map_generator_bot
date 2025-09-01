a46430874ba0545ee0fd35fb42ae40c9d36203e8
Fingon00 <britainfan234@gmail.com>
2025-09-01 15:21:46 -0400

    Fix

--- src/main/java/ti4/commands/cardsac/MakeCopiesOfACs.java
+++ src/main/java/ti4/commands/cardsac/MakeCopiesOfACs.java
@@ -1,17 +1,12 @@
 package ti4.commands.cardsac;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import ti4.commands.GameStateSubcommand;
 import ti4.helpers.Constants;
-import ti4.image.Mapper;
 import ti4.map.Game;
 import ti4.message.MessageHelper;
-import ti4.model.ActionCardModel;
 
 class MakeCopiesOfACs extends GameStateSubcommand {
 
@@ -31,9 +26,7 @@ class MakeCopiesOfACs extends GameStateSubcommand {
 
         Game game = getGame();
         if (count == 2) {
-            Map<String, ActionCardModel> actionCards = Mapper.getActionCards("extra1");
-            List<String> ACs = new ArrayList<>(actionCards.keySet());
-            game.addActionCardDuplicates(ACs);
+            game.duplicateACs();
         }
         if (count == 3) {
             game.triplicateExplores();
