eb81866da07c84a0606220d3d409a4c33c52d484
Fingon00 <britainfan234@gmail.com>
2025-09-04 23:21:37 -0400

    Update SwapFactionService.java

--- src/main/java/ti4/service/game/SwapFactionService.java
+++ src/main/java/ti4/service/game/SwapFactionService.java
@@ -3,6 +3,7 @@ package ti4.service.game;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+
 import lombok.experimental.UtilityClass;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.GenericInteractionCreateEvent;
@@ -15,7 +16,7 @@ public class SwapFactionService {
 
     public static void secondHalfOfSwap(
             Game game,
-            Player swapperPlayer,
+            Player addedPlayer,
             Player removedPlayer,
             User addedUser,
             GenericInteractionCreateEvent event) {
@@ -29,13 +30,13 @@ public class SwapFactionService {
         for (Map.Entry<String, List<String>> poEntry : scoredPublicObjectives.entrySet()) {
             List<String> value = poEntry.getValue();
             boolean removed = value.remove(removedPlayer.getUserID());
-            boolean removed2 = value.remove(swapperPlayer.getUserID());
-            boolean removed4 = value.remove(swapperPlayer.getUserID());
+            boolean removed2 = value.remove(addedPlayer.getUserID());
+            boolean removed4 = value.remove(addedPlayer.getUserID());
             if (removed) {
                 boolean removed3 = value.remove(removedPlayer.getUserID());
-                value.add(swapperPlayer.getUserID());
+                value.add(addedPlayer.getUserID());
                 if (removed3) {
-                    value.add(swapperPlayer.getUserID());
+                    value.add(addedPlayer.getUserID());
                 }
             }
             if (removed2) {
@@ -49,38 +50,40 @@ public class SwapFactionService {
         String oldActive = game.getActivePlayerID();
         String oldSpeaker = game.getSpeakerUserID();
 
-        if (swapperPlayer.getUserID().equalsIgnoreCase(oldActive)) {
+        if (addedPlayer.getUserID().equalsIgnoreCase(oldActive)) {
             game.setActivePlayerID(removedPlayer.getUserID());
         }
         if (removedPlayer.getUserID().equalsIgnoreCase(oldActive)) {
-            game.setActivePlayerID(swapperPlayer.getUserID());
+            game.setActivePlayerID(addedPlayer.getUserID());
         }
 
-        if (swapperPlayer.getUserID().equalsIgnoreCase(oldSpeaker)) {
+        if (addedPlayer.getUserID().equalsIgnoreCase(oldSpeaker)) {
             game.setSpeakerUserID(removedPlayer.getUserID());
         }
         if (removedPlayer.getUserID().equalsIgnoreCase(oldSpeaker)) {
-            game.setSpeakerUserID(swapperPlayer.getUserID());
+            game.setSpeakerUserID(addedPlayer.getUserID());
         }
 
         if (player.isDummy()) {
             player.setDummy(false);
-            swapperPlayer.setDummy(true);
+            addedPlayer.setDummy(true);
         }
 
         if (game.isFowMode()) {
             // Fog data is saved by userID so need to swap it too
-            game.getTileMap().values().forEach(tile -> tile.swapFogData(player, swapperPlayer));
+            game.getTileMap().values().forEach(tile -> tile.swapFogData(player, addedPlayer));
         }
 
         String before =
-                "> **Before:** " + swapperPlayer.getRepresentation() + " & " + removedPlayer.getRepresentation() + "\n";
-        swapperPlayer.setUserName(removedPlayer.getUserName());
-        swapperPlayer.setUserID(removedPlayer.getUserID());
+                "> **Before:** " + addedPlayer.getRepresentation() + " & " + player.getRepresentation() + "\n";
+        addedPlayer.setUserName(removedPlayer.getUserName());
+        addedPlayer.setUserID(removedPlayer.getUserID());
+        addedPlayer.removeTeamMateID(removedPlayer.getUserID());
         player.setUserName(addedUser.getName());
         player.setUserID(addedUser.getId());
+        player.removeTeamMateID(addedUser.getId());
         String after =
-                "> **After:** " + swapperPlayer.getRepresentation() + " & " + removedPlayer.getRepresentation() + "\n";
+                "> **After:** " + addedPlayer.getRepresentation() + " & " + player.getRepresentation() + "\n";
 
         String message = "Users have swapped factions:\n" + before + after;
         MessageHelper.sendMessageToChannel(event.getMessageChannel(), message);
