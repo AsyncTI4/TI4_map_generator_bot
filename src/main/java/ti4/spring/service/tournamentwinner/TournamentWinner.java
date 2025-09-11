537cb00d6e03faff718cabd53d77b9a75cc79eb0
Will <willholton1@gmail.com>
2025-09-11 11:57:10 -0400

    Update TournamentWinner.java

--- src/main/java/ti4/spring/service/tournamentwinner/TournamentWinner.java
+++ src/main/java/ti4/spring/service/tournamentwinner/TournamentWinner.java
@@ -1,22 +1,30 @@
 package ti4.spring.service.tournamentwinner;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "tournament_winner")
 class TournamentWinner {
 
-    @Id
-    private String userId;
+    TournamentWinner(String userId, String userName, String tourneyName) {
+        this.userId = userId;
+        this.userName = userName;
+        this.tourneyName = tourneyName;
+    }
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userId;
     private String userName;
     private String tourneyName;
 }
