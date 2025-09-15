ef322d2d16492d89a2151be30895948f4b990432
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2025-09-15 16:41:12 -0400

    try botlog oneliner (#3701)
    & revert some stuff that shouldn't have been pushed

--- test.sh
+++
@@ -1,8 +0,0 @@
-DISCORD_WEBHOOK_URL=https://discord.com/api/webhooks/1417222855082643638/c_xYaZivcDbeG8pYC1UONDhOam3nKvMBdrQqnPCsWBdggG_6tbvhizny0WhzMZsRTcR5
-
-function botlog() {
-    timestamp=$(date "+%F %T.%3N")
-    curl -X POST -H 'Content-Type: application/json' -d "{\"content\":\"\`$timestamp\`$1\"}" $DISCORD_WEBHOOK_URL
-}
-
-botlog "This is a test message from the botlog function."
