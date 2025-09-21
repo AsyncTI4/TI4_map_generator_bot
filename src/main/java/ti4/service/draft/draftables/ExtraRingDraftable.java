2e06ea506a6b0c24b43adaecbb5a4abfd1c7bcf6
Clayton Willey <willeyshm@gmail.com>
2025-09-21 00:11:08 -0400

    ideas

---
+++ src/main/java/ti4/service/draft/draftables/ExtraRingDraftable.java
@@ -0,0 +1,8 @@
+package ti4.service.draft.draftables;
+
+// e.g. for a 3 ring map, generate 4-tile "slices" which go *behind* your home system.
+// Draft them to add to the board.
+
+public class ExtraRingDraftable {
+    
+}
