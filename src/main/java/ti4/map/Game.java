7a3a4008b1b8556e2eae99292826cef67cd459ea
Kala Maria Hirmo <kala.hirmo@gmail.com>
2025-10-03 20:24:17 +0300

    Fix null pointer

--- src/main/java/ti4/map/Game.java
+++ src/main/java/ti4/map/Game.java
@@ -4654,6 +4654,9 @@ public class Game extends GameProperties {
 
     public void setStrategyCardSet(String scSetID) {
         StrategyCardSetModel strategyCardModel = Mapper.getStrategyCardSets().get(scSetID);
+        if (strategyCardModel == null) {
+            throw new IllegalArgumentException("Strategy card set not found for ID: " + scSetID);
+        };
         setHomebrewSCMode(!"pok".equals(scSetID) && !"base_game".equals(scSetID));
 
         Map<Integer, Integer> oldTGs = getScTradeGoods();
