197c7241b0dcae070e92b8c001dc07b2f1b1f16d
Clayton Willey <willeyshm@gmail.com>
2025-10-06 23:21:48 -0400

    respect x89c4

--- src/main/java/ti4/helpers/ButtonHelper.java
+++ src/main/java/ti4/helpers/ButtonHelper.java
@@ -2385,9 +2385,15 @@ public class ButtonHelper {
                             || ("space".equalsIgnoreCase(type) && removedUnit.getIsGroundForce())) {
                         continue;
                     }
-                    count += removedUnit.getCombatDieCount()
+                    float combatValue = removedUnit.getCombatDieCount()
                             * ((11.0f - removedUnit.getCombatHitsOn()) / 10 + unrelententing)
                             * uh.getUnitCount(unit);
+
+                    if ("ground".equalsIgnoreCase(type) && player.hasTech("x89c4")) {
+                        combatValue *= 2.0f;
+                    }
+
+                    count += combatValue;
                 }
             }
         }
