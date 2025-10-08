27daf9b6df72dc3cd452c2018d667229f3e69ff3
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2025-10-08 11:31:23 -0400

    rename daneMode -> galacticEvent

--- src/main/java/ti4/service/option/TEOptionService.java
+++ src/main/java/ti4/service/option/TEOptionService.java
@@ -15,11 +15,11 @@ public class TEOptionService {
 
     @ButtonHandler("offerTEOptionButtons")
     public static void offerTEOptionButtons(Game game, MessageChannel channel) {
-        List<Button> daneLinkButtons = getDaneLeakModeButtons(game);
+        List<Button> galacticEventButtons = getGalacticEventButtons(game);
         MessageHelper.sendMessageToChannelWithButtonsAndNoUndo(
                 channel,
-                "Enable or Disable Galactic Events\nSee [here](https://twilight-imperium.fandom.com/wiki/Galactic_Events) for details",
-                daneLinkButtons);
+                "Enable or Disable Galactic Events\n-# See [here](https://twilight-imperium.fandom.com/wiki/Galactic_Events) for details",
+                galacticEventButtons);
 
         String msg =
                 "Thunder's Edge contains a new version of mecatol rex, which is legendary (it's ability allows you to discard and then draw a secret objective). If you want to play with this new version of mecatol rex in your game, press this button and it will be added to the map when secrets are dealt.";
@@ -49,62 +49,68 @@ public class TEOptionService {
         MessageHelper.sendMessageToChannelWithButtonsAndNoUndo(channel, msg, buttons);
     }
 
-    public static List<Button> getDaneLeakModeButtons(Game game) {
-        List<Button> daneLinkButtons = new ArrayList<>();
+    public static List<Button> getGalacticEventButtons(Game game) {
+        List<Button> galacticEventButtons = new ArrayList<>();
 
         if (game.isMinorFactionsMode()) {
-            daneLinkButtons.add(Buttons.red("enableDaneMode_minorFactions_disable", "Disable Minor Factions"));
+            galacticEventButtons.add(Buttons.red("enableDaneMode_minorFactions_disable", "Disable Minor Factions"));
         } else {
-            daneLinkButtons.add(Buttons.green("enableDaneMode_minorFactions_enable", "Enable Minor Factions"));
+            galacticEventButtons.add(Buttons.green("enableDaneMode_minorFactions_enable", "Enable Minor Factions"));
         }
         if (game.isHiddenAgendaMode()) {
-            daneLinkButtons.add(Buttons.red("enableDaneMode_hiddenAgenda_disable", "Disable Hidden Agenda"));
+            galacticEventButtons.add(Buttons.red("enableDaneMode_hiddenAgenda_disable", "Disable Hidden Agenda"));
         } else {
-            daneLinkButtons.add(Buttons.green("enableDaneMode_hiddenAgenda_enable", "Enable Hidden Agenda"));
+            galacticEventButtons.add(Buttons.green("enableDaneMode_hiddenAgenda_enable", "Enable Hidden Agenda"));
         }
         if (game.isAgeOfExplorationMode()) {
-            daneLinkButtons.add(Buttons.red("enableDaneMode_ageOfExploration_disable", "Disable Age of Exploration"));
+            galacticEventButtons.add(
+                    Buttons.red("enableDaneMode_ageOfExploration_disable", "Disable Age of Exploration"));
         } else {
-            daneLinkButtons.add(Buttons.green("enableDaneMode_ageOfExploration_enable", "Enable Age of Exploration"));
+            galacticEventButtons.add(
+                    Buttons.green("enableDaneMode_ageOfExploration_enable", "Enable Age of Exploration"));
         }
         if (game.isTotalWarMode()) {
-            daneLinkButtons.add(Buttons.red("enableDaneMode_totalWar_disable", "Disable Total War"));
+            galacticEventButtons.add(Buttons.red("enableDaneMode_totalWar_disable", "Disable Total War"));
         } else {
-            daneLinkButtons.add(Buttons.green("enableDaneMode_totalWar_enable", "Enable Total War"));
+            galacticEventButtons.add(Buttons.green("enableDaneMode_totalWar_enable", "Enable Total War"));
         }
         if (game.isDangerousWildsMode()) {
-            daneLinkButtons.add(Buttons.red("enableDaneMode_DangerousWilds_disable", "Disable Dangerous Wilds"));
+            galacticEventButtons.add(Buttons.red("enableDaneMode_DangerousWilds_disable", "Disable Dangerous Wilds"));
         } else {
-            daneLinkButtons.add(Buttons.green("enableDaneMode_DangerousWilds_enable", "Enable Dangerous Wilds"));
+            galacticEventButtons.add(Buttons.green("enableDaneMode_DangerousWilds_enable", "Enable Dangerous Wilds"));
         }
         if (game.isAgeOfFightersMode()) {
-            daneLinkButtons.add(Buttons.red("enableDaneMode_AgeOfFighters_disable", "Disable Age Of Fighters"));
+            galacticEventButtons.add(Buttons.red("enableDaneMode_AgeOfFighters_disable", "Disable Age Of Fighters"));
         } else {
-            daneLinkButtons.add(Buttons.green("enableDaneMode_AgeOfFighters_enable", "Enable Age Of Fighters"));
+            galacticEventButtons.add(Buttons.green("enableDaneMode_AgeOfFighters_enable", "Enable Age Of Fighters"));
         }
         if (game.isMercenariesForHireMode()) {
-            daneLinkButtons.add(
+            galacticEventButtons.add(
                     Buttons.red("enableDaneMode_MercenariesForHire_disable", "Disable Mercenaries For Hire"));
         } else {
-            daneLinkButtons.add(
+            galacticEventButtons.add(
                     Buttons.green("enableDaneMode_MercenariesForHire_enable", "Enable Mercenaries For Hire"));
         }
         if (game.isZealousOrthodoxyMode()) {
-            daneLinkButtons.add(Buttons.red("enableDaneMode_ZealousOrthodoxy_disable", "Disable Zealous Orthodoxy"));
+            galacticEventButtons.add(
+                    Buttons.red("enableDaneMode_ZealousOrthodoxy_disable", "Disable Zealous Orthodoxy"));
         } else {
-            daneLinkButtons.add(Buttons.green("enableDaneMode_ZealousOrthodoxy_enable", "Enable Zealous Orthodoxy"));
+            galacticEventButtons.add(
+                    Buttons.green("enableDaneMode_ZealousOrthodoxy_enable", "Enable Zealous Orthodoxy"));
         }
         if (game.isCulturalExchangeProgramMode()) {
-            daneLinkButtons.add(
+            galacticEventButtons.add(
                     Buttons.red("enableDaneMode_CulturalExchangeProgram_disable", "Disable Cultural Exchange Program"));
         } else {
-            daneLinkButtons.add(
+            galacticEventButtons.add(
                     Buttons.green("enableDaneMode_CulturalExchangeProgram_enable", "Enable Cultural Exchange Program"));
         }
         if (game.isRapidMobilizationMode()) {
-            daneLinkButtons.add(Buttons.red("enableDaneMode_RapidMobilization_disable", "Disable Rapid Mobilization"));
+            galacticEventButtons.add(
+                    Buttons.red("enableDaneMode_RapidMobilization_disable", "Disable Rapid Mobilization"));
         } else {
-            daneLinkButtons.add(Buttons.green("enableDaneMode_RapidMobilization_enable", "Enable Rapid Mobilization"));
+            galacticEventButtons.add(
+                    Buttons.green("enableDaneMode_RapidMobilization_enable", "Enable Rapid Mobilization"));
         }
         // if (game.isCosmicPhenomenaeMode()) {
         //     daneLinkButtons.add(Buttons.red("enableDaneMode_Cosmic_disable", "Disable Cosmic Phenomenae"));
@@ -117,9 +123,9 @@ public class TEOptionService {
         //     daneLinkButtons.add(Buttons.green("enableDaneMode_Monument_enable", "Enable Monuments to the Ages"));
         // }
         if (game.isWeirdWormholesMode()) {
-            daneLinkButtons.add(Buttons.red("enableDaneMode_WeirdWormholes_disable", "Disable Weird Wormholes"));
+            galacticEventButtons.add(Buttons.red("enableDaneMode_WeirdWormholes_disable", "Disable Weird Wormholes"));
         } else {
-            daneLinkButtons.add(Buttons.green("enableDaneMode_WeirdWormholes_enable", "Enable Weird Wormholes"));
+            galacticEventButtons.add(Buttons.green("enableDaneMode_WeirdWormholes_enable", "Enable Weird Wormholes"));
         }
         // if (game.isWildWildGalaxyMode()) {
         //     daneLinkButtons.add(Buttons.red("enableDaneMode_WildGalaxy_disable", "Disable Wild Wild Galaxy"));
@@ -127,37 +133,39 @@ public class TEOptionService {
         //     daneLinkButtons.add(Buttons.green("enableDaneMode_WildGalaxy_enable", "Enable Wild Wild Galaxy"));
         // }
         if (game.isConventionsOfWarAbandonedMode()) {
-            daneLinkButtons.add(
+            galacticEventButtons.add(
                     Buttons.red("enableDaneMode_Conventions_disable", "Disable Conventions of War Abandoned"));
         } else {
-            daneLinkButtons.add(
+            galacticEventButtons.add(
                     Buttons.green("enableDaneMode_Conventions_enable", "Enable Conventions of War Abandoned"));
         }
         if (game.isAdventOfTheWarsunMode()) {
-            daneLinkButtons.add(
+            galacticEventButtons.add(
                     Buttons.red("enableDaneMode_AdventOfTheWarsun_disable", "Disable Advent of the Warsun"));
         } else {
-            daneLinkButtons.add(
+            galacticEventButtons.add(
                     Buttons.green("enableDaneMode_AdventOfTheWarsun_enable", "Enable Advent of the Warsun"));
         }
         if (game.isCivilizedSocietyMode()) {
-            daneLinkButtons.add(Buttons.red("enableDaneMode_CivilizedSociety_disable", "Disable Civilized Society"));
+            galacticEventButtons.add(
+                    Buttons.red("enableDaneMode_CivilizedSociety_disable", "Disable Civilized Society"));
         } else {
-            daneLinkButtons.add(Buttons.green("enableDaneMode_CivilizedSociety_enable", "Enable Civilized Society"));
+            galacticEventButtons.add(
+                    Buttons.green("enableDaneMode_CivilizedSociety_enable", "Enable Civilized Society"));
         }
         if (game.isStellarAtomicsMode()) {
-            daneLinkButtons.add(Buttons.red("enableDaneMode_StellarAtomics_disable", "Disable Stellar Atomics"));
+            galacticEventButtons.add(Buttons.red("enableDaneMode_StellarAtomics_disable", "Disable Stellar Atomics"));
         } else {
-            daneLinkButtons.add(Buttons.green("enableDaneMode_StellarAtomics_enable", "Enable Stellar Atomics"));
+            galacticEventButtons.add(Buttons.green("enableDaneMode_StellarAtomics_enable", "Enable Stellar Atomics"));
         }
         if (game.isAgeOfCommerceMode()) {
-            daneLinkButtons.add(Buttons.red("enableDaneMode_ageOfCommerce_disable", "Disable Age of Commerce"));
+            galacticEventButtons.add(Buttons.red("enableDaneMode_ageOfCommerce_disable", "Disable Age of Commerce"));
         } else {
-            daneLinkButtons.add(Buttons.green("enableDaneMode_ageOfCommerce_enable", "Enable Age of Commerce"));
+            galacticEventButtons.add(Buttons.green("enableDaneMode_ageOfCommerce_enable", "Enable Age of Commerce"));
         }
 
-        daneLinkButtons.add(Buttons.gray("deleteButtons", "Done"));
+        galacticEventButtons.add(Buttons.gray("deleteButtons", "Done"));
 
-        return daneLinkButtons;
+        return galacticEventButtons;
     }
 }
