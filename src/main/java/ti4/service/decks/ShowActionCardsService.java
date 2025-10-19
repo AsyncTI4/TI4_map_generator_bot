27849e4640598679f6d54bebbdcafffcb4721a5d
Chase Hinesman <chasehinesman@gmail.com>
2025-10-19 13:29:11 -0500

    Add Garbozia and Data Skimmer back-end handling

--- src/main/java/ti4/service/decks/ShowActionCardsService.java
+++ src/main/java/ti4/service/decks/ShowActionCardsService.java
@@ -5,16 +5,20 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import lombok.experimental.UtilityClass;
 import net.dv8tion.jda.api.components.buttons.Button;
 import net.dv8tion.jda.api.events.interaction.GenericInteractionCreateEvent;
 import ti4.buttons.Buttons;
 import ti4.helpers.ActionCardHelper;
+import ti4.helpers.ActionCardHelper.ACStatus;
 import ti4.helpers.Helper;
 import ti4.image.Mapper;
 import ti4.map.Game;
 import ti4.message.MessageHelper;
 import ti4.service.emoji.CardEmojis;
+import ti4.service.emoji.FactionEmojis;
+import ti4.service.emoji.MiscEmojis;
 
 @UtilityClass
 public class ShowActionCardsService {
@@ -28,17 +32,47 @@ public class ShowActionCardsService {
 
     public static void showDiscard(Game game, GenericInteractionCreateEvent event, boolean showFullText) {
         StringBuilder sb = new StringBuilder();
-        List<Map.Entry<String, Integer>> discards =
-                game.getDiscardActionCards().entrySet().stream().toList();
+        List<Entry<String, Integer>> discards = game.getDiscardActionCards().entrySet().stream()
+                .filter(x -> game.getDiscardACStatus().get(x.getKey()) == null)
+                .toList();
+        List<Entry<String, Integer>> purged = game.getDiscardActionCards().entrySet().stream()
+                .filter(x -> ACStatus.purged.equals(game.getDiscardACStatus().get(x.getKey())))
+                .toList();
+        List<Entry<String, Integer>> dataSkimmer = game.getDiscardActionCards().entrySet().stream()
+                .filter(x -> ACStatus.ralnelbt.equals(game.getDiscardACStatus().get(x.getKey())))
+                .toList();
+        List<Entry<String, Integer>> garbozia = game.getDiscardActionCards().entrySet().stream()
+                .filter(x -> ACStatus.garbozia.equals(game.getDiscardACStatus().get(x.getKey())))
+                .toList();
 
-        Button showFullTextButton = null;
-        if (showFullText) {
-            sb.append(actionCardListFullText(discards, "Action card discard list", game));
-        } else {
-            sb.append(discardListCondensed(discards, "Action card discard list"));
-            showFullTextButton = Buttons.green("ACShowDiscardFullText", "Show Full Text");
+        if (!purged.isEmpty()) {
+            String title = "PURGED Action cards";
+            sb.append(acDiscardText(showFullText, purged, title, game));
+            sb.append("\n\n");
         }
+        {
+            String title = "Action card discard list";
+            sb.append(acDiscardText(showFullText, discards, title, game));
+        }
+        if (!dataSkimmer.isEmpty()) {
+            sb.append("\n\n");
+            String title = "Action cards on " + FactionEmojis.Ralnel + " Data Skimmer";
+            sb.append(acDiscardText(showFullText, dataSkimmer, title, game));
+        }
+        if (!garbozia.isEmpty()) {
+            sb.append("\n\n");
+            String title = "Action cards on " + MiscEmojis.LegendaryPlanet + " Garbozia";
+            sb.append(acDiscardText(showFullText, garbozia, title, game));
+        }
+
+        Button showFullTextButton = showFullText ? Buttons.green("ACShowDiscardFullText", "Show Full Text") : null;
         MessageHelper.sendMessageToChannelWithButton(event.getMessageChannel(), sb.toString(), showFullTextButton);
+    }
+
+    public static String acDiscardText(
+            boolean fullText, List<Entry<String, Integer>> discards, String title, Game game) {
+        if (fullText) return actionCardListFullText(discards, title, game);
+        return discardListCondensed(discards, title);
     }
 
     private static String actionCardListFullText(List<Map.Entry<String, Integer>> discards, String title, Game game) {
