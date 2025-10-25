e5288ca7e6b632767ea1115c30b826fee56efd8c
Clayton Willey <willeyshm@gmail.com>
2025-10-25 17:09:00 -0400

    mulligan option, cleanup context init

--- src/main/java/ti4/service/draft/MantisMapBuildContext.java
+++ src/main/java/ti4/service/draft/MantisMapBuildContext.java
@@ -1,8 +1,6 @@
 package ti4.service.draft;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.Optional;
@@ -13,6 +11,7 @@ import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+
 import net.dv8tion.jda.api.events.interaction.GenericInteractionCreateEvent;
 import ti4.draft.DraftItem;
 import ti4.draft.DraftItem.Category;
@@ -23,7 +22,6 @@ import ti4.message.MessageHelper;
 import ti4.model.MapTemplateModel;
 import ti4.service.draft.draftables.MantisTileDraftable;
 import ti4.service.draft.draftables.SpeakerOrderDraftable;
-import ti4.service.franken.FrankenDraftBagService;
 
 public record MantisMapBuildContext(
         @Nonnull Game game,
@@ -181,111 +179,6 @@ public record MantisMapBuildContext(
             // RedTileDraftItems
             result.add(PlayerTiles.create(playerId, mantisDraftable, mantisPicks));
         }
-        return result;
-    }
-
-    public static MantisMapBuildContext fromFranken(@Nonnull Game game) {
-        String mapTemplateId = game.getMapTemplateID();
-        if (mapTemplateId == null) {
-            throw new IllegalStateException("Game does not have a map template ID set.");
-        }
-        MapTemplateModel mapTemplateModel = Mapper.getMapTemplate(mapTemplateId);
-        if (mapTemplateModel == null) {
-            throw new IllegalStateException("Could not find map template model for ID: " + mapTemplateId);
-        }
-        return new MantisMapBuildContext(
-                game,
-                mapTemplateModel,
-                1,
-                (String buttonAction) -> FrankenDraftBagService.ACTION_NAME + buttonAction,
-                game.getStoredValue("frankenMapBuildTileId"),
-                null,
-                tileId -> game.setStoredValue("frankenMapBuildTileId", tileId),
-                tileId -> {
-                    String mulligans = game.getStoredValue("frankenMapBuildMulligans");
-                    if (mulligans == null || mulligans.isEmpty()) {
-                        mulligans = tileId;
-                    } else {
-                        mulligans += "," + tileId;
-                    }
-                    game.setStoredValue("frankenMapBuildMulligans", mulligans);
-                },
-                tileId -> {
-                    String discards = game.getStoredValue("frankenMapBuildDiscards");
-                    if (discards == null || discards.isEmpty()) {
-                        discards = tileId;
-                    } else {
-                        discards += "," + tileId;
-                    }
-                    game.setStoredValue("frankenMapBuildDiscards", discards);
-                },
-                playerNum -> {
-                    for (Player player : game.getRealPlayers()) {
-                        if (player.getDraftHand() == null) {
-                            continue;
-                        }
-                        boolean hasSpeakerOrder = player.getDraftHand().Contents.stream()
-                                .filter(item -> item.ItemCategory == Category.DRAFTORDER)
-                                .filter(item -> item.ItemId.equals("" + playerNum))
-                                .findFirst()
-                                .isPresent();
-                        if (hasSpeakerOrder) {
-                            return Optional.of(player);
-                        }
-                    }
-                    return Optional.empty();
-                },
-                getPlayerTileStateFranken(game),
-                () -> getPlayerTileStateFranken(game),
-                event -> {
-                    game.removeStoredValue("frankenMapBuildTileId");
-                    game.removeStoredValue("frankenMapBuildMulligans");
-                    game.removeStoredValue("frankenMapBuildDiscards");
-                });
-    }
-
-    private static List<PlayerTiles> getPlayerTileStateFranken(Game game) {
-        List<PlayerTiles> result = new ArrayList<>();
-        Set<String> placedTiles =
-                game.getTileMap().values().stream().map(t -> t.getTileID()).collect(Collectors.toSet());
-        String discardedTilesStr = game.getStoredValue("frankenMapBuildDiscards");
-        Set<String> discardedTiles = new HashSet<>();
-        if (discardedTilesStr != null && !discardedTilesStr.isEmpty()) {
-            discardedTiles.addAll(Arrays.asList(discardedTilesStr.split(",")));
-        }
-        String mulliganedTilesStr = game.getStoredValue("frankenMapBuildMulligans");
-        List<String> mulliganedTiles = new ArrayList<>();
-        if (mulliganedTilesStr != null && !mulliganedTilesStr.isEmpty()) {
-            mulliganedTiles.addAll(Arrays.asList(mulliganedTilesStr.split(",")));
-        }
-        for (Player player : game.getRealPlayers()) {
-            if (player.getDraftHand() == null) {
-                continue;
-            }
-            List<DraftItem> tileItems = player.getDraftHand().Contents.stream()
-                    .filter(item -> item.ItemCategory == Category.BLUETILE || item.ItemCategory == Category.REDTILE)
-                    .collect(Collectors.toList());
-
-            // Count how many of the mulliganed tiles are in this bag (one tile can be mulliganed more than once)
-            int mulligansUsed = 0;
-            for (String mulliganedTileId : mulliganedTiles) {
-                boolean isInBag = tileItems.stream()
-                        .filter(item -> item.ItemId.equals(mulliganedTileId))
-                        .findFirst()
-                        .isPresent();
-                if (isInBag) {
-                    mulligansUsed++;
-                }
-            }
-
-            // Remove tiles that are placed on the game board
-            tileItems.removeIf(tileItem -> placedTiles.contains(tileItem.ItemId));
-            // Remove tiles that the player discarded
-            tileItems.removeIf(tileItem -> discardedTiles.contains(tileItem.ItemId));
-
-            result.add(PlayerTiles.create(player.getUserID(), tileItems, mulligansUsed));
-        }
-
         return result;
     }
 
