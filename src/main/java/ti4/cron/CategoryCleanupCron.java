021ef78197c768f180bb94029ff7c24949e20ac5
James Rudolph <39609802+jamesrudolph@users.noreply.github.com>
2025-10-30 23:01:14 -0400

    add cron to cleanup empty game categories

---
+++ src/main/java/ti4/cron/CategoryCleanupCron.java
@@ -0,0 +1,25 @@
+package ti4.cron;
+
+import java.util.concurrent.TimeUnit;
+import lombok.experimental.UtilityClass;
+import ti4.message.logging.BotLogger;
+import ti4.spring.jda.JdaService;
+
+@UtilityClass
+public class CategoryCleanupCron {
+
+    public static void register() {
+        CronManager.schedulePeriodically(
+                CategoryCleanupCron.class, CategoryCleanupCron::cleanupCategories, 1, 30, TimeUnit.MINUTES);
+    }
+
+    private static void cleanupCategories() {
+        JdaService.guilds.forEach(guild -> guild.getCategories().stream()
+                .filter(category -> category.getName().startsWith("PBD #"))
+                .filter(category -> category.getChannels().isEmpty())
+                .forEach(category -> {
+                    BotLogger.info("**CategoryCleanupCron** Deleted empty category: " + category.getName() + " on guild: " + guild.getName());
+                    category.delete().queue();
+                }));
+    }
+}
