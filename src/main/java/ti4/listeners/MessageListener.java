ebb84445db379ac8d2c5cca811ab446789cc5c65
Will <willholton1@gmail.com>
2025-10-31 21:41:46 -0400

    Update MessageListener.java

--- src/main/java/ti4/listeners/MessageListener.java
+++ src/main/java/ti4/listeners/MessageListener.java
@@ -38,7 +38,8 @@ public class MessageListener extends ListenerAdapter {
     private static final Pattern PATTERN = Pattern.compile("[^a-zA-Z0-9]+$");
     // The mention itself is 23 characters long
     private static final int BOTHELPER_MENTION_REMINDER_MESSAGE_LENGTH_THRESHOLD = 53;
-    private static final String BOTHELPER_MENTION_REMINDER_TEXT = """
+    private static final String BOTHELPER_MENTION_REMINDER_TEXT =
+            """
         Friendly reminder in case you forgot, please include the specific reason for the ping (e.g. something is not working,
         there is a bug, or you're not sure how to do something) and any other relevant information. This will speed up the process by
         allowing the staff to know what you are pinging. Thanks!
