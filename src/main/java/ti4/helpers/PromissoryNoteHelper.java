e4d78a30cafb933088eb9b5ad459e5b2c4acc8d6
Fingon00 <britainfan234@gmail.com>
2025-11-01 08:45:51 -0400

    Fix

--- src/main/java/ti4/helpers/PromissoryNoteHelper.java
+++ src/main/java/ti4/helpers/PromissoryNoteHelper.java
@@ -342,7 +342,8 @@ public class PromissoryNoteHelper {
             List<Button> buttons = ButtonHelper.getGainCCButtons(player);
             String message2 = trueIdentity + ", your current command tokens are " + player.getCCRepresentation()
                     + ". Use buttons to gain 2 command tokens.";
-            MessageHelper.sendMessageToChannelWithButtons((MessageChannel) event.getChannel(), message2, buttons);
+            MessageHelper.sendMessageToChannelWithButtons(
+                    (MessageChannel) player.getCorrectChannel(), message2, buttons);
             game.setStoredValue("originalCCsFor" + player.getFaction(), player.getCCRepresentation());
             ActionCardHelper.sendPlotCardInfo(game, owner);
             MessageHelper.sendMessageToChannel(
