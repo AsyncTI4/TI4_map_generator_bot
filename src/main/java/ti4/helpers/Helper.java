ce2928f8c809b4b1ef5f68ffdc9928323a03ff45
Chase Hinesman <chasehinesman@gmail.com>
2025-11-02 09:05:19 -0600

    Automate DWS share knowledge return
    - Give a message for no sabo / Nip and Tuck

--- src/main/java/ti4/helpers/Helper.java
+++ src/main/java/ti4/helpers/Helper.java
@@ -19,7 +19,12 @@ import java.util.StringTokenizer;
 import java.util.concurrent.TimeUnit;
 import java.util.function.ToIntFunction;
 import java.util.stream.Collectors;
+
 import javax.annotation.Nullable;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NotNull;
+
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.components.buttons.Button;
 import net.dv8tion.jda.api.components.buttons.ButtonStyle;
@@ -38,8 +43,6 @@ import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.interaction.GenericInteractionCreateEvent;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.managers.channel.concrete.TextChannelManager;
-import org.apache.commons.lang3.StringUtils;
-import org.jetbrains.annotations.NotNull;
 import ti4.ResourceHelper;
 import ti4.buttons.Buttons;
 import ti4.helpers.Units.UnitKey;
@@ -144,6 +147,10 @@ public class Helper {
     public static String noSaboReason(Game game, Player player) {
         if (checkForAllSabotagesDiscarded(game) || checkAcd2ForAllSabotagesDiscarded(game)) {
             return "All _Sabotages_ are in the discard.";
+        }
+        if (game.playerHasLeaderUnlockedOrAlliance(player, "bastioncommander")) {
+            LeaderModel nipAndTuck = Mapper.getLeader("bastioncommander");
+            return "Player has access to the Last Bastion commander, " + nipAndTuck.getNameRepresentation();
         }
         if (player.hasTech("tp")
                 && game.getActivePlayerID() != null
--- src/main/java/ti4/service/StatusCleanupService.java
+++ src/main/java/ti4/service/StatusCleanupService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
+
 import lombok.experimental.UtilityClass;
 import net.dv8tion.jda.api.components.buttons.Button;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
@@ -21,6 +22,7 @@ import ti4.map.Tile;
 import ti4.map.UnitHolder;
 import ti4.message.MessageHelper;
 import ti4.model.PromissoryNoteModel;
+import ti4.model.TechnologyModel;
 import ti4.service.info.ListPlayerInfoService;
 import ti4.service.player.RefreshCardsService;
 
@@ -90,6 +92,26 @@ public class StatusCleanupService {
             game.removeStoredValue("passOnAllWhensNAfters" + player.getFaction());
             game.removeStoredValue(player.getFaction() + "scpickqueue");
 
+            String shareKnowledgeConst = "ShareKnowledge_" + player.getFaction();
+            String sharedKnowledge = game.getStoredValue(shareKnowledgeConst);
+            if (player.isRealPlayer() && sharedKnowledge != null && !sharedKnowledge.equals("")) {
+                game.removeStoredValue(shareKnowledgeConst);
+                player.removeTech(sharedKnowledge);
+                TechnologyModel tech = Mapper.getTech(sharedKnowledge);
+                String msg = player.getRepresentation() + " technology " + tech.getRepresentation(false) + " has been removed, and Share Knowledge has been returned to the owner.";
+                MessageHelper.sendMessageToChannel(game.getActionsChannel(), msg);
+
+                player.removePromissoryNote("shareknowledge");
+                for (Player p2 : game.getRealPlayers()) {
+                    if (p2.ownsPromissoryNote("shareknowledge")) {
+                        p2.setPromissoryNote("shareknowledge");
+                        PromissoryNoteHelper.sendPromissoryNoteInfo(game, p2, false);
+                        break;
+                    }
+                }
+                PromissoryNoteHelper.sendPromissoryNoteInfo(game, player, false);
+            }
+
             if (player.isRealPlayer()
                     && game.getStoredValue("Pre Pass " + player.getFaction()) != null
                     && game.getStoredValue("Pre Pass " + player.getFaction()).contains(player.getFaction())) {
@@ -129,6 +151,7 @@ public class StatusCleanupService {
         game.removeStoredValue("allianceModeSimultaneousAction");
         game.removeStoredValue("Coup");
         game.removeStoredValue("PublicExecution");
+        game.removeStoredValue("VisionariaResponded");
         game.setHasHadAStatusPhase(true);
         if (game.getSpinMode() != null && !"OFF".equalsIgnoreCase(game.getSpinMode())) {
             if ("ON".equalsIgnoreCase(game.getSpinMode())) {
