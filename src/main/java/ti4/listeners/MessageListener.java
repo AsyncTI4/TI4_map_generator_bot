82cc740432625a41190708953430581215d06d37
Ben T <teuschb@users.noreply.github.com>
2025-11-06 14:54:15 -0700

    Blt typo (#3929)
    * Fix typo in salvage AC  resolution text
    
    * Update acd2.json
    
    fixed Virulent Payload to Action phase; it was listed as agenda phase and throwing erros
    
    * Update MessageListener.java
    
    I hate how this message has random line breaks in it
    
    ---------
    
    Co-authored-by: Will Holton <willholton1@gmail.com>

--- src/main/java/ti4/listeners/MessageListener.java
+++ src/main/java/ti4/listeners/MessageListener.java
@@ -40,10 +40,8 @@ public class MessageListener extends ListenerAdapter {
     private static final int BOTHELPER_MENTION_REMINDER_MESSAGE_LENGTH_THRESHOLD = 53;
     private static final String BOTHELPER_MENTION_REMINDER_TEXT =
             """
-        Friendly reminder in case you forgot, please include the specific reason for the ping (e.g. something is not working,
-        there is a bug, or you're not sure how to do something) and any other relevant information. This will speed up the process by
-        allowing the staff to know how they can help. Thanks!
-        """;
+        Friendly reminder in case you forgot, please include the specific reason for the ping (e.g. something is not working, there is a bug, or you're not sure how to do something) and any other relevant information. This will speed up the process by allowing the staff to know how they can help. Thanks!
+            """;
 
     @Override
     public void onMessageReceived(@Nonnull MessageReceivedEvent event) {
