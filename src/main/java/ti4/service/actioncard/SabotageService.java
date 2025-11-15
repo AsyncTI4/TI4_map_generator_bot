58669ad4f827e3cc416a6d5b6e6d79bf53a2965c
Will Holton <willholton1@gmail.com>
2025-11-15 06:14:56 -0500

    Acd2 te updates123 (#3943)
    * Revise and add action cards in acd2 and legacy_data
    
    Updated several action cards in acd2.json with revised text, flavor text, and rules for clarity and balance. Moved 'Counterintelligence' to legacy_data.json and introduced 'Silence' as a new card in acd2.json. Various minor corrections and consistency improvements were made to card descriptions and flavor text.
    
    * f
    
    * f
    
    * Update acd2.json
    
    * Update acd2.json
    
    * Update legacy_data.json
    
    * Update acd2.json
    
    * Update action cards: add Cache, Rogues; deprecate Strategic Aid
    
    Added the 'Cache' and 'Rogues' action cards to the main deck and updated their definitions. Deprecated 'Strategic Aid' and 'cache_te', moving them to legacy data. Updated deck lists to reflect these changes and ensure correct card availability.
    
    * Update acd2.json
    
    * Update acd2.json
    
    * Update acd2.json
    
    * Update acd2.json
    
    * Update acd2.json
    
    * Update SabotageService.java
    
    * Update acd2.json
    
    * Update acd2.json

--- src/main/java/ti4/service/actioncard/SabotageService.java
+++ src/main/java/ti4/service/actioncard/SabotageService.java
@@ -1,12 +1,10 @@
 package ti4.service.actioncard;
 
 import java.util.Calendar;
-import java.util.EnumSet;
 import java.util.Set;
 import lombok.experimental.UtilityClass;
 import net.dv8tion.jda.api.entities.MessageReaction;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
-import ti4.helpers.ActionCardHelper.ACStatus;
 import ti4.helpers.Helper;
 import ti4.helpers.Units;
 import ti4.image.Mapper;
@@ -40,9 +38,6 @@ public class SabotageService {
             "sabotage4_acd2",
             "tf-shatter1",
             "tf-shatter2");
-
-    private static final Set<ACStatus> AC_STATUS_TO_IGNORE =
-            EnumSet.of(ACStatus.garbozia, ACStatus.ralnelbt, ACStatus.purged);
 
     public static boolean couldFeasiblySabotage(Player player, Game game) {
         if (player.isNpc()) {
@@ -184,7 +179,7 @@ public class SabotageService {
     }
 
     private static boolean checkForAllShattersDiscarded(Game game) {
-        return game.getDiscardActionCards().keySet().containsAll(SHATTER_CARD_ALIASES);
+        return SHATTER_CARD_ALIASES.stream().allMatch(alias -> isActionCardNotInDeckOrHand(game, alias));
     }
 
     private static boolean checkForAllSabotagesDiscarded(Game game) {
