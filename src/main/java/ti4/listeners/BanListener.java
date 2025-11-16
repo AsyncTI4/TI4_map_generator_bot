5cb8e94c7026fcccad7f0ae4b41791241ec547f8
Chase Hinesman <chasehinesman@gmail.com>
2025-11-16 08:58:15 -0600

    Fix eronous new tokens

--- src/main/java/ti4/listeners/BanListener.java
+++ src/main/java/ti4/listeners/BanListener.java
@@ -7,6 +7,7 @@ import net.dv8tion.jda.api.audit.TargetType;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.guild.GuildAuditLogEntryCreateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import ti4.message.logging.BotLogger;
 import ti4.service.async.BanCleanupService;
 import ti4.spring.jda.JdaService;
 
@@ -14,22 +15,27 @@ public class BanListener extends ListenerAdapter {
 
     @Override
     public void onGuildAuditLogEntryCreate(@Nonnull GuildAuditLogEntryCreateEvent event) {
+        try {
         AuditLogEntry log = event.getEntry();
         if (log.getType() == ActionType.BAN) {
             BanCleanupService.banSpamAccount(log, getTargetUser(log), getInitiatingUser(log));
+        }
+        } catch (Exception e) {
+            BotLogger.error("Error attepting to propagate ban", e);
         }
     }
 
     private User getTargetUser(AuditLogEntry log) {
         if (log.getTargetType() == TargetType.MEMBER) {
-            log.getTargetId();
-            return JdaService.jda.getUserById(log.getTargetId());
+            if (log.getTargetId() != null)
+                return JdaService.jda.getUserById(log.getTargetId());
         }
         return null;
     }
 
     private User getInitiatingUser(AuditLogEntry log) {
-        log.getUserId();
-        return JdaService.jda.getUserById(log.getUserId());
+        if (log.getUserId() != null)
+            return JdaService.jda.getUserById(log.getUserId());
+        return null;
     }
 }
