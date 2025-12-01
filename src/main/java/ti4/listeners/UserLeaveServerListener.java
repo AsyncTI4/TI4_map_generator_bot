26be2bed6c5dc18084df19ed1c8862c0dae411e2
Chase Hinesman <chasehinesman@gmail.com>
2025-12-01 12:15:58 -0600

    More reliably clean up spam in synced channels

--- src/main/java/ti4/listeners/UserLeaveServerListener.java
+++ src/main/java/ti4/listeners/UserLeaveServerListener.java
@@ -21,6 +21,7 @@ import ti4.map.persistence.ManagedGame;
 import ti4.map.persistence.ManagedPlayer;
 import ti4.message.MessageHelper;
 import ti4.message.logging.BotLogger;
+import ti4.service.async.BanCleanupService;
 import ti4.service.emoji.MiscEmojis;
 import ti4.settings.users.UserSettingsManager;
 import ti4.spring.jda.JdaService;
@@ -46,6 +47,9 @@ public class UserLeaveServerListener extends ListenerAdapter {
                                     if (log.getTargetIdLong() == event.getUser().getIdLong()) {
                                         if (log.getType() == ActionType.BAN || log.getType() == ActionType.KICK) {
                                             voluntary = false;
+                                            if (log.getType() == ActionType.BAN) {
+                                                BanCleanupService.cleanupBotQuestionChannels(event.getUser());
+                                            }
                                             break;
                                         }
                                     }
