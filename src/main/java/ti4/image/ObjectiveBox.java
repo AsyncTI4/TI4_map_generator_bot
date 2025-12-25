2f3f7b1a35cda6568474df94a05f5c739836365b
Wezabi <Westonhuhman@gmail.com>
2025-12-24 21:32:58 -0600

    Update WebObjectives data to support red tape data

--- src/main/java/ti4/image/ObjectiveBox.java
+++ src/main/java/ti4/image/ObjectiveBox.java
@@ -42,7 +42,7 @@ class ObjectiveBox {
 
         graphics.drawString(objective.getDisplayText(game), x, y + textVerticalOffset);
         graphics.drawRect(x - horizontalBoxOffset, y - spacingBetweenBoxes, boxWidth, objectiveBoxHeight);
-        if (objective.revealed() && Mapper.getPublicObjective(objective.key()) != null) {
+        if ((objective.revealed() || game.isRedTapeMode() || game.isCivilizedSocietyMode()) && Mapper.getPublicObjective(objective.key()) != null) {
             generator.addWebsiteOverlay(
                     Mapper.getPublicObjective(objective.key()),
                     x - horizontalBoxOffset,
