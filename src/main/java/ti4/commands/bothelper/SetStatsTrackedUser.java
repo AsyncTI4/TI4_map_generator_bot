95253856ecf030c658e8e77a6521578b315623b4
Will Holton <willholton1@gmail.com>
2025-12-31 16:41:43 -0500

    Add bothelper command to set stats tracked user (#4204)
    * Add bothelper command to set stats tracking user
    
    * Refactor SetStatsTrackedUser to use JdaService for member lookup
    
    Updated SetStatsTrackedUser to retrieve guild members using JdaService.guildPrimary instead of the event's guild. Also removed unused imports and simplified player retrieval logic.

---
+++ src/main/java/ti4/commands/bothelper/SetStatsTrackedUser.java
@@ -0,0 +1,39 @@
+package ti4.commands.bothelper;
+
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import ti4.commands.GameStateSubcommand;
+import ti4.helpers.Constants;
+import ti4.map.Player;
+import ti4.message.MessageHelper;
+import ti4.spring.jda.JdaService;
+
+class SetStatsTrackedUser extends GameStateSubcommand {
+
+    SetStatsTrackedUser() {
+        super(Constants.SET_STATS_TRACKED_USER, "Set stats tracking user for a player.", true, true);
+        addOptions(new OptionData(OptionType.STRING, Constants.FACTION_COLOR, "Player faction or color")
+                .setRequired(true)
+                .setAutoComplete(true));
+        addOptions(new OptionData(OptionType.USER, Constants.USER, "User to track stats for").setRequired(true));
+    }
+
+    @Override
+    public void execute(SlashCommandInteractionEvent event) {
+        Player player = getPlayer();
+
+        User trackedUser = event.getOption(Constants.USER).getAsUser();
+        Member member = JdaService.guildPrimary.getMember(trackedUser);
+        String trackedUserName = member == null ? trackedUser.getName() : member.getEffectiveName();
+
+        player.setStatsTrackedUserID(trackedUser.getId());
+        player.setStatsTrackedUserName(trackedUserName);
+
+        MessageHelper.sendMessageToChannel(
+                event.getMessageChannel(),
+                "Updated stats tracking for " + player.getUserName() + " to " + trackedUserName + ".");
+    }
+}
