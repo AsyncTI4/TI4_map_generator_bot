761e4c33c538ca3ae378fc9b1e38e0130f3265c7
Fingon00 <britainfan234@gmail.com>
2026-01-01 15:33:05 -0500

    Small but important fixes

--- src/main/java/ti4/helpers/ButtonHelperActionCards.java
+++ src/main/java/ti4/helpers/ButtonHelperActionCards.java
@@ -2934,7 +2934,7 @@ public class ButtonHelperActionCards {
         StringBuilder bestPlanet = new StringBuilder();
         for (String planet : player.getPlanetsAllianceMode()) {
             Planet p = game.getPlanetsInfo().get(planet);
-            if (p != null && p.getResources() > count) {
+            if (p != null && p.getResources() > count && !p.getName().equalsIgnoreCase("triad")) {
                 count = p.getResources();
                 bestPlanet = new StringBuilder(planet);
             } else if (p != null
