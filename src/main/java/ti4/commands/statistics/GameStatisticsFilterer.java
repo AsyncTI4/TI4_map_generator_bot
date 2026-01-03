de9b6f8de23ae7a9782c252886ad7a108f6c0d2c
Will <willholton1@gmail.com>
2026-01-03 10:23:30 -0500

    Update GameStatisticsFilterer.java

--- src/main/java/ti4/commands/statistics/GameStatisticsFilterer.java
+++ src/main/java/ti4/commands/statistics/GameStatisticsFilterer.java
@@ -3,7 +3,6 @@ package ti4.commands.statistics;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.function.Predicate;
 import lombok.experimental.UtilityClass;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
