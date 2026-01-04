3e0249fd69e021af1e59089dea6a333c027b2dcf
bradleysigma <bradley.sigma@gmail.com>
2026-01-04 18:48:29 +1000

    Updates
    - Wording changes
    - Fix space station convert error
    - Some end-of-status abilities only show when relevant

--- src/main/java/ti4/commands/agenda/Cleanup.java
+++ src/main/java/ti4/commands/agenda/Cleanup.java
@@ -21,7 +21,10 @@ class Cleanup extends GameStateSubcommand {
     public void execute(SlashCommandInteractionEvent event) {
         OptionMapping option = event.getOption(Constants.CONFIRM);
         if (option == null || !"YES".equals(option.getAsString())) {
-            MessageHelper.replyToMessage(event, "Must confirm with YES (case sensitive/full uppercase YES)");
+            MessageHelper.replyToMessage(
+                    event,
+                    "Must confirm with `YES`"
+                            + ("YES".equalsIgnoreCase(option.getAsString()) ? " - this is case sensitive" : "") + ".");
             return;
         }
 
