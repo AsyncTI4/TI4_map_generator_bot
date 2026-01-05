54b57107b32f03332df037eff5ddbfef1f403bb0
Will Holton <willholton1@gmail.com>
2026-01-05 07:01:20 -0500

    Resolve creationDateTime overlaps in RunAgainstAllGames (#4230)
    * Resolve creationDateTime overlaps
    
    * Refactor usedCreationDateTimes to local variable
    
    Moved usedCreationDateTimes from a static class field to a local variable within the execute method. Updated makeChanges to accept usedCreationDateTimes as a parameter, improving encapsulation and thread safety.

--- src/main/java/ti4/commands/developer/RunAgainstAllGames.java
+++ src/main/java/ti4/commands/developer/RunAgainstAllGames.java
@@ -1,21 +1,20 @@
 package ti4.commands.developer;
 
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import ti4.commands.Subcommand;
 import ti4.map.Game;
-import ti4.map.helper.GameHelper;
+import ti4.map.persistence.GameManager;
 import ti4.map.persistence.GamesPage;
 import ti4.message.MessageHelper;
 import ti4.message.logging.BotLogger;
 
 class RunAgainstAllGames extends Subcommand {
+
+    private static final long ONE_SECOND_MILLIS = 1000L;
 
     RunAgainstAllGames() {
         super("run_against_all_games", "Runs this custom code against all games.");
@@ -25,64 +24,36 @@ class RunAgainstAllGames extends Subcommand {
     public void execute(SlashCommandInteractionEvent event) {
         MessageHelper.sendMessageToChannel(event.getChannel(), "Running custom command against all games.");
 
-        Map<Long, List<String>> creationDateTimeToGames = new HashMap<>();
-        Map<Long, List<String>> setupTimestampToGames = new HashMap<>();
-        GamesPage.consumeAllGames(game -> {
-            long creationDateTime = game.getCreationDateTime();
-            creationDateTimeToGames
-                    .computeIfAbsent(creationDateTime, key -> new ArrayList<>())
-                    .add(formatGameLabel(game));
+        Set<Long> usedCreationDateTimes = new HashSet<>();
 
-            long setupTimestamp = getSetupTimestamp(game);
-            setupTimestampToGames
-                    .computeIfAbsent(setupTimestamp, key -> new ArrayList<>())
-                    .add(formatGameLabel(game));
+        List<String> changedGames = new ArrayList<>();
+        GamesPage.consumeAllGames(game -> {
+            boolean changed = makeChanges(game, usedCreationDateTimes);
+            if (changed) {
+                changedGames.add(game.getName());
+                GameManager.save(game, "Developer ran custom command against this game, probably migration related.");
+            }
         });
 
-        List<String> creationDateTimeOverlaps = formatOverlaps("creationDateTime", creationDateTimeToGames);
-        List<String> setupTimestampOverlaps = formatOverlaps("setupTimestamp", setupTimestampToGames);
-
-        BotLogger.info(String.join(System.lineSeparator(), creationDateTimeOverlaps));
-        BotLogger.info(String.join(System.lineSeparator(), setupTimestampOverlaps));
-
-        MessageHelper.sendMessageToChannel(
-                event.getChannel(),
-                "Finished custom command against all games. "
-                        + "creationDateTime overlaps: " + (creationDateTimeOverlaps.size() - 1)
-                        + ", setupTimestamp overlaps: " + (setupTimestampOverlaps.size() - 1) + ".");
+        BotLogger.info("Changes made to " + changedGames.size() + " games:" + String.join(", ", changedGames));
+        MessageHelper.sendMessageToChannel(event.getChannel(), "Finished custom command against all games.");
+        BotLogger.info("Changes made to " + changedGames.size() + " games out of " + GameManager.getGameCount()
+                + " games: " + String.join(", ", changedGames));
     }
 
-    private static List<String> formatOverlaps(String label, Map<Long, List<String>> timestampToGames) {
-        List<String> overlaps = timestampToGames.entrySet().stream()
-                .filter(entry -> entry.getValue().size() > 1)
-                .map(entry -> label + " overlap at " + entry.getKey() + ": " + String.join(", ", entry.getValue()))
-                .toList();
-        List<String> lines = new ArrayList<>();
-        lines.add("Found " + overlaps.size() + " " + label + " overlap(s).");
-        lines.addAll(overlaps);
-        return lines;
-    }
-
-    private static String formatGameLabel(Game game) {
-        return game.getName() + " (" + game.getID() + ")";
-    }
-
-    private static long getSetupTimestamp(Game game) {
-        LocalDate localDate;
-        try {
-            localDate = GameHelper.getCreationDateAsLocalDate(game);
-        } catch (DateTimeParseException e) {
-            localDate = LocalDate.now();
+    private static boolean makeChanges(Game game, Set<Long> usedCreationDateTimes) {
+        long creationDateTime = game.getCreationDateTime();
+        if (usedCreationDateTimes.add(creationDateTime)) {
+            return false;
         }
 
-        int gameNameHash = game.getName().hashCode();
-        int hours = Math.floorMod(gameNameHash, 24);
-        int minutes = Math.floorMod(gameNameHash, 60);
+        long updatedCreationDateTime = creationDateTime + ONE_SECOND_MILLIS;
+        while (usedCreationDateTimes.contains(updatedCreationDateTime)) {
+            updatedCreationDateTime += ONE_SECOND_MILLIS;
+        }
 
-        int customNameHash = game.getCustomName().hashCode();
-        int seconds = Math.floorMod(customNameHash, 60);
-
-        var localDateTime = localDate.atTime(hours, minutes, seconds);
-        return localDateTime.atZone(ZoneId.of("UTC")).toInstant().getEpochSecond();
+        game.setCreationDateTime(updatedCreationDateTime);
+        usedCreationDateTimes.add(updatedCreationDateTime);
+        return true;
     }
 }
