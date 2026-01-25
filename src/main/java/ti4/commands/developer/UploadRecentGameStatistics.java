ce88f500d7e98e2158c44c34f084458c409916a0
Will Holton <willholton1@gmail.com>
2026-01-25 05:16:45 -0500

    Add developer command to upload recently changed game statistics (#4327)
    * Add developer command for recent stats upload
    
    * Update UploadRecentGameStatistics.java

---
+++ src/main/java/ti4/commands/developer/UploadRecentGameStatistics.java
@@ -0,0 +1,28 @@
+package ti4.commands.developer;
+
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import ti4.commands.Subcommand;
+import ti4.helpers.Constants;
+import ti4.message.MessageHelper;
+import ti4.message.logging.BotLogger;
+import ti4.website.GameStatisticsUploadService;
+
+class UploadRecentGameStatistics extends Subcommand {
+
+    UploadRecentGameStatistics() {
+        super(Constants.UPLOAD_RECENT_GAME_STATS, "Upload recently changed game statistics.");
+    }
+
+    @Override
+    public void execute(SlashCommandInteractionEvent event) {
+        try {
+            GameStatisticsUploadService.uploadRecentStats();
+            MessageHelper.sendMessageToChannel(
+                    event.getChannel(), "Triggered upload of recently changed game statistics.");
+        } catch (Exception e) {
+            BotLogger.error("Failed to upload recently changed game statistics.", e);
+            MessageHelper.sendMessageToChannel(
+                    event.getChannel(), "Failed to upload recently changed game statistics.");
+        }
+    }
+}
