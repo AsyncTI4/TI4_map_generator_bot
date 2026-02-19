f299ced70cd07e05c083ec78ee79ae3e2a51a45f
Will <willholton1@gmail.com>
2026-02-19 04:43:20 -0500

    Extract game mode logic and update stats
    Add GameModeService to centralize game mode detection and replace duplicated mode logic in GameStatsDashboardPayload. Refactor GameModeStatisticsService to use GameModeService.getModes(), switch to Integer counters with merge(), sort and format entries, and send results to a thread via MessageHelper.sendMessageToThread. Remove the old GameModeStatisticsService test and obsolete predicate-based helpers.

--- src/main/java/ti4/service/statistics/game/GameModeStatisticsService.java
+++ src/main/java/ti4/service/statistics/game/GameModeStatisticsService.java
@@ -1,63 +1,46 @@
 package ti4.service.statistics.game;
 
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Predicate;
 import lombok.experimental.UtilityClass;
+import net.dv8tion.jda.api.entities.channel.unions.MessageChannelUnion;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import ti4.commands.statistics.GameStatisticsFilterer;
-import ti4.map.Game;
 import ti4.map.persistence.GamesPage;
 import ti4.message.MessageHelper;
+import ti4.service.game.GameModeService;
 
 @UtilityClass
 class GameModeStatisticsService {
 
     static void showModeCounts(SlashCommandInteractionEvent event) {
-        Map<String, Predicate<Game>> modePredicates = modePredicateByName();
         AtomicInteger totalGames = new AtomicInteger();
-        Map<String, AtomicInteger> modeCounts = buildModeCounters(modePredicates);
+        Map<String, Integer> modeCounts = new HashMap<>();
 
         GamesPage.consumeAllGames(GameStatisticsFilterer.getGamesFilter(event), game -> {
             totalGames.incrementAndGet();
-            modePredicates.forEach((modeName, modePredicate) -> {
-                if (modePredicate.test(game)) {
-                    modeCounts.get(modeName).incrementAndGet();
-                }
-            });
+            GameModeService.getModes(game).forEach(modeName -> modeCounts.merge(modeName, 1, Integer::sum));
         });
 
-        MessageHelper.sendMessageToChannel(
-                event.getMessageChannel(), formatModeStatistics(totalGames.get(), modeCounts));
+        MessageHelper.sendMessageToThread(
+                (MessageChannelUnion) event.getMessageChannel(),
+                "Game Mode Counts",
+                formatModeStatistics(totalGames.get(), modeCounts));
     }
 
-    static String formatModeStatistics(int totalGames, Map<String, AtomicInteger> modeCounts) {
+    private static String formatModeStatistics(int totalGames, Map<String, Integer> modeCounts) {
         StringBuilder message = new StringBuilder("Game count by mode (total games: " + totalGames + "):\n");
-        modeCounts.forEach((modeName, modeCount) -> {
-            double percent = totalGames == 0 ? 0.0 : (modeCount.get() * 100.0) / totalGames;
+        modeCounts.entrySet().stream().sorted(Map.Entry.comparingByValue()).forEach(entry -> {
+            double percent = totalGames == 0 ? 0.0 : (entry.getValue() * 100.0) / totalGames;
             message.append("- ")
-                    .append(modeName)
+                    .append(entry.getKey())
                     .append(": ")
-                    .append(modeCount.get())
+                    .append(entry.getValue())
                     .append(" (")
                     .append(String.format("%.1f%%", percent))
                     .append(")\n");
         });
         return message.toString();
-    }
-
-    private static Map<String, AtomicInteger> buildModeCounters(Map<String, Predicate<Game>> modePredicates) {
-        Map<String, AtomicInteger> modeCounts = new LinkedHashMap<>();
-        modePredicates.keySet().forEach(mode -> modeCounts.put(mode, new AtomicInteger()));
-        return modeCounts;
-    }
-
-    private static Map<String, Predicate<Game>> modePredicateByName() {
-        Map<String, Predicate<Game>> modes = new LinkedHashMap<>();
-        modes.put("Prophecy of Kings", Game::isProphecyOfKings);
-        modes.put("Thunder's Edge", Game::isThundersEdge);
-        modes.put("Discordant Stars", Game::isDiscordantStarsMode);
-        return modes;
     }
 }
--- src/main/java/ti4/website/model/stats/GameStatsDashboardPayload.java
+++ src/main/java/ti4/website/model/stats/GameStatsDashboardPayload.java
@@ -12,11 +12,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import ti4.helpers.Constants;
-import ti4.helpers.omega_phase.PriorityTrackHelper.PriorityTrackMode;
 import ti4.image.Mapper;
 import ti4.map.Game;
 import ti4.map.Player;
@@ -25,6 +23,7 @@ import ti4.map.helper.GameHelper;
 import ti4.model.AgendaModel;
 import ti4.model.PublicObjectiveModel;
 import ti4.model.SecretObjectiveModel;
+import ti4.service.game.GameModeService;
 import ti4.service.map.FractureService;
 
 public class GameStatsDashboardPayload {
@@ -165,68 +164,7 @@ public class GameStatsDashboardPayload {
     }
 
     public List<String> getModes() {
-        List<String> enabledModes = Stream.of(
-                        Map.entry("Alliance", (Supplier<Boolean>) game::isAllianceMode),
-                        Map.entry("Community", (Supplier<Boolean>) game::isCommunityMode),
-                        Map.entry("TIGL", (Supplier<Boolean>) game::isCompetitiveTIGLGame),
-                        Map.entry("Fog of War", (Supplier<Boolean>) game::isFowMode),
-                        Map.entry("Light Fog", (Supplier<Boolean>) game::isLightFogMode),
-                        Map.entry("Absol", (Supplier<Boolean>) game::isAbsolMode),
-                        Map.entry("Discordant Stars", (Supplier<Boolean>) game::isDiscordantStarsMode),
-                        Map.entry("Uncharted Space", (Supplier<Boolean>) game::isUnchartedSpaceStuff),
-                        Map.entry("Milty Mod", (Supplier<Boolean>) game::isMiltyModMode),
-                        Map.entry("Promises, Promises", (Supplier<Boolean>) game::isPromisesPromisesMode),
-                        Map.entry("Flagshipping", (Supplier<Boolean>) game::isFlagshippingMode),
-                        Map.entry("Red Tape", (Supplier<Boolean>) game::isRedTapeMode),
-                        Map.entry("Omega Phase", (Supplier<Boolean>) game::isOmegaPhaseMode),
-                        Map.entry("Homebrew", (Supplier<Boolean>) game::hasHomebrew),
-                        Map.entry("Homebrew Strategy Cards", (Supplier<Boolean>) game::isHomebrewSCMode),
-                        Map.entry("Extra Secret", (Supplier<Boolean>) game::isExtraSecretMode),
-                        Map.entry("Voice of the Council", (Supplier<Boolean>) game::isVotcMode),
-                        Map.entry("Base Game", (Supplier<Boolean>) game::isBaseGameMode),
-                        Map.entry("Prophecy of Kings", (Supplier<Boolean>) game::isProphecyOfKings),
-                        Map.entry("Thunder's Edge", (Supplier<Boolean>)
-                                () -> game.isThundersEdge() && !game.isThundersEdgeDemo()),
-                        Map.entry("Thunder's Edge Demo", (Supplier<Boolean>) game::isThundersEdgeDemo),
-                        Map.entry("Twilight's Fall", (Supplier<Boolean>) game::isTwilightsFallMode),
-                        Map.entry("Age of Exploration", (Supplier<Boolean>) game::isAgeOfExplorationMode),
-                        Map.entry("Facilities", (Supplier<Boolean>) game::isFacilitiesMode),
-                        Map.entry("Minor Factions", (Supplier<Boolean>) game::isMinorFactionsMode),
-                        Map.entry("Total War", (Supplier<Boolean>) game::isTotalWarMode),
-                        Map.entry("Dangerous Wilds", (Supplier<Boolean>) game::isDangerousWildsMode),
-                        Map.entry("Civilized Society", (Supplier<Boolean>) game::isCivilizedSocietyMode),
-                        Map.entry("Age of Fighters", (Supplier<Boolean>) game::isAgeOfFightersMode),
-                        Map.entry("Mercenaries for Hire", (Supplier<Boolean>) game::isMercenariesForHireMode),
-                        Map.entry("Advent of the Warsun", (Supplier<Boolean>) game::isAdventOfTheWarsunMode),
-                        Map.entry("Cultural Exchange Program", (Supplier<Boolean>) game::isCulturalExchangeProgramMode),
-                        Map.entry("Conventions of War Abandoned", (Supplier<Boolean>)
-                                game::isConventionsOfWarAbandonedMode),
-                        Map.entry("Rapid Mobilization", (Supplier<Boolean>) game::isRapidMobilizationMode),
-                        Map.entry("Weird Wormholes", (Supplier<Boolean>) game::isWeirdWormholesMode),
-                        Map.entry("Cosmic Phenomenae", (Supplier<Boolean>) game::isCosmicPhenomenaeMode),
-                        Map.entry("Monument to the Ages", (Supplier<Boolean>) game::isMonumentToTheAgesMode),
-                        Map.entry("Wild, Wild Galaxy", (Supplier<Boolean>) game::isWildWildGalaxyMode),
-                        Map.entry("Zealous Orthodoxy", (Supplier<Boolean>) game::isZealousOrthodoxyMode),
-                        Map.entry("Stellar Atomics", (Supplier<Boolean>) game::isStellarAtomicsMode),
-                        Map.entry("No Support Swap", (Supplier<Boolean>) game::isNoSwapMode),
-                        Map.entry("Age of Commerce", (Supplier<Boolean>) game::isAgeOfCommerceMode),
-                        Map.entry("Hidden Agenda", (Supplier<Boolean>) game::isHiddenAgendaMode),
-                        Map.entry("Ordinian", (Supplier<Boolean>) game::isOrdinianC1Mode),
-                        Map.entry("Liberation", (Supplier<Boolean>) game::isLiberationC4Mode))
-                .filter(entry -> entry.getValue().get())
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toCollection(ArrayList::new));
-
-        if (game.getSpinMode() != null && !"OFF".equalsIgnoreCase(game.getSpinMode())) {
-            enabledModes.add("Spin Mode");
-        }
-
-        PriorityTrackMode priorityTrackMode = game.getPriorityTrackMode();
-        if (priorityTrackMode != null && priorityTrackMode != PriorityTrackMode.NONE) {
-            enabledModes.add(priorityTrackMode.name() + " Priority Track");
-        }
-
-        return enabledModes;
+        return GameModeService.getModes(game);
     }
 
     public String getPlatform() {
