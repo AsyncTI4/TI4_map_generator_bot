3c7f7fcd8346088a8e2b4502383475a7163ff8d8
Will <willholton1@gmail.com>
2026-02-23 22:03:53 -0500

    Update PlayerEntity.java

--- src/main/java/ti4/spring/persistence/PlayerEntity.java
+++ src/main/java/ti4/spring/persistence/PlayerEntity.java
@@ -26,7 +26,7 @@ public class PlayerEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private UserEntity user;
 
