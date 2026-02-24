fdd77706a6995f66782cf79e0397d70a13f207df
Will <willholton1@gmail.com>
2026-02-23 22:01:49 -0500

    Update TurnCountService.java

--- src/main/java/ti4/spring/service/statistics/TurnCountService.java
+++ src/main/java/ti4/spring/service/statistics/TurnCountService.java
@@ -1,17 +1,14 @@
 package ti4.spring.service.statistics;
 
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ti4.spring.context.SpringContext;
 import ti4.spring.persistence.PlayerEntity;
 import ti4.spring.persistence.PlayerEntityRepository;
-import ti4.spring.persistence.UserEntity;
 
 @Service
 @RequiredArgsConstructor
@@ -22,49 +19,23 @@ public class TurnCountService {
     @Transactional(readOnly = true)
     public Map<String, Integer> getUserIdsToTurnCounts(List<String> userIds) {
         List<PlayerEntity> players = playerEntityRepository.findAllPlayersForUsers(userIds);
-
-        List<UserTurnCountAccumulator> turnCounts = getTurnCounts(players);
-
-        return turnCounts.stream()
-                .collect(Collectors.toMap(
-                        acc -> acc.userId, acc -> acc.totalTurns, (existing, replacement) -> existing));
+        return getTurnCounts(players);
     }
 
-    private List<UserTurnCountAccumulator> getTurnCounts(List<PlayerEntity> players) {
-        Map<UserEntity, UserTurnCountAccumulator> statsMap = new HashMap<>();
+    private Map<String, Integer> getTurnCounts(List<PlayerEntity> players) {
+        Map<String, Integer> usersToTurnCounts = new HashMap<>();
 
         for (PlayerEntity player : players) {
             if (player.getTotalNumberOfTurns() <= 0) {
                 continue;
             }
-            statsMap.computeIfAbsent(player.getUser(), user -> new UserTurnCountAccumulator(user.getId()))
-                    .addTurns(player.getTotalNumberOfTurns());
+            usersToTurnCounts.merge(player.getUser().getId(), 1, Integer::sum);
         }
 
-        return statsMap.values().stream()
-                .sorted(Comparator.comparingInt(UserTurnCountAccumulator::getTotalTurns)
-                        .reversed())
-                .toList();
+        return usersToTurnCounts;
     }
 
     public static TurnCountService getBean() {
         return SpringContext.getBean(TurnCountService.class);
-    }
-
-    private static class UserTurnCountAccumulator {
-        String userId;
-        int totalTurns;
-
-        UserTurnCountAccumulator(String userId) {
-            this.userId = userId;
-        }
-
-        void addTurns(int turns) {
-            totalTurns += turns;
-        }
-
-        int getTotalTurns() {
-            return totalTurns;
-        }
     }
 }
