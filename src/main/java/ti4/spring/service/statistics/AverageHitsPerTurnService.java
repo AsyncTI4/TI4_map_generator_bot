9cb2c75ddca6c5762603402a0c61d22fb33c9491
Will <willholton1@gmail.com>
2026-02-24 16:29:26 -0500

    Update AverageHitsPerTurnService.java

--- src/main/java/ti4/spring/service/statistics/AverageHitsPerTurnService.java
+++ src/main/java/ti4/spring/service/statistics/AverageHitsPerTurnService.java
@@ -143,7 +143,7 @@ public class AverageHitsPerTurnService {
 
     private static String getPercentile(int index, int totalPlayers) {
         double percentile = ((double) index / totalPlayers) * 100;
-        return String.format("Top %.2f%%", percentile);
+        return String.format("%.2f%% have higher", percentile);
     }
 
     public static AverageHitsPerTurnService getBean() {
