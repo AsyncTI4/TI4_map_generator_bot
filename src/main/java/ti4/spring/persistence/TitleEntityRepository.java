bb46e6ffa5909cbb52cf2b31237f91d790fb7f52
Will <willholton1@gmail.com>
2026-02-26 14:26:03 -0500

    Update TitleEntityRepository.java

--- src/main/java/ti4/spring/persistence/TitleEntityRepository.java
+++ src/main/java/ti4/spring/persistence/TitleEntityRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface TitleEntityRepository extends JpaRepository<TitleEntity, Long> {
 
-    @Query("SELECT t FROM TitleEntity t JOIN FETCH t.user u")
+    @Query("SELECT t FROM TitleEntity t JOIN FETCH t.user")
     List<TitleEntity> findAllWithUsers();
 }
