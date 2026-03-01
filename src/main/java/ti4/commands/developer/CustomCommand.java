7acb8dce7ea9e91595647e9b4a56b0200372ce54
Will <willholton1@gmail.com>
2026-03-01 06:56:20 -0500

    Update CustomCommand.java

--- src/main/java/ti4/commands/developer/CustomCommand.java
+++ src/main/java/ti4/commands/developer/CustomCommand.java
@@ -7,23 +7,21 @@ import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import ti4.buttons.Buttons;
-import ti4.commands.GameStateSubcommand;
+import ti4.commands.Subcommand;
 import ti4.helpers.Constants;
+import ti4.map.Game;
 import ti4.map.Player;
 import ti4.map.persistence.GameManager;
 import ti4.message.MessageHelper;
 
-class CustomCommand extends GameStateSubcommand {
+class CustomCommand extends Subcommand {
 
     CustomCommand() {
-        super("custom_command", "Custom command written for a custom purpose.", true, true);
+        super("custom_command", "Custom command written for a custom purpose.");
         addOptions(new OptionData(OptionType.STRING, Constants.GAME_NAME, "The game to run the command against.")
                 .setRequired(true)
                 .setAutoComplete(true));
-        addOptions(new OptionData(
-                        OptionType.STRING,
-                        Constants.FACTION_COLOR,
-                        "Faction/color of the player to spawn Alliance Rider buttons for")
+        addOptions(new OptionData(OptionType.STRING, Constants.FACTION_COLOR, "Faction/color of the player")
                 .setRequired(true)
                 .setAutoComplete(true));
     }
@@ -35,15 +33,22 @@ class CustomCommand extends GameStateSubcommand {
             MessageHelper.sendMessageToChannel(event.getChannel(), "Game not found: " + gameName);
             return;
         }
+        Game game = GameManager.getManagedGame(gameName).getGame();
 
-        Player player = getPlayer();
+        String factionColor = event.getOption(Constants.FACTION_COLOR, null, OptionMapping::getAsString);
+        Player player = game.getPlayerFromColorOrFaction(factionColor);
+        if (player == null) {
+            MessageHelper.sendMessageToChannel(
+                    event.getChannel(),
+                    "Player not found for faction/color `" + factionColor + "` in game `" + gameName + "`.");
+            return;
+        }
 
-        List<Button> buttons = List.of(
-                Buttons.green(player.getFinsFactionCheckerPrefix() + "resolveOracle", "Resolve Oracle"));
+        List<Button> buttons =
+                List.of(Buttons.green(player.getFinsFactionCheckerPrefix() + "resolveOracle", "Resolve Oracle"));
         MessageHelper.sendMessageToChannelWithButtons(
                 event.getMessageChannel(),
-                player.getRepresentation()
-                        + " may use this button to resolve _Oracle_.",
+                player.getRepresentation() + " may use this button to resolve _Oracle_.",
                 buttons);
     }
 }
