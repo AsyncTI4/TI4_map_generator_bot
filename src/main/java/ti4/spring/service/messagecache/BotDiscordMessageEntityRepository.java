13c268a960ded41b161262a2ab3ecfe528b35c13
Will <willholton1@gmail.com>
2026-03-04 11:30:44 -0500

    Rename message cache entity and repository
    Rename BotMessageRecord -> DiscordMessageEntity and BotMessageCacheRepository -> DiscordMessageEntityRepository, and update the DB table name from 'bot_message_cache' to 'discord_message'. Update BotMessageCacheService to use the new entity/repository types and adjust method references (content lookup and expiration delete) while keeping existing retention logic.

--- src/main/java/ti4/spring/service/messagecache/BotDiscordMessageEntityRepository.java
+++ src/main/java/ti4/spring/service/messagecache/BotDiscordMessageEntityRepository.java
@@ -2,6 +2,6 @@ package ti4.spring.service.messagecache;
 
 import org.springframework.data.repository.CrudRepository;
 
-interface BotMessageCacheRepository extends CrudRepository<BotMessageRecord, Long> {
+interface BotDiscordMessageEntityRepository extends CrudRepository<BotDiscordMessageEntity, Long> {
     void deleteByCreatedAtEpochMillisLessThan(long cutoffEpochMillis);
 }
