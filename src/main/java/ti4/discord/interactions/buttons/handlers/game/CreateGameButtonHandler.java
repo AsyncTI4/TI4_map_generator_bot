d672f079aa89d537843e632879e6e9a472128550
Will <willholton1@gmail.com>
2026-04-20 00:16:51 -0400

    F

--- src/main/java/ti4/discord/interactions/buttons/handlers/game/CreateGameButtonHandler.java
+++ src/main/java/ti4/discord/interactions/buttons/handlers/game/CreateGameButtonHandler.java
@@ -423,7 +423,7 @@ public class CreateGameButtonHandler {
         if (managedPlayer == null) return 0;
         Set<ManagedGame> managedGames = managedPlayer.getGames();
         return (int) managedGames.stream()
-                .filter(game -> game.getRealPlayers().size() >= 3)
+                .filter(game -> game.getRealPlayers().size() >= 2)
                 .filter(game -> !game.isHasEnded())
                 .filter(game -> game.isRealPlayer(managedPlayer))
                 .count();
--- src/main/java/ti4/discord/interactions/listeners/UserLeaveServerListener.java
+++ src/main/java/ti4/discord/interactions/listeners/UserLeaveServerListener.java
@@ -75,7 +75,7 @@ class UserLeaveServerListener extends ListenerAdapter {
 
     private static int getInProgressGamesCount(ManagedPlayer user) {
         return (int) user.getGames().stream()
-                .filter(managedGame -> managedGame.getRealPlayers().size() > 2)
+                .filter(managedGame -> managedGame.getRealPlayers().size() > 1)
                 .filter(mg -> !mg.isHasEnded() && !mg.isHasWinner() && !mg.isVpGoalReached())
                 .count();
     }
@@ -192,7 +192,7 @@ class UserLeaveServerListener extends ListenerAdapter {
                 .append(getInProgressGamesCount(player))
                 .append("__** in-progress games and **__")
                 .append(player.getGames().stream()
-                        .filter(managedGame -> managedGame.getRealPlayers().size() > 2)
+                        .filter(managedGame -> managedGame.getRealPlayers().size() > 1)
                         .count())
                 .append("__** lifetime 3+ player games across all servers.");
         if (!foundOne) {
