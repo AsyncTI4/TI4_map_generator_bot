9d6954678d6cae7de30a242c200673fd9f166017
Will <willholton1@gmail.com>
2026-04-27 05:18:09 -0400

    Update FactionAverageTurnsInGameStatisticsService.java

--- src/main/java/ti4/service/statistics/game/FactionAverageTurnsInGameStatisticsService.java
+++ src/main/java/ti4/service/statistics/game/FactionAverageTurnsInGameStatisticsService.java
@@ -27,8 +27,8 @@ class FactionAverageTurnsInGameStatisticsService {
 
         StringBuilder sb = new StringBuilder();
         sb.append("Average Turns per Faction:").append("\n");
-        Integer allFactionGames = factionCount.get("allFactions");
-        Integer allFactionTurns = factionTurnCount.get("allFactions");
+        Integer allFactionGames = factionCount.remove("allFactions");
+        Integer allFactionTurns = factionTurnCount.remove("allFactions");
         if (allFactionGames != null && allFactionTurns != null) {
             sb.append("All Factions Combined:")
                     .append(String.format("%.2f", allFactionTurns / (double) allFactionGames))
@@ -36,22 +36,21 @@ class FactionAverageTurnsInGameStatisticsService {
         }
         factionCount.entrySet().stream()
                 .sorted(Map.Entry.comparingByValue())
-                .filter(entry -> !"allFactions".equals(entry.getKey()))
+                .filter(entry -> Mapper.getFaction(entry.getKey()) != null)
                 .map(entry -> Map.entry(Mapper.getFaction(entry.getKey()), entry.getValue()))
-                .filter(entry -> entry.getKey() != null)
                 .forEach(entry -> sb.append('`')
-                        .append(StringUtils.leftPad(
-                                String.format(
-                                        "%.2f",
-                                        (factionTurnCount.get(entry.getKey().getAlias()) / (double) entry.getValue())),
-                                4))
-                        .append(" turns from ")
-                        .append(entry.getValue())
-                        .append(" games`")
-                        .append(entry.getKey().getFactionEmoji())
-                        .append(' ')
-                        .append(entry.getKey().getFactionNameWithSourceEmoji())
-                        .append('\n'));
+                    .append(StringUtils.leftPad(
+                        String.format(
+                            "%.2f",
+                            (factionTurnCount.get(entry.getKey().getAlias()) / (double) entry.getValue())),
+                        4))
+                    .append(" turns from ")
+                    .append(entry.getValue())
+                    .append(" games`")
+                    .append(entry.getKey().getFactionEmoji())
+                    .append(' ')
+                    .append(entry.getKey().getFactionNameWithSourceEmoji())
+                    .append('\n'));
 
         MessageHelper.sendMessageToThread(
                 (MessageChannelUnion) event.getMessageChannel(), "Average Turns per Faction", sb.toString());
