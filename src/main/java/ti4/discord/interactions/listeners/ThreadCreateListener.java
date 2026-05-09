5e8685cdfabeec702b9295d25ba18d694306b3ea
Will <willholton1@gmail.com>
2026-05-09 01:35:32 -0400

    Add Thread Archiver Cooldown (#5014)

--- src/main/java/ti4/discord/interactions/listeners/ThreadCreateListener.java
+++ src/main/java/ti4/discord/interactions/listeners/ThreadCreateListener.java
@@ -1,5 +1,9 @@
 package ti4.discord.interactions.listeners;
 
+import java.time.Instant;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
 import net.dv8tion.jda.api.events.channel.ChannelCreateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -10,12 +14,17 @@ import ti4.spring.service.deploy.ActiveLeaseService;
 
 class ThreadCreateListener extends ListenerAdapter {
 
+    private static final Map<String, Instant> guildToLatestArchive = new ConcurrentHashMap<>();
+    private static final int GUILD_THREAD_ARCHIVE_COOLDOWN_SECONDS = 5;
+
     @Override
     public void onChannelCreate(@NotNull ChannelCreateEvent event) {
         if (!ActiveLeaseService.shouldHandleCurrentProcessInteraction()) return;
         if (!event.getChannelType().isThread()) return;
         // These are archived already, and if this listener fails it could death spiral on Stack Trace threads
         if ("Stack Trace".equalsIgnoreCase(event.getChannel().getName())) return;
+
+        if (isOnCooldown(event.getGuild().getId())) return;
 
         // lock per guild so we don't run it more than once
         ExecutorServiceManager.runAsyncWithLock(
@@ -26,5 +35,21 @@ class ThreadCreateListener extends ListenerAdapter {
                         BotLogger.error("Failed to check thread limit and archive on thread creation", e);
                     }
                 });
+    }
+
+    private static boolean isOnCooldown(String guildId) {
+        AtomicBoolean onCooldown = new AtomicBoolean(true);
+        guildToLatestArchive.compute(guildId, (_, existingTime) -> {
+            Instant now = Instant.now();
+            if (existingTime == null
+                    || existingTime
+                            .plusSeconds(GUILD_THREAD_ARCHIVE_COOLDOWN_SECONDS)
+                            .isBefore(now)) {
+                onCooldown.set(false);
+                return now;
+            }
+            return existingTime;
+        });
+        return onCooldown.get();
     }
 }
