0d5d9793c1762710949947892a4054bd464b7902
Will <willholton1@gmail.com>
2026-05-20 00:53:48 -0400

    Revert "(LOG) MMR for lobbies (#5106)" (#5108)

--- src/main/java/ti4/spring/service/statistics/matchmaking/MatchmakingGameQuality.java
+++
@@ -1,8 +0,0 @@
-package ti4.spring.service.statistics.matchmaking;
-
-public record MatchmakingGameQuality(MatchmakingSkillLevel skillRating, MatchmakingSkillLevel skillDifference) {
-
-    static MatchmakingGameQuality unknown() {
-        return new MatchmakingGameQuality(MatchmakingSkillLevel.UNKNOWN, MatchmakingSkillLevel.UNKNOWN);
-    }
-}
