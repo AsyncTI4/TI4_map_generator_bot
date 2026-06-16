3ffb7deab908f1af14009f5859389cf7d7404f1e
Will <willholton1@gmail.com>
2026-06-15 20:31:44 -0400

    Update GuildWhitelistListener.java

--- src/main/java/ti4/discord/interactions/listeners/GuildWhitelistListener.java
+++ src/main/java/ti4/discord/interactions/listeners/GuildWhitelistListener.java
@@ -13,26 +13,27 @@ class GuildWhitelistListener extends ListenerAdapter {
     @Override
     public void onReady(ReadyEvent event) {
         event.getJDA().getGuilds().stream()
-                .filter(guild -> JdaService.guilds.stream()
-                        .noneMatch(whitelistGuild -> whitelistGuild.getId().equals(guild.getId())))
-                .forEach(badGuild -> {
-                    postLeavingGuildWarning(badGuild);
-                    badGuild.leave().queue(Consumers.nop(), BotLogger::catchRestError);
-                });
+                .filter(GuildWhitelistListener::isNotWhitelistedGuild)
+                .forEach(GuildWhitelistListener::leaveGuild);
+    }
+
+    private static boolean isNotWhitelistedGuild(Guild guild) {
+        return JdaService.guilds.stream()
+                .noneMatch(whitelistGuild -> whitelistGuild.getId().equals(guild.getId()));
+    }
+
+    private static void leaveGuild(Guild badGuild) {
+        logLeavingGuildWarning(badGuild);
+        badGuild.leave().queue(Consumers.nop(), BotLogger::catchRestError);
+    }
+
+    private static void logLeavingGuildWarning(Guild eventGuild) {
+        BotLogger.warning("Leaving guild '" + eventGuild.getName() + "' because it wasn't whitelisted!");
     }
 
     @Override
     public void onGuildJoin(GuildJoinEvent event) {
         Guild eventGuild = event.getGuild();
-        boolean isBadGuild = JdaService.guilds.stream()
-                .noneMatch(whitelistGuild -> whitelistGuild.getId().equals(eventGuild.getId()));
-        if (isBadGuild) {
-            postLeavingGuildWarning(eventGuild);
-            event.getGuild().leave().queue();
-        }
-    }
-
-    private static void postLeavingGuildWarning(Guild eventGuild) {
-        BotLogger.warning("Leaving guild '" + eventGuild.getName() + "' because it wasn't whitelisted!");
+        if (isNotWhitelistedGuild(eventGuild)) leaveGuild(eventGuild);
     }
 }
