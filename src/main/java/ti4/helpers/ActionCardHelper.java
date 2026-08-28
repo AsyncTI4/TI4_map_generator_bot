743ff08967430936e56acd641fd476601e04f1e7
Jay Cyr Zak <JayCyrZak@gmail.com>
2026-08-28 12:58:19 +0200

    remove old TK starflare

--- src/main/java/ti4/helpers/ActionCardHelper.java
+++ src/main/java/ti4/helpers/ActionCardHelper.java
@@ -384,9 +384,6 @@ public class ActionCardHelper {
                         .append(actionCard.getRepresentationJustText((game)))
                         .append('\n');
                 if (actionCard.getNotes() != null) {
-                    if (game != null && game.isTwilightKart() && "tf-starflare".equalsIgnoreCase(actionCard.getID())) {
-                        continue;
-                    }
                     sb.append("> -# [").append(actionCard.getNotes()).append("]\n");
                 }
             }
