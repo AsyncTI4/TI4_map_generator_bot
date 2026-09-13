22bc6fb1b261d17360174dc9c907428c0f8c295b
Wezabi <Westonhuhman@gmail.com>
2026-09-12 20:33:46 -0500

    Fix mvn spotbugs

--- src/main/java/ti4/game/Player.java
+++ src/main/java/ti4/game/Player.java
@@ -831,7 +831,7 @@ public class Player extends PlayerProperties implements StoredValueHelper {
 
     public int getUnitCap(String unit) {
         if (unitCaps.get(unit) == null) {
-            if (unit == "monument") return 1;
+            if ("monument".equals(unit)) return 1;
             if (PositionMapper.getReinforcementsPosition(unit) == null) return 0;
             return PositionMapper.getReinforcementsPosition(unit).getPositionCount(unit);
             // return 0;
